780bcf9e6fbadfa75bb876d08a1753be2c5f5c57
marvinrusinek <marvinrusinek@gmail.com>
2020-03-05 11:33:43 -0500

    1 file modified

--- src/app/containers/scoreboard/timer/timer.component.ts
+++ src/app/containers/scoreboard/timer/timer.component.ts
@@ -1,29 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
-
-import { TimerService } from '../../../services/timer.service';
-import { QuizService } from '../../../services/quiz.service';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'codelab-scoreboard-timer',
   templateUrl: './timer.component.html',
-  styleUrls: ['./timer.component.scss'],
-  providers: [ QuizService, TimerService ]
+  styleUrls: ['./timer.component.scss']
 })
-export class TimerComponent implements OnInit {
-  @Input() correctAnswers: [];
+export class TimerComponent {
   @Input() timeLeft: number;
-  @Input() hasAnswer: boolean;
-  @Input() showExplanation: boolean;
-  timePerQuestion = 20;
-  interval: any;
-  quizIsOver: boolean;
-
-  constructor(private quizService: QuizService, private timerService: TimerService) {}
-
-  ngOnInit(): void {
-    this.timeLeft = this.timePerQuestion;
-    this.countdown();
-  }
 
   // countdown clock
   private countdown() {
@@ -34,30 +17,49 @@ export class TimerComponent implements OnInit {
         if (this.timeLeft > 0) {
           this.timeLeft--;
 
-          if (this.correctAnswers.length !== null) {
+          if (this.answer !== null) {
             this.showExplanation = true;
             this.timerService.elapsedTime = Math.ceil(this.timePerQuestion - this.timeLeft);
-            this.timerService.calculateTotalElapsedTime(this.timerService.elapsedTimes);
-            // this.checkIfAnsweredCorrectly(this.quizData.questions[this.questionIndex].options[this.optionIndex]);
+            this.calculateTotalElapsedTime(this.elapsedTimes);
+            // this.checkIfAnsweredCorrectly(this.DIQuiz.questions[this.questionIndex].options[this.optionIndex]);
           }
 
-          if (this.timeLeft === 0 && !this.quizService.isFinalQuestion()) {
-            this.quizService.navigateToNextQuestion();
+          if (this.timeLeft === 0 && !this.isFinalQuestion()) {
+            this.navigateToNextQuestion();
           }
-          if (this.timeLeft === 0 && this.quizService.isFinalQuestion()) {
-            this.quizService.calculateQuizPercentage();
-            this.quizService.navigateToResults();
+          if (this.timeLeft === 0 && this.isFinalQuestion()) {
+            this.calculateQuizPercentage();
+            this.navigateToResults();
           }
-          if (this.quizService.isFinalQuestion() && this.hasAnswer === true) {
-            this.quizService.calculateQuizPercentage();
-            this.quizService.navigateToResults();
+          if (this.isFinalQuestion() && this.hasAnswer === true) {
+            this.calculateQuizPercentage();
+            this.navigateToResults();
             this.quizIsOver = true;
           }
 
-          // disable the next button until an option has been selected - work on this later!
-          // this.answer === null ? this.disabled = true : this.disabled = false;
+          // disable the next button until an option has been selected
+          this.answer === null ? this.disabled = true : this.disabled = false;
         }
       }, 1000);
+    }
+  }
+
+  private resetTimer() {
+    this.timeLeft = this.timePerQuestion;
+  }
+
+  private stopTimer() {
+    this.timeLeft = this.timePerQuestion - this.elapsedTime;
+  }
+
+  quizDelay(milliseconds) {
+    const start = new Date().getTime();
+    let counter = 0;
+    let end = 0;
+
+    while (counter < milliseconds) {
+      end = new Date().getTime();
+      counter = end - start;
     }
   }
 }
