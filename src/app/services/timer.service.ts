ed32cc878744ca91a809c5d9976426796b30207c
marvinrusinek <marvinrusinek@gmail.com>
2020-03-16 21:49:07 -0400

    remove stray comma

--- src/app/services/timer.service.ts
+++ src/app/services/timer.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Input, Output } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class TimerService {
   @Input() timeLeft: number;
