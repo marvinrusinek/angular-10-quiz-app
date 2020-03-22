7c87cf9621ece1065581764f14acb2f9a1d2c6f1
marvinrusinek <marvinrusinek@gmail.com>
2020-03-22 10:28:14 -0400

    2 files modified

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -17,6 +17,7 @@ export class QuizQuestionComponent implements OnInit, OnChanges {
   matRadio: boolean;
   option: number;
   optionText: string;
+  correctAnswers = [];
   
   constructor(private quizService: QuizService) {
     this.optionText = this.quizService.optionText;
@@ -33,6 +34,40 @@ export class QuizQuestionComponent implements OnInit, OnChanges {
     if (changes.question && changes.question.currentValue && !changes.question.firstChange) {
       this.formGroup.patchValue({answer: ''});
     }
+  }
+
+  addCorrectAnswersToArray(optionIndex: number): void {
+    if (this.question.options[optionIndex].correct === true) {
+      this.correctAnswers = [...this.correctAnswers, optionIndex];
+      console.log(this.correctAnswers);
+    }
+
+    // increment indexes by 1 to show correct option numbers
+    // if there's only one answer
+    if (this.correctAnswers.length === 1) {
+      let firstAnswer = this.correctAnswers[0] + 1;
+      this.optionText = "Option " + firstAnswer;
+    }
+
+    // if there's more than one answer
+    if (this.correctAnswers.length > 1) {
+      let firstAnswer = this.correctAnswers[0] + 1;
+      let secondAnswer = this.correctAnswers[1] + 1;
+      let thirdAnswer = this.correctAnswers[2] + 1;
+
+      if (firstAnswer && secondAnswer) {
+        this.optionText = "Options " +  firstAnswer + " and " + secondAnswer;
+      }
+      if (firstAnswer && secondAnswer && thirdAnswer) {
+        this.optionText = "Options " +  firstAnswer + ", " + secondAnswer + 
+        " and " + thirdAnswer;
+      }
+    }
+
+    // highlight all correct answers at the same time (maybe?)
+    // sort the correct answers in numerical order 1 & 2 instead of 2 & 1
+    // once the correct answer(s) are selected, pause quiz and prevent any other answers from being selected,
+    // display "Move on to next question...") or somehow animate the next button so it's obvious to move to the next question
   }
 
   radioChange(answer: number) {
