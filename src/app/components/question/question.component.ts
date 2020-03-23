28db15ec2989bc100595960ecdffa211fbeab338
marvinrusinek <marvinrusinek@gmail.com>
2020-03-23 15:10:05 -0400

    1 file modified

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -52,6 +52,8 @@ export class QuizQuestionComponent implements OnInit, OnChanges {
   setSelected(optionIndex: number): void {
     this.question.options.forEach(o => o.selected = false);
     this.question.options[optionIndex].selected = true;
+
+    // should add indexes only if the correct answer is chosen
     this.quizService.addCorrectIndexesToCorrectAnswerOptionsArray(optionIndex);   // add correct option(s) positions to the correctAnswers array
     this.quizService.setCorrectAnswerText();
   }
