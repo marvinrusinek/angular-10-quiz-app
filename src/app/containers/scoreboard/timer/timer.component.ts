b44a9dbd554b9a0c07dea16f5ef4cc774aed49b5
marvinrusinek <marvinrusinek@gmail.com>
2020-03-25 12:24:47 -0400

    1 file modified

--- src/app/containers/scoreboard/timer/timer.component.ts
+++ src/app/containers/scoreboard/timer/timer.component.ts
@@ -36,37 +36,37 @@ export class TimerComponent implements OnInit {
 
   // countdown clock
   timer() {
-    if (this.quizService.isThereAnotherQuestion()) {      
+    if (this.quizService.isThereAnotherQuestion()) {
       setInterval(() => {
         if (this.timeleft > 0) {
           this.timeleft--;
           console.log("timeleft: " + this.timeleft);
-        }
 
-        // check if question has been answered (not equal to null)
-        if (this.answer !== null) {
-          this.hasAnswer = true;
-          this.quizService.checkIfAnsweredCorrectly();
-          this.timerService.elapsedTime = Math.ceil(this.timePerQuestion - this.timeleft);
-          this.timerService.calculateTotalElapsedTime(this.elapsedTimes);
-        }
-
-        if (this.timeleft === 0) {
-          if (!this.quizService.isFinalQuestion()) {
-            // show answer(s) and have a quiz delay here
-            this.quizService.nextQuestion();
+          // check if question has been answered (not equal to null)
+          if (this.answer !== null) {
+            this.hasAnswer = true;
+            this.quizService.checkIfAnsweredCorrectly();
+            this.timerService.elapsedTime = Math.ceil(this.timePerQuestion - this.timeleft);
+            this.timerService.calculateTotalElapsedTime(this.elapsedTimes);
           }
-          if (this.quizService.isFinalQuestion() && this.hasAnswer === true) {
-            this.quizService.calculateQuizPercentage();
-            this.quizService.navigateToResults();
-            this.quizIsOver = true;
-          }
-          // clearInterval(this.quizInterval);
-        }
 
-        // disable the next button until an option has been selected (doesn't seem to be disabled ATM)
-        this.disabled = this.answer === null;
-      }, 1000); 
+          if (this.timeleft === 0) {
+            if (!this.quizService.isFinalQuestion()) {
+              // show answer(s) and have a quiz delay here
+              this.quizService.nextQuestion();
+            }
+            if (this.quizService.isFinalQuestion() && this.hasAnswer === true) {
+              this.quizService.calculateQuizPercentage();
+              this.quizService.navigateToResults();
+              this.quizIsOver = true;
+            }
+            // clearInterval(this.quizInterval);
+          }
+
+          // disable the next button until an option has been selected (doesn't seem to be disabled ATM)
+          this.disabled = this.answer === null;
+        }
+      }, 1000);
       // clearInterval(this.quizInterval);
     }
   }
