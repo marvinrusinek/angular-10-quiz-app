781f3ba9fe7f1186e57617b4bb7dfe3f64a7d141
marvinrusinek <marvinrusinek@gmail.com>
2020-03-25 09:52:13 -0400

    1 file modified

--- src/app/containers/scoreboard/timer/timer.component.ts
+++ src/app/containers/scoreboard/timer/timer.component.ts
@@ -20,7 +20,6 @@ export class TimerComponent implements OnInit {
   
   elapsedTime: number;
   elapsedTimes: [];
-  quizInterval: number;
   timeLeft: number;
   timePerQuestion = 20;
   quizIsOver: boolean;
@@ -37,39 +36,37 @@ export class TimerComponent implements OnInit {
 
   // countdown clock
   timer() {
-    if (this.quizService.isThereAnotherQuestion()) {
-      this.quizInterval = setInterval(() => {
-        this.hasAnswer = false;
-
+    if (this.quizService.isThereAnotherQuestion()) {      
+      setInterval(() => {
         if (this.timeLeft > 0) {
           this.timeLeft--;
           console.log("timeLeft: " + this.timeLeft);
-
-          // check if question has been answered (not equal to null)
-          if (this.answer !== null) {
-            this.hasAnswer = true;
-            this.quizService.checkIfAnsweredCorrectly();
-            this.timerService.elapsedTime = Math.ceil(this.timePerQuestion - this.timeLeft);
-            this.timerService.calculateTotalElapsedTime(this.elapsedTimes);
-          }
-
-          if (this.timeLeft === 0) {
-            if (!this.quizService.isFinalQuestion()) {
-              // show answer(s) and have a quiz delay here
-              this.quizService.nextQuestion();
-            }
-            if (this.quizService.isFinalQuestion() && this.hasAnswer === true) {
-              this.quizService.calculateQuizPercentage();
-              this.quizService.navigateToResults();
-              this.quizIsOver = true;
-            }
-            clearInterval(this.quizInterval);
-          }
-
-          // disable the next button until an option has been selected (doesn't seem to be disabled ATM)
-          this.disabled = this.answer === null;
         }
-      }, 1000);
+
+        // check if question has been answered (not equal to null)
+        if (this.answer !== null) {
+          this.hasAnswer = true;
+          this.quizService.checkIfAnsweredCorrectly();
+          this.timerService.elapsedTime = Math.ceil(this.timePerQuestion - this.timeLeft);
+          this.timerService.calculateTotalElapsedTime(this.elapsedTimes);
+        }
+
+        if (this.timeLeft === 0) {
+          if (!this.quizService.isFinalQuestion()) {
+            // show answer(s) and have a quiz delay here
+            this.quizService.nextQuestion();
+          }
+          if (this.quizService.isFinalQuestion() && this.hasAnswer === true) {
+            this.quizService.calculateQuizPercentage();
+            this.quizService.navigateToResults();
+            this.quizIsOver = true;
+          }
+          clearInterval(this.quizInterval);
+        }
+
+        // disable the next button until an option has been selected (doesn't seem to be disabled ATM)
+        this.disabled = this.answer === null;
+      }, 1000); 
       clearInterval(this.quizInterval);
     }
   }
