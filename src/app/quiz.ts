6bcf4e4014da4afdeb197d0533bcb3ad19edc2cc
marvinrusinek <marvinrusinek@gmail.com>
2020-03-27 16:01:38 -0400

    2 files modified

--- src/app/quiz.ts
+++ src/app/quiz.ts
@@ -18,30 +18,30 @@ export const QUIZ_DATA: Quiz = {
     {
       questionText: 'Which of the following benefit from dependency injection?',
       options: [
-        { text: 'Programming', selected: false },
-        { text: 'Testability', selected: false },
-        { text: 'Software design', selected: false },
-        { text: 'All of the above.', correct: true, selected: false }
+        { text: 'Programming' },
+        { text: 'Testability' },
+        { text: 'Software design' },
+        { text: 'All of the above.', correct: true }
       ],
       explanation: 'DI simplifies both programming and testing as well as being a popular design pattern'
     },
     {
       questionText: 'Which of the following is the first step in setting up dependency injection?',
       options: [
-        { text: 'Require in the component.', selected: false },
-        { text: 'Provide in the module.', selected: false },
-        { text: 'Mark dependency as @Injectable().', correct: true, selected: false },
-        { text: 'Declare an object.', selected: false }
+        { text: 'Require in the component.' },
+        { text: 'Provide in the module.' },
+        { text: 'Mark dependency as @Injectable().', correct: true },
+        { text: 'Declare an object.' }
       ],
       explanation: 'the first step is marking the class as @Injectable()'
     },
     {
       questionText: 'In which of the following does dependency injection occur?',
       options: [
-        { text: '@Injectable()', selected: false },
-        { text: 'constructor', correct: true, selected: false },
-        { text: 'function', selected: false },
-        { text: 'NgModule', selected: false }
+        { text: '@Injectable()' },
+        { text: 'constructor', correct: true },
+        { text: 'function' },
+        { text: 'NgModule' }
       ],
       explanation: 'object instantiations are taken care of by the constructor in Angular'
     }
