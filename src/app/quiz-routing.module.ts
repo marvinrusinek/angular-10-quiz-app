6c3e8fb27dd7701cea592e24f1e34d4f4b954beb
marvinrusinek <marvinrusinek@gmail.com>
2020-04-06 16:12:15 -0400

    update quiz-routing - remove pathmatch and use forChild

--- src/app/quiz-routing.module.ts
+++ src/app/quiz-routing.module.ts
@@ -6,15 +6,15 @@ import { DependencyInjectionQuizComponent } from './containers/dependency-inject
 import { ResultsComponent } from './containers/results/results.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'intro', pathMatch: 'full' },
-  { path: 'intro', component: IntroductionComponent, pathMatch: 'full' },
-  { path: 'question', component: DependencyInjectionQuizComponent, pathMatch: 'full' },
-  { path: 'question/:questionIndex', component: DependencyInjectionQuizComponent, pathMatch: 'full' },
-  { path: 'results', component: ResultsComponent, pathMatch: 'full' }
+  { path: '', redirectTo: 'intro' },
+  { path: 'intro', component: IntroductionComponent },
+  { path: 'question', component: DependencyInjectionQuizComponent },
+  { path: 'question/:questionIndex', component: DependencyInjectionQuizComponent },
+  { path: 'results', component: ResultsComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class QuizRoutingModule {}
