782e95a5e74240b30e7c3c64a400dc88ae138ee2
marvinrusinek <marvinrusinek@gmail.com>
2020-04-14 13:16:30 -0400

    edit router animation times

--- src/app/router/route-animations.ts
+++ src/app/router/route-animations.ts
@@ -26,10 +26,10 @@ export const slideInAnimation =
       query(':leave', animateChild()),
       group([
         query(':leave', [
-          animate('1000ms ease-out', style({ left: '100%'}))
+          animate('2000ms ease-out', style({ left: '100%'}))
         ]),
         query(':enter', [
-          animate('1000ms ease-out', style({ left: '0%'}))
+          animate('2000ms ease-out', style({ left: '0%'}))
         ])
       ]),
       query(':enter', animateChild()),
@@ -50,10 +50,10 @@ export const slideInAnimation =
       query(':leave', animateChild()),
       group([
         query(':leave', [
-          animate('1000ms ease-out', style({ left: '100%'}))
+          animate('2000ms ease-out', style({ left: '100%'}))
         ]),
         query(':enter', [
-          animate('1200ms ease-out', style({ left: '0%'}))
+          animate('2500ms ease-out', style({ left: '0%'}))
         ])
       ]),
       query(':enter', animateChild()),
