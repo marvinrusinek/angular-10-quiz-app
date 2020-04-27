50be180f23281c6924fd890ca97cf7c260d78e85
marvinrusinek <marvinrusinek@gmail.com>
2020-04-27 19:50:49 -0400

    1 file modified

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -78,6 +78,7 @@ export class QuizQuestionComponent implements OnInit, OnChanges {
     ) {
       this.quizService.correctAnswers = [...this.quizService.correctAnswers, optionIndex + 1];
       this.timerService.resetTimer();
+      this.answer = null;
 
       if (this.quizService.currentQuestionIndex < this.quizService.totalQuestions) {
         this.quizService.nextQuestion();
