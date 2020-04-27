0e91240c9f712b1d2c6fb8b256a3f659f214966f
marvinrusinek <marvinrusinek@gmail.com>
2020-04-27 16:45:13 -0400

    add quizDelay function to TimerService

--- src/app/shared/services/timer.service.ts
+++ src/app/shared/services/timer.service.ts
@@ -19,6 +19,16 @@ export class TimerService {
     this.timeLeft.next(this.timePerQuestion - this.elapsedTime);
   }
 
+  quizDelay(milliseconds) {
+    const start = new Date().getTime();
+    let counter = 0;
+    let end = 0;
+
+    while (counter < milliseconds) {
+      end = new Date().getTime();
+      counter = end - start;
+    }
+
   addElapsedTimeToElapsedTimes(elapsedTime) {
     this.elapsedTimes = [...this.elapsedTimes, elapsedTime];
     this.completionTime = this.calculateTotalElapsedTime(this.elapsedTimes);
