d4c5c3a5c8549ed4a8fd0f9e4c12e1102ec0a794
marvinrusinek <marvinrusinek@gmail.com>
2020-05-24 15:26:44 -0400

    1 file modified

--- src/app/shared/services/quiz.service.ts
+++ src/app/shared/services/quiz.service.ts
@@ -119,20 +119,20 @@ export class QuizService {
   }
 
   previousQuestion(): void {
-    this.router.navigate(['/quiz/question', this.currentQuestionIndex - 1]);
+    this.router.navigate(['/question', this.currentQuestionIndex - 1]);
     this.resetAll();
   }
 
   nextQuestion(): void {
     this.currentQuestionIndex++;
     let questionIndex = this.currentQuestionIndex;
-    this.router.navigate(['/quiz/question', questionIndex]);
+    this.router.navigate(['/question', questionIndex]);
     this.timerService.resetTimer();
     this.resetAll();
   }
 
   navigateToResults(): void {
-    this.router.navigate(['/quiz/results']);
+    this.router.navigate(['/results']);
   }
 
   sendCorrectCountToResults(value: number): void {
