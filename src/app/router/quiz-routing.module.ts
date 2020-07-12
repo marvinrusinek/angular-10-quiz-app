c35eb7d1f72e9e7677f343d83ed1663e861369a6
marvinrusinek <marvinrusinek@gmail.com>
2020-07-12 19:11:37 -0400

    1 file modified

--- src/app/router/quiz-routing.module.ts
+++ src/app/router/quiz-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { IntroductionComponent } from '../containers/introduction/introduction.component';
 import { QuizComponent } from '../containers/quiz/quiz.component';
+import { QuizSelectionComponent } from '../containers/quiz-selection/quiz-selection.component';
 import { ResultsComponent } from '../containers/results/results.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'intro', pathMatch: 'full' },
+  { path: '', redirectTo: 'select', pathMatch: 'full' },
+  { path: 'select', component: QuizSelectionComponent, pathMatch: 'full' },
   { path: 'intro', component: IntroductionComponent, pathMatch: 'full' },
+  { path: 'intro/:id', component: IntroductionComponent, pathMatch: 'full' },
   { path: 'question', component: QuizComponent, pathMatch: 'full' },
   { path: 'question/:questionIndex', component: QuizComponent, pathMatch: 'full' },
   { path: 'results', component: ResultsComponent, pathMatch: 'full' }
