11c1819cfe54e6476dc1239797a8cc28c6a6f3d8
marvinrusinek <marvinrusinek@gmail.com>
2020-07-12 18:53:19 -0400

    1 file modified

--- src/app/router/quiz-routing.module.ts
+++ src/app/router/quiz-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { IntroductionComponent } from '../containers/introduction/introduction.component';
-import { DependencyInjectionQuizComponent } from '../containers/dependency-injection-quiz/dependency-injection-quiz.component';
+import { DependencyInjectionQuizComponent } from '../containers/quiz/quiz.component';
 import { ResultsComponent } from '../containers/results/results.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'intro', pathMatch: 'full' },
   { path: 'intro', component: IntroductionComponent, pathMatch: 'full' },
-  { path: 'question', component: DependencyInjectionQuizComponent, pathMatch: 'full' },
-  { path: 'question/:questionIndex', component: DependencyInjectionQuizComponent, pathMatch: 'full' },
+  { path: 'question', component: QuizComponent, pathMatch: 'full' },
+  { path: 'question/:questionIndex', component: QuizComponent, pathMatch: 'full' },
   { path: 'results', component: ResultsComponent, pathMatch: 'full' }
 ];
 
