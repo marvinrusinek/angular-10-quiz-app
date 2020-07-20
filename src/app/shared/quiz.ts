e05cf7f55531f9049ca970c7754ca7b1115e7872
marvinrusinek <marvinrusinek@gmail.com>
2020-07-20 14:07:06 -0400

    3 files modified

--- src/app/shared/quiz.ts
+++ src/app/shared/quiz.ts
@@ -275,7 +275,7 @@ export const QUIZ_DATA: Quiz[] = [
       {
         questionText: 'How can one component render another one?',
         options: [
-          { text: 'using HTML element that matches selector', correct: true },
+          { text: 'using an HTML element that matches selector of other component', correct: true },
           { text: 'using data binding' },
           { text: 'using properties' },
           { text: 'passing components via functions' }
