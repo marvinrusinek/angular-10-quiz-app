688570a322075f9103562ff6bd1336cf1ad07d86
marvinrusinek <marvinrusinek@gmail.com>
2020-10-02 10:19:25 -0400

    1 file modified

--- src/app/containers/results/results.component.ts
+++ src/app/containers/results/results.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Observable, Subject } from 'rxjs';
-import { map, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 import { QUIZ_DATA } from '../../shared/quiz';
 import { Quiz } from '../../shared/models/Quiz.model';
@@ -15,15 +15,10 @@ import { QuizService } from '../../shared/services/quiz.service';
 })
 export class ResultsComponent implements OnInit, OnDestroy {
   quizData: Quiz[] = QUIZ_DATA;
-  quizzes$: Observable<Quiz[]>;
-  quizName$: Observable<string>;
   quizId: string;
   indexOfQuizId: number;
-  elapsedMinutes: number;
-  elapsedSeconds: number;
-  checkedShuffle: boolean;
   unsubscribe$ = new Subject<void>();
-// previousUserAnswers: any[] = [];
+  // previousUserAnswers: any[] = [];
 
   constructor(
     private quizService: QuizService,
@@ -38,9 +33,6 @@ export class ResultsComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    this.quizzes$ = this.quizService.getQuizzes();
-    this.quizName$ = this.activatedRoute.url.pipe(map(segments => segments[1] + ''));
-    this.checkedShuffle = this.quizService.checkedShuffle;
     // this.previousUserAnswers = this.quizService.userAnswers;
   }
 
@@ -54,7 +46,7 @@ export class ResultsComponent implements OnInit, OnDestroy {
     this.quizService.resetQuestions();
     this.quizId = '';
     this.indexOfQuizId = 0;
-    this.router.navigate(['/select/']).then();
+    this.router.navigate(['/quiz/select/']).then();
   }
 
   /* private sendPreviousUserAnswersToQuizService(): void {
