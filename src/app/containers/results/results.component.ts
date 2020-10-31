631251cda2c33de59faeede0e6cb3d75308d51c4
marvinrusinek <marvinrusinek@gmail.com>
2020-10-30 20:10:41 -0400

    2 files modified

--- src/app/containers/results/results.component.ts
+++ src/app/containers/results/results.component.ts
@@ -1,16 +1,21 @@
-import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnDestroy,
+  OnInit
+} from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
 
-import { QUIZ_DATA } from '../../shared/quiz';
-import { Quiz } from '../../shared/models/Quiz.model';
-import { QuizService } from '../../shared/services/quiz.service';
+import { QUIZ_DATA } from "../../shared/quiz";
+import { Quiz } from "../../shared/models/Quiz.model";
+import { QuizService } from "../../shared/services/quiz.service";
 
 @Component({
-  selector: 'codelab-quiz-results',
-  templateUrl: './results.component.html',
-  styleUrls: ['./results.component.scss'],
+  selector: "codelab-quiz-results",
+  templateUrl: "./results.component.html",
+  styleUrls: ["./results.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ResultsComponent implements OnInit, OnDestroy {
@@ -24,15 +29,20 @@ export class ResultsComponent implements OnInit, OnDestroy {
     private activatedRoute: ActivatedRoute,
     private router: Router
   ) {
-    this.quizService.setPreviousUserAnswersText(this.quizService.questions, this.quizService.userAnswers);
+    this.quizService.setPreviousUserAnswersText(
+      this.quizService.questions,
+      this.quizService.userAnswers
+    );
   }
 
   ngOnInit(): void {
     this.activatedRoute.paramMap
       .pipe(takeUntil(this.unsubscribe$))
-        .subscribe(params => this.quizId = params.get('quizId'));
+      .subscribe(params => (this.quizId = params.get("quizId")));
     this.sendCompletedQuizIdToQuizService(this.quizId);
-    this.indexOfQuizId = this.quizData.findIndex(elem => elem.quizId === this.quizId);
+    this.indexOfQuizId = this.quizData.findIndex(
+      elem => elem.quizId === this.quizId
+    );
   }
 
   ngOnDestroy(): void {
@@ -43,9 +53,9 @@ export class ResultsComponent implements OnInit, OnDestroy {
   selectQuiz(): void {
     this.quizService.resetAll();
     this.quizService.resetQuestions();
-    this.quizId = '';
+    this.quizId = "";
     this.indexOfQuizId = 0;
-    this.router.navigate(['/select/']).then();
+    this.router.navigate(["/select/"]).then();
   }
 
   private sendCompletedQuizIdToQuizService(quizId: string): void {
