ca0c8d3b9c9de4fd28198eb994a904a8249dc546
marvinrusinek <marvinrusinek@gmail.com>
2020-11-01 18:57:09 -0500

    1 file modified

--- src/app/containers/scoreboard/time/time.component.ts
+++ src/app/containers/scoreboard/time/time.component.ts
@@ -21,7 +21,7 @@ import { TimerService } from "../../../shared/services/timer.service";
 export class TimeComponent implements OnChanges {
   @Input() selectedAnswer: number;
   answer: number;
-  timePerQuestion = 20;
+  timePerQuestion = 0;
   timeLeft$: Observable<number>;
   start$: Observable<number>;
   reset$: Observable<number>;
