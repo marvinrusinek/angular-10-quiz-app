2a99b8eb942ef056304434a4413757381eca14ed
marvinrusinek <marvinrusinek@gmail.com>
2021-06-19 11:44:52 -0400

    2 files modified

--- src/app/containers/quiz-selection/quiz-selection.component.ts
+++ src/app/containers/quiz-selection/quiz-selection.component.ts
@@ -8,34 +8,29 @@ import { QuizService } from "../../shared/services/quiz.service";
 type AnimationState = "animationStarted" | "none";
 
 @Component({
-  selector: "codelab-quiz-selection",
-  templateUrl: "./quiz-selection.component.html",
-  styleUrls: ["./quiz-selection.component.scss"],
+  selector: 'codelab-quiz-selection',
+  templateUrl: './quiz-selection.component.html',
+  styleUrls: ['./quiz-selection.component.scss'],
   animations: [SlideLeftToRightAnimation.slideLeftToRight],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class QuizSelectionComponent implements OnInit {
   quizzes$: Observable<Quiz[]>;
   currentQuestionIndex: number;
-  selectionParams: Object;
-  animationState$ = new BehaviorSubject<AnimationState>("none");
-  unsubscribe$ = new Subject<void>();
-  breakpoint: number;
+  selectionParams: Object = {};
+  animationState$ = new BehaviorSubject<AnimationState>('none');
+  imagePath = "assets/images/milestones/";
 
-  constructor(private quizService: QuizService) {}
+  constructor(private quizService: QuizService) { }
 
   ngOnInit(): void {
     this.quizzes$ = this.quizService.getQuizzes();
     this.currentQuestionIndex = this.quizService.currentQuestionIndex;
     this.selectionParams = this.quizService.returnQuizSelectionParams();
-    this.breakpoint = window.innerWidth <= 440 ? 1 : 3;
   }
 
   animationDoneHandler(): void {
-    this.animationState$.next("none");
-  }
-
-  onResize(event) {
-    this.breakpoint = event.target.innerWidth <= 440 ? 1 : 3;
+    this.animationState$.next('none');
   }
 }
+
