adac2e2b81f5d918ba6feee4da9a5fcab0d4500a
marvinrusinek <marvinrusinek@gmail.com>
2021-06-19 12:10:48 -0400

    2 files modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,41 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { HttpClientModule } from "@angular/common/http";
-import { QuizRoutingModule } from "./router/quiz-routing.module";
-import { ReactiveFormsModule } from "@angular/forms";
-import { MatCardModule } from "@angular/material/card";
-import { MatRadioModule } from "@angular/material/radio";
-import { MatCheckboxModule } from "@angular/material/checkbox";
-import { MatIconModule } from "@angular/material/icon";
-import { MatExpansionModule } from "@angular/material/expansion";
-import { MatButtonModule } from "@angular/material/button";
-import { MatTooltipModule } from "@angular/material/tooltip";
-import { MatGridListModule } from "@angular/material/grid-list";
-import { MatMenuModule } from "@angular/material/menu";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
-import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { QuizRoutingModule } from './router/quiz-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
-import { AppComponent } from "./app.component";
-import { IntroductionComponent } from "./containers/introduction/introduction.component";
-import { QuizQuestionComponent } from "./components/question/question.component";
-import { QuizComponent } from "./containers/quiz/quiz.component";
-import { QuizSelectionComponent } from "./containers/quiz-selection/quiz-selection.component";
-import { ResultsComponent } from "./containers/results/results.component";
-import { ScoreboardComponent } from "./containers/scoreboard/scoreboard.component";
-import { ScoreComponent } from "./containers/scoreboard/score/score.component";
-import { TimeComponent } from "./containers/scoreboard/time/time.component";
-import { QuizService } from "./shared/services/quiz.service";
-import { TimerService } from "./shared/services/timer.service";
-import { JoinPipe } from "./pipes/join.pipe";
-import { MultipleAnswerComponent } from "./components/question/multiple-answer/multiple-answer.component";
-import { SingleAnswerComponent } from "./components/question/single-answer/single-answer.component";
-import { AccordionComponent } from "./containers/results/accordion/accordion.component";
-import { ReturnComponent } from "./containers/results/return/return.component";
-import { StatisticsComponent } from "./containers/results/statistics/statistics.component";
-import { SummaryReportComponent } from "./containers/results/summary-report/summary-report.component";
-import { ChallengeComponent } from "./containers/results/challenge/challenge.component";
-import { GridColsDirective } from "./grid-cols.directive";
-import { FlexLayoutModule } from "@angular/flex-layout";
+import { AppComponent } from './app.component';
+import { IntroductionComponent } from './containers/introduction/introduction.component';
+import { QuizQuestionComponent } from './components/question/question.component';
+import { QuizComponent } from './containers/quiz/quiz.component';
+import { QuizSelectionComponent } from './containers/quiz-selection/quiz-selection.component';
+import { ResultsComponent } from './containers/results/results.component';
+import { ScoreboardComponent } from './containers/scoreboard/scoreboard.component';
+import { ScoreComponent } from './containers/scoreboard/score/score.component';
+import { TimeComponent } from './containers/scoreboard/time/time.component';
+import { QuizService } from './shared/services/quiz.service';
+import { TimerService } from './shared/services/timer.service';
+import { JoinPipe } from './pipes/join.pipe';
+import { MultipleAnswerComponent } from './components/question/multiple-answer/multiple-answer.component';
+import { SingleAnswerComponent } from './components/question/single-answer/single-answer.component';
+import { AccordionComponent } from './containers/results/accordion/accordion.component';
+import { ReturnComponent } from './containers/results/return/return.component';
+import { StatisticsComponent } from './containers/results/statistics/statistics.component';
+import { SummaryReportComponent } from './containers/results/summary-report/summary-report.component';
+import { ChallengeComponent } from './containers/results/challenge/challenge.component';
 
 @NgModule({
   declarations: [
@@ -57,8 +55,7 @@ import { FlexLayoutModule } from "@angular/flex-layout";
     ReturnComponent,
     StatisticsComponent,
     SummaryReportComponent,
-    ChallengeComponent,
-    GridColsDirective
+    ChallengeComponent
   ],
   imports: [
     CommonModule,
@@ -77,8 +74,7 @@ import { FlexLayoutModule } from "@angular/flex-layout";
     MatMenuModule,
     MatToolbarModule,
     NgbModule,
-    FontAwesomeModule,
-    FlexLayoutModule
+    FontAwesomeModule
   ],
   exports: [MatExpansionModule],
   bootstrap: [AppComponent],
