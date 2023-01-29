4974160110c18808905f4bd03ff1d67d45b1d75f
marvinrusinek <marvinrusinek@gmail.com>
2023-01-29 14:34:54 -0500

    2 files modified

--- src/app/containers/results/statistics/statistics.component.ts
+++ src/app/containers/results/statistics/statistics.component.ts
@@ -1,25 +1,25 @@
-import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
-import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-import { Quiz } from "../../../shared/models/Quiz.model";
-import { QuizMetadata } from "../../../shared/models/QuizMetadata.model";
-import { Resource } from "../../../shared/models/Resource.model";
-import { QuizService } from "../../../shared/services/quiz.service";
-import { TimerService } from "../../../shared/services/timer.service";
+import { Quiz } from '../../../shared/models/Quiz.model';
+import { QuizMetadata } from '../../../shared/models/QuizMetadata.model';
+import { Resource } from '../../../shared/models/Resource.model';
+import { QuizService } from '../../../shared/services/quiz.service';
+import { TimerService } from '../../../shared/services/timer.service';
 
 enum Status {
-  Started = "Started",
-  Continue = "Continue",
-  Completed = "Completed"
+  Started = 'Started',
+  Continue = 'Continue',
+  Completed = 'Completed',
 }
 
 @Component({
-  selector: "codelab-results-statistics",
-  templateUrl: "./statistics.component.html",
-  styleUrls: ["./statistics.component.scss"],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'codelab-results-statistics',
+  templateUrl: './statistics.component.html',
+  styleUrls: ['./statistics.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StatisticsComponent implements OnInit {
   quizzes$: Observable<Quiz[]>;
@@ -32,19 +32,20 @@ export class StatisticsComponent implements OnInit {
     percentage: this.calculatePercentageOfCorrectlyAnsweredQuestions(),
     completionTime: this.timerService.calculateTotalElapsedTime(
       this.timerService.elapsedTimes
-    )
+    ),
   };
   resources: Resource[];
   status: Status;
   elapsedMinutes: number;
   elapsedSeconds: number;
+  percentage: number;
 
   CONGRATULATIONS =
-    "https://raw.githubusercontent.com/marvinrusinek/angular-9-quiz-app/master/src/assets/images/congratulations.jpg";
+    'https://raw.githubusercontent.com/marvinrusinek/angular-9-quiz-app/master/src/assets/images/congratulations.jpg';
   NOT_BAD =
-    "https://raw.githubusercontent.com/marvinrusinek/angular-9-quiz-app/master/src/assets/images/not-bad.jpg";
+    'https://raw.githubusercontent.com/marvinrusinek/angular-9-quiz-app/master/src/assets/images/not-bad.jpg';
   TRY_AGAIN =
-    "https://raw.githubusercontent.com/marvinrusinek/angular-9-quiz-app/master/src/assets/images/try-again.jpeg";
+    'https://raw.githubusercontent.com/marvinrusinek/angular-9-quiz-app/master/src/assets/images/try-again.jpeg';
 
   constructor(
     private quizService: QuizService,
@@ -55,11 +56,12 @@ export class StatisticsComponent implements OnInit {
   ngOnInit(): void {
     this.quizzes$ = this.quizService.getQuizzes();
     this.quizName$ = this.activatedRoute.url.pipe(
-      map(segments => segments[1].toString())
+      map((segments) => segments[1].toString())
     );
     this.quizId = this.quizService.quizId;
     this.resources = this.quizService.resources;
     this.status = Status.Completed;
+    this.percentage = this.quizMetadata.percentage;
     this.calculateElapsedTime();
     this.sendQuizStatusToQuizService();
   }
