5607003e23c82202e19c39feab40f5e896d29a72
marvinrusinek <marvinrusinek@gmail.com>
2023-01-30 11:56:35 -0500

    1 file modified

--- src/app/shared/services/stopwatch.service.ts
+++ src/app/shared/services/stopwatch.service.ts
@@ -1,5 +1,12 @@
 import { Injectable, Input } from '@angular/core';
-import { concat, BehaviorSubject, interval, Observable, timer } from 'rxjs';
+import {
+  concat,
+  BehaviorSubject,
+  interval,
+  Observable,
+  pipe,
+  timer,
+} from 'rxjs';
 import {
   first,
   map,
