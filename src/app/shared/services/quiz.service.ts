1f18c0e932b4c30c1ab6b6f814db4d1fde0ccfed
marvinrusinek <marvinrusinek@gmail.com>
2023-01-30 17:44:18 -0500

    1 file modified

--- src/app/shared/services/quiz.service.ts
+++ src/app/shared/services/quiz.service.ts
@@ -198,10 +198,10 @@ export class QuizService implements OnDestroy {
         this.correctMessage = 'ALL are correct!';
         break;
       default:
-        this.correctOptions = correctAnswers.map((ans) => `Option ${ans}`);
+        this.correctOptions = correctAnswers.map((ans) => `${ans}`);
         this.correctMessage = `The correct answers are ${this.correctOptions
           .slice(0, -1)
-          .join(', ')} and ${this.correctOptions.slice(-1)}.`;
+          .join(' and ')} and ${this.correctOptions.slice(-1)}.`;
         break;
     }
   }
