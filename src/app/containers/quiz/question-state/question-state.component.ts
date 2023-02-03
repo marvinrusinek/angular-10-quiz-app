a5560947f9a0cfe21d2ed223ab6b75c9a7e3520b
marvinrusinek <marvinrusinek@gmail.com>
2023-02-03 18:26:05 -0500

    1 file modified

--- src/app/containers/quiz/question-state/question-state.component.ts
+++ src/app/containers/quiz/question-state/question-state.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-question-state',
   templateUrl: './question-state.component.html',
   styleUrls: ['./question-state.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class QuestionStateComponent {
   @Input() isAnswered: boolean;
