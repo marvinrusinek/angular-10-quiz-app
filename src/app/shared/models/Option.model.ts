2e7eeedc56f930bf47af95bff5eb648c9f2d2639
marvinrusinek <marvinrusinek@gmail.com>
2023-02-08 11:31:31 -0500

    1 file modified

--- src/app/shared/models/Option.model.ts
+++ src/app/shared/models/Option.model.ts
@@ -1,6 +1,7 @@
 export interface Option {
   text: string;
-  correct?: boolean;
+  // correct?: boolean;
+  correct: boolean = false;
   selected?: boolean;
   styleClass?: string;
   value: number;
