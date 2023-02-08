da3148b916403202237ca1a2befeaea99b4216ab
marvinrusinek <marvinrusinek@gmail.com>
2023-02-08 11:42:19 -0500

    1 file modified

--- src/app/components/question/multiple-answer/multiple-answer.component.ts
+++ src/app/components/question/multiple-answer/multiple-answer.component.ts
@@ -23,7 +23,7 @@ export class MultipleAnswerComponent extends QuizQuestionComponent {
   @Input() correctMessage: string;
   @Input() selected: string;
   @Output() answer = new EventEmitter<number>();
-  selectedOption: Option = { correct: false } as Option;
+  selectedOption: Option = { text: '', correct: false, value: 0 } as Option;
 
   /* onOptionSelected(selectedOption: Option) {
     if (selectedOption && selectedOption.hasOwnProperty('correct')) {
