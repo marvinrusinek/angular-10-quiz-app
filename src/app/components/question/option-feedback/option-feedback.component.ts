fb859178c7e31641bdb86992c5781ef0d202df35
marvinrusinek <marvinrusinek@gmail.com>
2023-02-08 21:10:33 -0500

    5 files modified

--- src/app/components/question/option-feedback/option-feedback.component.ts
+++ src/app/components/question/option-feedback/option-feedback.component.ts
@@ -3,7 +3,23 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 @Component({
   selector: 'codelab-quiz-option-feedback',
   templateUrl: './option-feedback.component.html',
-  styleUrls: ['./option-feedback.component.scss'],
+  styles: [`
+    .material-icons {
+      font-family: 'Material Icons';
+      font-weight: normal;
+      font-style: normal;
+      font-size: 24px;
+      line-height: 1;
+      letter-spacing: normal;
+      text-transform: none;
+      display: inline-block;
+      white-space: nowrap;
+      word-wrap: normal;
+      direction: ltr;
+      -webkit-font-feature-settings: 'liga';
+      -webkit-font-smoothing: antialiased;
+    }
+  `],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class OptionFeedbackComponent {
