18100541a5c4353cc2bc4b51580441715fe31472
marvinrusinek <marvinrusinek@gmail.com>
2023-02-26 15:17:02 -0500

    1 file modified

--- src/app/containers/quiz/quiz.component.ts
+++ src/app/containers/quiz/quiz.component.ts
@@ -110,34 +110,16 @@ export class QuizComponent implements OnInit, OnDestroy {
 
   async ngOnInit(): Promise<void> {
     this.quizId = this.activatedRoute.snapshot.paramMap.get('quizId');
-    // this.questions$ = this.quizService.getQuestionsForQuiz(this.quizId);
-    this.questions$.subscribe((questions) => {
-      console.log('Questions:', questions);
-      if (questions.length > 0) {
-        this.questions = questions;
-        this.currentQuestion = this.questions[0];
-      }
-    });
-
-    /* this.quizId = this.activatedRoute.snapshot.paramMap.get('quizId');
     this.questions$ = this.quizService.getQuestionsForQuiz(this.quizId);
-    this.questions$.subscribe((questions) => {
-      if (questions.length > 0) {
-        this.questions = questions;
-        this.currentQuestion = this.questions[0];
-      }
-    }); */
-  
-    /* this.questions$ = this.quizService.getQuestionsForQuiz(this.quizId);
-  
-    this.questions$.subscribe((questions) => {
-      console.log('Questions:', questions);
-      if (questions.length > 0) {
-        this.questions = questions;
-        this.currentQuestion = this.questions[0];
-      }
-    }); */
-  }
+    if (this.questions$) {
+      this.questions$.subscribe((questions) => {
+        if ((questions?.length || 0) > 0) {
+          this.questions = questions;
+          this.currentQuestion = this.questions[0];
+        }
+      });
+    }
+  } 
 
   loadQuestion(index: number) {
     const question = this.questions[index];
