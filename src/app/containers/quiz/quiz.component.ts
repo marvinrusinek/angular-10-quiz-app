a568213865f949b1f5b5bf3c376f6f1d8868d379
marvinrusinek <marvinrusinek@gmail.com>
2023-03-02 20:06:50 -0500

    1 file modified

--- src/app/containers/quiz/quiz.component.ts
+++ src/app/containers/quiz/quiz.component.ts
@@ -111,7 +111,7 @@ export class QuizComponent implements OnInit, OnDestroy {
 
   async ngOnInit(): Promise<void> {
     console.log('quizId:', this.quizId);
-    this.quizService.getQuiz(this.quizId).subscribe(quiz => {
+    this.quizService.getQuizById(this.quizId).subscribe(quiz => {
       console.log('quiz:', quiz);
       if (!quiz) {
         console.error('Selected quiz is null or undefined');
