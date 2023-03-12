387e6319bfaa2515c9a39709bae59a73b390f9f7
marvinrusinek <marvinrusinek@gmail.com>
2023-03-12 12:10:03 -0400

    1 file modified

--- src/app/containers/introduction/introduction.component.ts
+++ src/app/containers/introduction/introduction.component.ts
@@ -79,21 +79,21 @@ export class IntroductionComponent implements OnInit, OnDestroy {
     if ($event.checked === true) {
       this.quizService.setChecked($event.checked);
     }
-  }
+  }  
 
-  onStartQuiz() {
-    if (!this.selectedQuiz || !this.selectedQuizId) {
+  onStartQuiz(quizId: string) {
+    if (!quizId || !this.selectedQuiz || !this.selectedQuizId) {
       console.error('No quiz selected');
       return;
     }
-  
+
     if (this.selectedQuizId) {
-      this.quizDataService.getQuizById(this.selectedQuizId).subscribe((quiz) => {
+      this.quizDataService.getQuizById(quizId).subscribe((quiz) => {
         this.quizDataService.setSelectedQuiz(quiz);
-        this.router.navigate(['/question/', this.quizId, 1]);
+        this.router.navigate(['/question/', quizId, 1]);
       });
     } else {
       console.log('Quiz ID is null or undefined');
-    }
-  }  
+    } 
+  }
 }
