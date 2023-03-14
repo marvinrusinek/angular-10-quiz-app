e92ff6f1e9f5e47bcf9f7beac3594e18a14ca114
marvinrusinek <marvinrusinek@gmail.com>
2023-03-14 14:19:37 -0400

    1 file modified

--- src/app/shared/services/quizdata.service.ts
+++ src/app/shared/services/quizdata.service.ts
@@ -152,31 +152,35 @@ export class QuizDataService implements OnInit {
     );
   }
 
-  getQuestionAndOptions(quizId: string, questionIndex: number): Observable<[QuizQuestion, Option[]]> {
+  getQuestionAndOptions(
+    quizId: string,
+    questionIndex: number
+  ): Observable<[QuizQuestion, Option[]]> {
+    console.log('getQuestionAndOptions() function called');
     return this.http.get<Quiz[]>(this.quizUrl).pipe(
       map((quizzes: Quiz[]) => {
-        const quiz = quizzes.find(q => q.quizId === quizId);
+        const quiz = quizzes.find((q) => q.quizId === quizId);
         console.log('quiz:', quiz);
         if (!quiz) {
           throw new Error('Invalid quizId');
         }
-  
+
         if (!quiz.questions || quiz.questions.length === 0) {
           throw new Error('Quiz or questions not found');
         }
-  
+
         const question = quiz.questions[questionIndex];
         console.log('question:', question);
         if (!question) {
           throw new Error('Invalid question index');
         }
-  
+
         const options = question.options;
         console.log('options:', options);
         if (!options) {
           throw new Error('Invalid question options');
         }
-  
+
         return [question, options];
       })
     );
