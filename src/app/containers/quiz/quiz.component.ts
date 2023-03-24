b722599d2da24c86b147e5fd7bac5410a5970c7a
marvinrusinek <marvinrusinek@gmail.com>
2023-03-24 17:39:47 -0400

    1 file modified

--- src/app/containers/quiz/quiz.component.ts
+++ src/app/containers/quiz/quiz.component.ts
@@ -563,15 +563,15 @@ export class QuizComponent implements OnInit, OnDestroy {
     console.log('QuizDataService returned options:::>>', options);
   
     if (question && options && options.length > 0) {
-      this.question = question[0];
-      this.options = options[0];
+      this.question = question;
+      this.options = options;
     } else {
       console.error('Question or options array is null or undefined');
       this.question = null;
       this.options = null;
     }
   }
-    
+      
   async onSubmit(): Promise<void> {
     if (this.form.invalid) {
       return;
