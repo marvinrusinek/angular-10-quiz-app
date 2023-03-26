df214adfdd95434956f59d08533de488a99a0b24
marvinrusinek <marvinrusinek@gmail.com>
2023-03-26 14:54:34 -0400

    1 file modified

--- src/app/shared/services/quizstate.service.ts
+++ src/app/shared/services/quizstate.service.ts
@@ -6,10 +6,16 @@ import { QuizQuestion } from '../../shared/models/QuizQuestion.model';
   providedIn: 'root',
 })
 export class QuizStateService {
+  private currentQuestion: QuizQuestion;
   currentQuestionSubject = new BehaviorSubject<QuizQuestion>(null);
   currentQuestion$ = this.currentQuestionSubject.asObservable();
 
   setCurrentQuestion(question: QuizQuestion): void {
+    this.currentQuestion = question;
     this.currentQuestionSubject.next(question);
+  }
+
+  getCurrentQuestion(): QuizQuestion {
+    return this.currentQuestion;
   }
 }
