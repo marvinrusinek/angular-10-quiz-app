c7c867840e96e90b920c0c3fc402447b06caf0a9
marvinrusinek <marvinrusinek@gmail.com>
2023-03-30 17:21:42 -0400

    2 files modified

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -31,8 +31,8 @@ import {
   tap,
 } from 'rxjs/operators';
 
-import { Option, Quiz, QuizQuestion } from '@codelab/shared/models/*';
-import { QuizService, QuizDataService, QuizStateService, TimerService } from '@codelab/shared/services/*';
+import { Option, Quiz, QuizQuestion } from '@codelab-quiz/shared/models/*';
+import { QuizService, QuizDataService, QuizStateService, TimerService } from '@codelab-quiz/shared/services/*';
 
 /* import { Option } from '../../shared/models/Option.model';
 import { Quiz } from '../../shared/models/Quiz.model';
