a42dd61d23beb85eadbca2bdfb28f454a1340d49
marvinrusinek <marvinrusinek@gmail.com>
2023-03-30 20:04:01 -0400

    2 files added

---
+++ src/app/shared/services/index.ts
@@ -0,0 +1,6 @@
+export { CountdownService } from './countdown.service';
+export { QuizService } from './quiz.service';
+export { QuizDataService } from './quizdata.service';
+export { QuizStateService } from './quizstate.service';
+export { StopwatchService } from './stopwatch.service';
+export { TimerService } from './timer.service';
