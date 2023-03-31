ecb2254e0b4e489b3ce3cfb085b81fb0a07ed5ed
marvinrusinek <marvinrusinek@gmail.com>
2023-03-31 11:29:05 -0400

    1 file modified

--- src/app/shared/services/quiz.service.ts
+++ src/app/shared/services/quiz.service.ts
@@ -258,7 +258,6 @@ export class QuizService implements OnDestroy {
   }
 
   async getCurrentQuestion(): Promise<[QuizQuestion, Option[]]> {
-    console.log('TESTING');
     if (this.isGettingQuestion) {
       return this.currentQuestionPromise;
     }
