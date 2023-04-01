494a8bf4d3f7dac445796501fa53d3715dccc053
marvinrusinek <marvinrusinek@gmail.com>
2023-04-01 19:10:06 -0400

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -92,7 +92,7 @@ import { ChallengeComponent } from './containers/results/challenge/challenge.com
     NgbModule,
     FontAwesomeModule,
   ],
-  exports: [MatExpansionModule, QuizQuestionComponent],
+  exports: [MatExpansionModule, QuizQuestionComponent, MultipleAnswerComponent, SingleAnswerComponent],
   bootstrap: [AppComponent],
   providers: [QuizService, QuizDataService, TimerService, CountdownService, StopwatchService],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
