87fdb8e925c3e416d2bb0da66daffab7387a2e15
marvinrusinek <marvinrusinek@gmail.com>
2023-04-09 21:14:42 -0400

    1 file modified

--- src/app/containers/scoreboard/score/score.component.ts
+++ src/app/containers/scoreboard/score/score.component.ts
@@ -1,6 +1,13 @@
-import { AfterViewInit, ChangeDetectorRef, Component, Input, OnInit, OnDestroy } from '@angular/core';
-import { BehaviorSubject, Observable, of, Subject, Subscription } from 'rxjs';
-import { map, takeUntil, tap } from 'rxjs/operators';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  Input,
+  OnInit,
+  OnDestroy,
+} from '@angular/core';
+import { BehaviorSubject, Observable, Subject, Subscription } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 import { QuizService } from '../../../shared/services/quiz.service';
 
@@ -11,32 +18,31 @@ import { QuizService } from '../../../shared/services/quiz.service';
 })
 export class ScoreComponent implements AfterViewInit, OnInit, OnDestroy {
   @Input() totalQuestions: number;
+  totalQuestions$: Observable<number>;
   score: string;
   numericalScore: string = '';
   percentageScore: string = '';
+  isPercentage: boolean = false;
 
   currentScore: string;
   currentScore$: BehaviorSubject<string> = new BehaviorSubject<string>('');
-  currentScoreSubject: BehaviorSubject<string> = new BehaviorSubject<string>(
-    ''
-  );
+  currentScoreSubject: BehaviorSubject<string> = new BehaviorSubject<string>('');
+
+  correctAnswersCountSubscription: Subscription;
   currentScoreSubscription: Subscription;
   numericalScoreSubscription: Subscription;
+  percentageScoreSubscription: Subscription;
   percentageScore$: BehaviorSubject<string>;
-
+  
   correctAnswersCount: number;
   correctAnswersCount$: BehaviorSubject<number> = new BehaviorSubject<number>(0);
-  correctAnswersCountSubscription: Subscription;
-  percentageScoreSubscription: Subscription;
 
-  totalQuestions: number = 0;
-  totalQuestions$: Observable<number>;
   private unsubscribeTrigger$: Subject<void> = new Subject<void>();
 
-  isPercentage: boolean = false;
-
-  constructor(private quizService: QuizService,
-    private changeDetectorRef: ChangeDetectorRef) {
+  constructor(
+    private quizService: QuizService,
+    private changeDetectorRef: ChangeDetectorRef
+  ) {
     this.currentScoreSubject = new BehaviorSubject<string>('');
     this.currentScore$ = new BehaviorSubject<string>('');
 
@@ -49,11 +55,13 @@ export class ScoreComponent implements AfterViewInit, OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.isPercentage = true;
-    this.correctAnswersCount = 0; 
+    this.correctAnswersCount = 0;
     this.correctAnswersCount$ = this.quizService.correctAnswersCountSubject;
 
     this.currentScoreSubject = new BehaviorSubject<string>('0');
-    this.currentScoreSubject.next(`${this.correctAnswersCount}/${this.totalQuestions}`);
+    this.currentScoreSubject.next(
+      `${this.correctAnswersCount}/${this.totalQuestions}`
+    );
 
     this.quizService.getTotalQuestions().subscribe((totalQuestions: number) => {
       this.totalQuestions = totalQuestions;
@@ -72,7 +80,7 @@ export class ScoreComponent implements AfterViewInit, OnInit, OnDestroy {
         this.isPercentage = true;
         this.changeDetectorRef.detectChanges();
       });
-      
+
     // Subscribe to the currentScore$ Observable
     this.currentScoreSubscription = this.currentScore$
       .pipe(takeUntil(this.unsubscribeTrigger$))
@@ -82,21 +90,14 @@ export class ScoreComponent implements AfterViewInit, OnInit, OnDestroy {
         this.changeDetectorRef.detectChanges();
       });
   }
-  
+
   ngOnDestroy(): void {
     this.unsubscribeTrigger$.next();
     this.unsubscribeTrigger$.complete();
     this.correctAnswersCountSubscription.unsubscribe();
     this.currentScoreSubscription.unsubscribe();
-  }
-
-  setCorrectAnswersCount(count: number): void {
-    this.correctAnswersCount = count;
-    this.correctAnswersCount$.next(count);
-  }
-
-  setTotalQuestions(count: number): void {
-    this.totalQuestions = count;
+    this.numericalScoreSubscription.unsubscribe();
+    this.percentageScoreSubscription.unsubscribe();
   }
 
   displayScore(): void {
@@ -109,11 +110,13 @@ export class ScoreComponent implements AfterViewInit, OnInit, OnDestroy {
       this.currentScore$.next(this.numericalScore);
     }
   }
-  
+
   calculateNumericalScore(totalQuestions: number): string {
-    const numericalScore = `${this.correctAnswersCount}/${totalQuestions}`;
-    this.currentScore$.next(numericalScore);
-    return numericalScore;
+    if (this.totalQuestions !== 0) {
+      const numericalScore = `${this.correctAnswersCount}/${totalQuestions}`;
+      this.currentScore$.next(numericalScore);
+      return numericalScore;
+    }
   }
 
   calculatePercentageScore(): number {
@@ -128,17 +131,19 @@ export class ScoreComponent implements AfterViewInit, OnInit, OnDestroy {
   }
 
   displayPercentageScore(): void {
-    const percentage = (this.correctAnswersCount / this.totalQuestions) * 100;
-    this.percentageScore = percentage.toFixed(0) + '%';
-    this.percentageScore$.next(this.percentageScore);
+    this.percentageScore = `${(
+      (this.correctAnswersCount / this.totalQuestions) *
+      100
+    ).toFixed(2)}%`;
+    this.currentScore$.next(this.percentageScore);
   }
-  
-  toggleScoreDisplay(displayType: string): void {
-    this.isPercentage = (displayType === 'percentage');
+
+  toggleScoreDisplay(scoreType: 'numerical' | 'percentage'): void {
+    this.isPercentage = scoreType === 'percentage';
     if (this.isPercentage) {
-      this.displayPercentageScore(this.totalQuestions);
+      this.displayPercentageScore();
     } else {
       this.displayNumericalScore();
     }
-  }  
+  }
 }
