a2b1c34c45f10282fab81be0bcca401dc4a64377
marvinrusinek <marvinrusinek@gmail.com>
2023-04-12 13:11:01 -0400

    1 file modified

--- src/app/containers/scoreboard/score/score.component.ts
+++ src/app/containers/scoreboard/score/score.component.ts
@@ -6,7 +6,15 @@ import {
   OnInit,
   OnDestroy
 } from '@angular/core';
-import { BehaviorSubject, combineLatest, Observable, of, Subject, Subscription, timer } from 'rxjs';
+import {
+  BehaviorSubject,
+  combineLatest,
+  Observable,
+  of,
+  Subject,
+  Subscription,
+  timer
+} from 'rxjs';
 import { switchMap, takeUntil, tap } from 'rxjs/operators';
 
 import { QuizQuestion } from '../../../shared/models/QuizQuestion.model';
@@ -17,14 +25,14 @@ import { QuizService } from '../../../shared/services/quiz.service';
   templateUrl: './score.component.html',
   styleUrls: ['./score.component.scss'],
 })
-export class ScoreComponent
-  implements AfterViewInit, OnInit, OnDestroy
-{
+export class ScoreComponent implements AfterViewInit, OnInit, OnDestroy {
   @Input() correctAnswersCount: number = 0;
   @Input() totalQuestions: number = 0;
   questions$: Observable<QuizQuestion[]>;
   totalQuestions$: Observable<number>;
-  correctAnswersCount$: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+  correctAnswersCount$: BehaviorSubject<number> = new BehaviorSubject<number>(
+    0
+  );
   score: string;
   numericalScore: string = '0/0';
   percentageScore: string;
@@ -32,11 +40,15 @@ export class ScoreComponent
   percentage: number = 0;
 
   currentScore: string;
-  currentScore$: BehaviorSubject<string> = new BehaviorSubject<string>(this.numericalScore);
-  currentScoreSubject: BehaviorSubject<string> = new BehaviorSubject<string>('');
+  currentScore$: BehaviorSubject<string> = new BehaviorSubject<string>(
+    this.numericalScore
+  );
+  currentScoreSubject: BehaviorSubject<string> = new BehaviorSubject<string>(
+    ''
+  );
 
   correctAnswersCountSubscription: Subscription;
-  currentScoreSubscription: Subscription;
+  // currentScoreSubscription: Subscription;
   numericalScoreSubscription: Subscription;
   percentageScoreSubscription: Subscription;
   percentageScore$: BehaviorSubject<string>;
@@ -60,45 +72,30 @@ export class ScoreComponent
     this.correctAnswersCount = 0;
     this.correctAnswersCount$ = this.quizService.correctAnswersCountSubject;
 
-    this.currentScoreSubject = new BehaviorSubject<string>('0');
-    this.currentScoreSubject.next(
-      `${this.correctAnswersCount}/${this.totalQuestions}`
-    );
-
     this.correctAnswersCountSubscription = this.correctAnswersCount$
       .pipe(takeUntil(this.unsubscribeTrigger$))
       .subscribe((correctAnswersCount: number) => {
         this.correctAnswersCount = correctAnswersCount;
       });
 
-    this.currentScoreSubscription = this.currentScore$
-      .pipe(takeUntil(this.unsubscribeTrigger$))
-      .subscribe((currentScore: string) => {
-        this.currentScore = currentScore;
+    this.quizService
+      .getQuestions()
+      .pipe(
+        tap((questions) => (this.questions$ = of(questions))),
+        switchMap((questions) =>
+          combineLatest([of(questions), this.quizService.getTotalQuestions()])
+        )
+      )
+      .subscribe(([questions, totalQuestions]) => {
+        this.totalQuestions = totalQuestions;
+        this.numericalScore = `${this.correctAnswersCount}/${totalQuestions}`;
+        timer(0).subscribe(() => {
+          this.displayNumericalScore();
+        });
       });
-
-    this.quizService.getQuestions().pipe(
-      tap(questions => this.questions$ = of(questions)),
-      switchMap(questions => combineLatest([of(questions), this.quizService.getTotalQuestions()]))
-    ).subscribe(([questions, totalQuestions]) => {
-      this.totalQuestions = totalQuestions;
-      this.numericalScore = `${this.correctAnswersCount}/${totalQuestions}`;
-      timer(0).subscribe(() => {
-        this.displayNumericalScore();
-      });
-    });    
   }
 
   ngAfterViewInit(): void {
-    // Subscribe to the currentScore$ Observable
-    this.currentScoreSubscription = this.currentScore$
-    .pipe(takeUntil(this.unsubscribeTrigger$))
-    .subscribe((score: string) => {
-      this.currentScore = score;
-      this.isPercentage = false;
-      this.changeDetectorRef.detectChanges();
-    });
-
     // Subscribe to the numericalScore$ Observable
     this.numericalScoreSubscription = this.numericalScore$
       .pipe(takeUntil(this.unsubscribeTrigger$))
@@ -121,7 +118,6 @@ export class ScoreComponent
 
   ngOnDestroy(): void {
     this.correctAnswersCountSubscription.unsubscribe();
-    this.currentScoreSubscription.unsubscribe();
     this.numericalScoreSubscription.unsubscribe();
     this.percentageScoreSubscription.unsubscribe();
     this.unsubscribeTrigger$.next();
