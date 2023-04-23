4ae104cad6a58f3a335cbcf5c7ce8cb159d63b3a
marvinrusinek <marvinrusinek@gmail.com>
2023-04-23 16:08:38 -0400

    1 file modified

--- src/app/containers/quiz/explanation/explanation.component.ts
+++ src/app/containers/quiz/explanation/explanation.component.ts
@@ -7,9 +7,21 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class QuizExplanationComponent {
-  @Input() isAnswered: boolean;
-  @Input() questionText: string;
+  @Input() isAnswered: boolean = false;
+  @Input() questionText: string = '';
   @Input() numberOfCorrectAnswers: number;
-  @Input() correctOptions: string;
-  @Input() explanationText: string;
+  @Input() correctOptions: string = '';
+  @Input() explanationText: string = '';
+
+  getExplanationText(): string {
+    if (!this.explanationText) {
+      throw new Error('No explanation available for this question.');
+    }
+    
+    if (this.numberOfCorrectAnswers === 1) {
+      return `Option ${this.correctOptions} was correct because ${this.explanationText}`;
+    } else {
+      return `Options ${this.correctOptions} were correct because ${this.explanationText}`;
+    }
+  }
 }
