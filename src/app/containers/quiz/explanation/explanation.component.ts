86cbc18de63feda745c6e61df061155a98531368
marvinrusinek <marvinrusinek@gmail.com>
2023-04-26 15:34:30 -0400

    2 files modified

--- src/app/containers/quiz/explanation/explanation.component.ts
+++ src/app/containers/quiz/explanation/explanation.component.ts
@@ -13,6 +13,7 @@ export class QuizExplanationComponent implements OnInit {
   @Input() numberOfCorrectAnswers: number;
   @Input() correctOptions: string = '';
   @Input() explanationText: string;
+  @Input() answers: any[] = [];
   explanation: string;
 
   ngOnInit(): void {
@@ -22,17 +23,36 @@ export class QuizExplanationComponent implements OnInit {
   getExplanationText(): string {
     console.log("isAnswered", this.isAnswered);
     console.log("numberOfCorrectAnswers", this.numberOfCorrectAnswers);
+
+    const correctAnswers = this.question.options.filter(
+      (option) => option.correct
+    );
+
+    const selectedOptions = this.question.options.filter(
+      (option) => this.answers.indexOf(option.id) !== -1
+    );
+
+    const selectedCorrectOptions = selectedOptions.filter(
+      (option) => option.correct
+    );
+
     if (!this.explanationText) {
       throw new Error('No explanation available for this question.');
     }
 
-    if (this.isAnswered === true) {
+    if (correctAnswers.length === selectedCorrectOptions.length) {
+      this.explanation = this.question.explanationText;
+    } else {
+      this.explanation = 'Sorry, that is not correct.';
+    }
+
+    /* if (this.isAnswered === true) {
       if (this.numberOfCorrectAnswers === 1) {
         return `Option ${this.correctOptions} was correct because ${this.explanationText}`;
       } else {
         return `Options ${this.correctOptions} were correct because ${this.explanationText}`;
       }
-    }
+    } */
 
     return '';
   }
