a4d74925242dcd0e13d9f110daae014bc2d19487
marvinrusinek <marvinrusinek@gmail.com>
2023-04-30 22:03:44 -0400

    1 file modified

--- src/app/shared/models/QuizQuestion.model.ts
+++ src/app/shared/models/QuizQuestion.model.ts
@@ -1,5 +1,11 @@
 import { Option } from './Option.model';
 
+enum QuestionType {
+  SingleAnswer = 'single_answer',
+  MultipleAnswer = 'multiple_answer',
+  TrueFalse = 'true_false',
+}
+
 export interface QuizQuestion {
   questionText: string;
   options: Option[];
