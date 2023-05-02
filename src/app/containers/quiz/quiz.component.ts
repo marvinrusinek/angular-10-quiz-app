6c7d79a98bcd01fff5cf035745648d615c905ab9
marvinrusinek <marvinrusinek@gmail.com>
2023-05-02 17:04:11 -0400

    1 file modified

--- src/app/containers/quiz/quiz.component.ts
+++ src/app/containers/quiz/quiz.component.ts
@@ -201,7 +201,6 @@ export class QuizComponent implements OnInit, OnDestroy {
   calculateNumberOfCorrectAnswers() {
     let numberOfCorrectAnswers = 0;
 
-    console.log("LENGTH", this.selectedQuiz?.questions?.length);
     for (let i = 0; i < this.selectedQuiz?.questions?.length; i++) {
       if (this.selectedAnswers[i] === this.correctAnswers[i]) {
         numberOfCorrectAnswers++;
