19289f621ae1097992589fbac31d29e263b3bc40
marvinrusinek <marvinrusinek@gmail.com>
2023-05-19 19:44:34 -0400

    1 file modified

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -576,7 +576,7 @@ export class QuizQuestionComponent implements OnInit, OnChanges, OnDestroy {
   toggleNumberOfCorrectAnswersVisibility(): void {
     this.shouldDisplayNumberOfCorrectAnswers = !this.shouldDisplayNumberOfCorrectAnswers;
     // this.shouldDisplayNumberOfCorrectAnswersChanged.emit(this.shouldDisplayNumberOfCorrectAnswers);  
-    this.shouldDisplayNumberOfCorrectAnswersChanged.emit({ shouldDisplay: this.shouldDisplayNumberOfCorrectAnswers, numberOfCorrectAnswers });      
+    this.shouldDisplayNumberOfCorrectAnswersChanged.emit({ shouldDisplay: this.shouldDisplayNumberOfCorrectAnswers, numberOfCorrectAnswers: this.quizService.numberOfCorrectAnswers });     
     this.toggleVisibility.emit();
   }
 
