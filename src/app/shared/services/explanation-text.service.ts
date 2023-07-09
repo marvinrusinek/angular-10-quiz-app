baa003fd761c3863dd51a73f26534e0675ec1961
marvinrusinek <marvinrusinek@gmail.com>
2023-07-08 20:20:51 -0400

    1 file modified

--- src/app/shared/services/explanation-text.service.ts
+++ src/app/shared/services/explanation-text.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, of } from 'rxjs';
+import { BehaviorSubject, of } from 'rxjs';
 
 import { Option } from '../../shared/models/Option.model';
 import { QuizQuestion } from '../../shared/models/QuizQuestion.model';
