bdbe172f47a48c0f650dd877eda114a9b7c4ab19
marvinrusinek <marvinrusinek@gmail.com>
2023-07-16 17:26:44 -0400

    1 file modified

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -652,7 +652,7 @@ export class QuizQuestionComponent
     }
   }
   
-  onOptionClicked(option: Option): void {
+  /* onOptionClicked(option: Option): void {
     const index = this.selectedOptions.findIndex((o) => o === option);
     const isOptionSelected = index !== -1;
   
@@ -712,7 +712,64 @@ export class QuizQuestionComponent
       this.showFeedbackForOption[this.selectedOption.optionId];
   
     this.showFeedback = isFeedbackVisible;
+  } */
+
+  onOptionClicked(option: Option): void {
+    const index = this.selectedOptions.findIndex((o) => o === option);
+    const isOptionSelected = index !== -1;
+  
+    if (!isOptionSelected) {
+      this.selectedOptions.push(option);
+      this.selectedOption = option;
+      this.optionChecked[option.optionId] = true;
+      this.showFeedbackForOption[option.optionId] = true;
+      this.showFeedback = true; // Set showFeedback to true for immediate feedback display
+    } else {
+      this.selectedOptions.splice(index, 1);
+      this.selectedOption = null;
+      this.optionChecked[option.optionId] = false;
+      this.showFeedbackForOption[option.optionId] = false;
+  
+      if (this.selectedOptions.length === 0) {
+        this.showFeedback = false; // Set showFeedback to false if no options are selected
+      }
+    }
+  
+    this.optionClicked.emit();
+    this.isOptionSelected = true;
+    this.isAnswered = this.selectedOptions.length > 0;
+    this.isAnsweredChange.emit(this.isAnswered);
+    this.isAnswerSelectedChange.emit(this.isAnswered);
+    this.optionSelected.emit(this.isOptionSelected);
+  
+    this.explanationTextService
+      .setExplanationText(this.selectedOptions, this.question)
+      .subscribe((explanationText: string) => {
+        this.explanationText$.next(explanationText);
+        this.explanationTextValue$.next(explanationText);
+        this.isAnswerSelectedChange.emit(true);
+        this.toggleVisibility.emit();
+        this.updateFeedbackVisibility();
+      });
+  
+    // Emit updated selection
+    this.selectionChanged.emit({
+      question: this.currentQuestion,
+      selectedOptions: this.selectedOptions,
+    });
   }
+  
+  updateFeedbackVisibility(): void {
+    const isOptionSelected = this.selectedOptions.length > 0;
+    const isFeedbackVisible =
+      isOptionSelected &&
+      this.isAnswered &&
+      this.selectedOption &&
+      this.showFeedbackForOption[this.selectedOption.optionId];
+  
+    this.showFeedback = isFeedbackVisible;
+  }
+  
                             
   updateSelectedOption(selectedOption: Option, optionIndex: number): void {
     this.alreadyAnswered = true;
