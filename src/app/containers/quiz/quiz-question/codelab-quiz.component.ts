f1615292f29c12dd22f590a3feb6d185f7be68e3
marvinrusinek <marvinrusinek@gmail.com>
2023-07-17 12:31:41 -0400

    3 files modified

--- src/app/containers/quiz/quiz-question/codelab-quiz.component.ts
+++ src/app/containers/quiz/quiz-question/codelab-quiz.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'codelab-quiz-component',
+  selector: 'codelab-quiz-cp-component',
   templateUrl: './codelab-quiz.component.html',
   styleUrls: ['./codelab-quiz.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
