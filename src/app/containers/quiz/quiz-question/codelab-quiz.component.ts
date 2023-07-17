990b964e2afc274a3d6addd180a4a850372a8e12
marvinrusinek <marvinrusinek@gmail.com>
2023-07-17 13:08:35 -0400

    1 file modified

--- src/app/containers/quiz/quiz-question/codelab-quiz.component.ts
+++ src/app/containers/quiz/quiz-question/codelab-quiz.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { QuizQuestionManagerService } from '../../../shared/services/quizquestionmgr.service';
+
 @Component({
   selector: 'codelab-quiz-cp-component',
   templateUrl: './codelab-quiz.component.html',
@@ -7,15 +9,23 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CodelabQuizComponent { 
+  currentQuestion: any; // Replace `any` with the appropriate type for your quiz question
+  explanationText: string;
+  numberOfCorrectAnswers: number;
 
+  constructor(private quizQuestionManagerService: QuizQuestionManagerService) {}
+
+  ngOnInit(): void {
+    this.currentQuestion = this.quizQuestionManagerService.getCurrentQuestion();
+    this.explanationText = this.quizQuestionManagerService.getExplanationText();
+    this.numberOfCorrectAnswers = this.quizQuestionManagerService.getNumberOfCorrectAnswers();
+  }
+
+  shouldDisplayExplanationText(): boolean {
+    return this.quizQuestionManagerService.shouldDisplayExplanationText();
+  }
 
   shouldDisplayNumberOfCorrectAnswersCount(): boolean {
-    return (
-      this.shouldDisplayNumberOfCorrectAnswers &&
-      this.isMultipleCorrectAnswers() &&
-      !this.isOptionSelected &&
-      !this.shouldDisplayExplanation() &&
-      !this.shouldDisplayExplanationText()
-    );
-  } 
+    return this.quizQuestionManagerService.shouldDisplayNumberOfCorrectAnswersCount();
+  }
 }
