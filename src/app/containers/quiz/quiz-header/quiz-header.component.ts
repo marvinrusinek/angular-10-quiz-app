9cf923648101b86573f9b0850ff6161384423107
marvinrusinek <marvinrusinek@gmail.com>
2023-07-18 12:59:42 -0400

    1 file added

---
+++ src/app/containers/quiz/quiz-header/quiz-header.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'codelab-quiz-header',
+  templateUrl: './quiz-header.component.html',
+  styleUrls: ['./quiz-header.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CodelabQuizComponent { }
