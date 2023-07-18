570e4313eac5034cddf933463f8edd4f9cf1f260
marvinrusinek <marvinrusinek@gmail.com>
2023-07-18 18:42:39 -0400

    1 file modified

--- src/app/containers/quiz/quiz-question/codelab-quiz.component.ts
+++ src/app/containers/quiz/quiz-question/codelab-quiz.component.ts
@@ -47,7 +47,7 @@ export class CodelabQuizComponent {
 
     this.nextQuestionSubscription = this.quizService.nextQuestion$.subscribe((nextQuestion) => {
       if (nextQuestion) {
-        this.currentQuestion = nextQuestion;
+        // this.currentQuestion = nextQuestion;
         this.quizStateService.setCurrentQuestion(nextQuestion);
       } else {
         // Handle the scenario when there are no more questions
