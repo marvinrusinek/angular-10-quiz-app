ea2ac19046cbce0411c8bcdd8f7359b1bec39ad5
marvinrusinek <marvinrusinek@gmail.com>
2023-07-20 19:40:51 -0400

    1 file modified

--- src/app/containers/quiz/quiz.component.ts
+++ src/app/containers/quiz/quiz.component.ts
@@ -879,45 +879,44 @@ export class QuizComponent implements OnInit, OnDestroy {
     if (!this.selectedQuiz) {
       return;
     }
-
+  
+    this.animationState$.next('animationStarted');
+  
     const selectedOption = this.form.value.selectedOption;
-
-    if (this.form.valid) {
-      this.animationState$.next('animationStarted');
-
-      const nextQuestion = await this.quizService.getNextQuestion();
-
-      if (nextQuestion) {
-        this.currentQuestion = nextQuestion;
-        this.nextQuestionText = nextQuestion.questionText;
-        this.quizService.setNextQuestion(nextQuestion);
-        this.quizService.setCurrentQuestionIndex(this.currentQuestionIndex + 1);
-        this.currentOptions.next(nextQuestion.options); // set the current options observable with the options of the next question
-      } else {
-        this.nextQuestionText = null;
-      }
-
-      this.selectedOption = null;
-      this.quizService.navigateToNextQuestion();
-      this.quizService.resetAll();
-
-      if (!selectedOption) {
-        return;
-      }
-
-      this.checkIfAnsweredCorrectly();
-      this.answers = [];
-      this.status = QuizStatus.CONTINUE;
-
-      if (this.quizService.isLastQuestion()) {
-        this.status = QuizStatus.COMPLETED;
-        this.submitQuiz();
-        this.router.navigate([QuizRoutes.RESULTS]);
-      } else {
-        this.timerService.resetTimer();
-      }
+  
+    // Get the next question
+    const nextQuestion = await this.quizService.getNextQuestion();
+  
+    if (nextQuestion) {
+      this.currentQuestion = nextQuestion;
+      this.nextQuestionText = nextQuestion.questionText;
+      this.quizService.setNextQuestion(nextQuestion);
+      this.quizService.setCurrentQuestionIndex(this.currentQuestionIndex + 1);
+      this.currentOptions.next(nextQuestion.options); // set the current options observable with the options of the next question
+    } else {
+      this.nextQuestionText = null;
     }
-  }
+  
+    this.selectedOption = null;
+    this.quizService.resetAll();
+  
+    if (!selectedOption) {
+      return;
+    }
+  
+    this.checkIfAnsweredCorrectly();
+    this.answers = [];
+    this.status = QuizStatus.CONTINUE;
+  
+    if (this.quizService.isLastQuestion()) {
+      this.status = QuizStatus.COMPLETED;
+      this.submitQuiz();
+      this.router.navigate([QuizRoutes.RESULTS]);
+    } else {
+      this.timerService.resetTimer();
+    }
+  }  
+  
 
   advanceToPreviousQuestion() {
     this.answers = [];
