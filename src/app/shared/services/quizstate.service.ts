6449a08727042a651918326e3f33c98caa3b87b6
marvinrusinek <marvinrusinek@gmail.com>
2023-07-22 09:55:29 -0400

    1 file modified

--- src/app/shared/services/quizstate.service.ts
+++ src/app/shared/services/quizstate.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, of, Subject, throwError } from 'rxjs';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
 import { catchError, distinctUntilChanged } from 'rxjs/operators';
 
 import { Option } from '../../shared/models/Option.model';
@@ -9,17 +9,14 @@ import { QuizQuestion } from '../../shared/models/QuizQuestion.model';
   providedIn: 'root',
 })
 export class QuizStateService {
-  currentQuestion: BehaviorSubject<QuizQuestion | null> 
-    = new BehaviorSubject<QuizQuestion | null>(null);
-  currentQuestionSubject = new BehaviorSubject<QuizQuestion>(null);
-  private currentQuestionSource = new Subject<QuizQuestion>();
-  currentQuestion$: Observable<QuizQuestion> = this.currentQuestionSource.asObservable();
+  private currentQuestionSubject = new BehaviorSubject<QuizQuestion | null>(null);
+  currentQuestion$: Observable<QuizQuestion | null> = this.currentQuestionSubject.asObservable();
 
-  currentOptionsSubject = new BehaviorSubject<Option[]>(null);
-  currentOptions$: Observable<Option[]> = of(null);
+  private currentOptionsSubject = new BehaviorSubject<Option[]>([]);
+  currentOptions$: Observable<Option[]> = this.currentOptionsSubject.asObservable();
 
   private multipleAnswerSubject = new BehaviorSubject<boolean>(false);
-  multipleAnswer$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  multipleAnswer$: Observable<boolean> = this.multipleAnswerSubject.asObservable();
 
   private quizQuestionCreated = false;
 
@@ -27,10 +24,10 @@ export class QuizStateService {
 
   setCurrentQuestion(question$: Observable<QuizQuestion>): void {
     if (!question$) {
-      throwError('Question$ is null or undefined.');
+      console.error('Question$ is null or undefined.');
       return;
     }
-  
+
     question$.pipe(
       catchError((error) => {
         console.error(error);
@@ -39,34 +36,31 @@ export class QuizStateService {
       distinctUntilChanged()
     ).subscribe((question) => {
       console.log('Current question:::', question);
-      this.currentQuestion.next(question);
       this.currentQuestionSubject.next(question);
-      this.currentQuestionSource.next(question);
       if (question && question.options) {
         console.log('Options:::', question.options);
-        this.currentQuestion.next(question);
-        this.currentOptionsSubject.next(question?.options || []);
+        this.currentOptionsSubject.next(question.options || []);
       } else {
         console.log('No options found.');
       }
-    });   
+    });
   }
-      
-  getCurrentQuestion(): Observable<QuizQuestion> {
+
+  getCurrentQuestion(): Observable<QuizQuestion | null> {
     return this.currentQuestion$;
   }
 
   setCurrentOptions(options: Option[]): void {
-    this.currentOptions$ = of(options);
+    this.currentOptionsSubject.next(options);
   }
 
   isMultipleAnswer(): Observable<boolean> {
-    const question = this.currentQuestion.value;
+    const question = this.currentQuestionSubject.value;
     if (!question) {
       console.error('Question is not defined');
-      return of(false);
+      return this.multipleAnswerSubject;
     }
-  
+
     if (question && question.options) {
       const correctOptions = question.options?.filter((option) => option.correct);
       const isMultipleAnswer = correctOptions.length > 1;
@@ -75,15 +69,12 @@ export class QuizStateService {
       console.error('Question options not found.', question);
       this.setMultipleAnswer(false);
     }
-  
-    return this.multipleAnswerSubject.asObservable();
+
+    return this.multipleAnswerSubject;
   }
 
   setMultipleAnswer(value: boolean): void {
     this.multipleAnswerSubject.next(value);
-    this.multipleAnswer$.subscribe((value) => {
-      this.multipleAnswerSubject.next(value);
-    });
   }
 
   setQuizQuestionCreated(): void {
