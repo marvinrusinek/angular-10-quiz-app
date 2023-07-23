9ebbf7cf5e59b1a0dd2be2236e7bbc75b28a2475
marvinrusinek <marvinrusinek@gmail.com>
2023-07-23 13:18:17 -0400

    2 files modified

--- src/app/containers/quiz/quiz.component.ts
+++ src/app/containers/quiz/quiz.component.ts
@@ -905,25 +905,23 @@ export class QuizComponent implements OnInit, OnDestroy {
     // Get the next question
     const nextQuestion = await this.quizService.getNextQuestion();
   
-    if (nextQuestion && nextQuestion.options) {
-      if (nextQuestion && nextQuestion.options) {
-        this.currentQuestion = nextQuestion;
-        this.nextQuestionText = nextQuestion.questionText;
-        this.quizService.setNextQuestion(nextQuestion);
-        this.quizService.setCurrentQuestionIndex(this.currentQuestionIndex + 1);
-        
-        this.quizService.updateCurrentOptions(nextQuestion.options);
-        this.quizService.updateCurrentQuestion();
-        
-        const explanationTextOfNextQuestion = nextQuestion.questionText;
-        this.explanationTextSource.next(explanationTextOfNextQuestion);
-
-        this.quizService.navigateToNextQuestion();
-      } else {
-        this.nextQuestionText = null;
-      }
+    if (nextQuestion) {
+      this.currentQuestion = nextQuestion;
+      this.nextQuestionText = nextQuestion.questionText;
+      this.quizService.setNextQuestion(nextQuestion);
+      this.quizService.setCurrentQuestionIndex(this.currentQuestionIndex + 1);
+  
+      this.quizService.updateCurrentOptions(nextQuestion.options);
+  
+      const explanationTextOfNextQuestion = nextQuestion.questionText;
+      this.explanationTextSource.next(explanationTextOfNextQuestion);
+  
+      this.quizService.navigateToNextQuestion();
+    } else {
+      this.nextQuestionText = null;
     }
   }
+  
     
   advanceToPreviousQuestion() {
     this.answers = [];
