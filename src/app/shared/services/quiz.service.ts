4d4dd2568fe13d6b0205c50e0cf380cc72aac424
marvinrusinek <marvinrusinek@gmail.com>
2023-07-24 11:36:59 -0400

    1 file modified

--- src/app/shared/services/quiz.service.ts
+++ src/app/shared/services/quiz.service.ts
@@ -981,14 +981,14 @@ export class QuizService implements OnDestroy {
   }
 
   /********* navigation functions ***********/
-  async navigateToNextQuestion(): Promise<void> {
+  navigateToNextQuestion(): void {
     this.currentQuestionIndex++;
     console.log('Current question index after navigation:', this.currentQuestionIndex);
     this.currentQuestionIndexSource.next(this.currentQuestionIndex);
     const newUrl = `${QuizRoutes.QUESTION}${encodeURIComponent(
       this.quizId
     )}/${this.currentQuestionIndex + 1}`;
-    await this.router.navigate([newUrl]);
+    this.router.navigate([newUrl]);
     console.log('Current question index from QuizService:', this.quizService.getCurrentQuestionIndex());
   }
 
