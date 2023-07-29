de271052b76cdf2f4eeb469e3d6490e6e28fe2a6
marvinrusinek <marvinrusinek@gmail.com>
2023-07-29 11:24:05 -0400

    1 file modified

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -564,26 +564,6 @@ export class QuizQuestionComponent
     }
   }
 
-  /* private updateCorrectMessage(): void {
-    console.log("DATA:::>>>", this.data);
-    console.log("Correct Answers:::>>>", this.correctAnswers);
-    console.log("Current Options:::", this.data?.currentOptions);
-
-    if (this.data && this.data?.currentOptions && this.data?.currentOptions?.length > 0) {
-      try {
-        this.correctMessage = this.quizService.setCorrectMessage(
-          this.data,
-          this.quizService.correctAnswers,
-          this.data.currentOptions
-        );
-      } catch (error) {
-        console.error('An error occurred while updating the correct message:', error);
-      }
-    } else {
-      this.correctMessage = 'The correct answers are not available yet.';
-    }
-  } */
-
   private updateCorrectMessage(): void {
     if (!this.quizService.data) {
       this.correctMessage = 'The correct answers are not available yet.';
