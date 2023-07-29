8917f1221b82413ab90c47158329952c5e8bf427
marvinrusinek <marvinrusinek@gmail.com>
2023-07-29 14:29:36 -0400

    1 file modified

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -261,7 +261,14 @@ export class QuizQuestionComponent
       this.loadCurrentQuestion();
       this.toggleOptions();
       this.getCorrectAnswers();
-      this.updateCorrectMessage();
+      // this.updateCorrectMessage();
+
+      this.quizService.currentOptions$.subscribe((currentOptions) => {
+        this.correctAnswers = this.quizService.correctAnswers;
+        this.currentOptions = currentOptions;
+    
+        this.updateCorrectMessage();
+      });
     } catch (error) {
       console.error('Error getting current question:', error);
     }
