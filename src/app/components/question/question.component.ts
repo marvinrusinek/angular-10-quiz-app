b21acd0e5f598995a88d4ac1fbb7333155c99de5
marvinrusinek <marvinrusinek@gmail.com>
2023-08-05 18:40:55 -0400

    1 file modified

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -786,6 +786,9 @@ export class QuizQuestionComponent
   
     this.correctAnswers = this.quizService.getCorrectAnswersForQuestion(data.questionText);
     this.updateCorrectMessage(this.correctAnswers);
+
+    // Call the isMultipleAnswer function to determine if the question is a multiple-answer question
+    data.isMultipleAnswer = await this.quizStateService.isMultipleAnswer().toPromise();
   }  
           
   private updateMultipleAnswer(): void {
