bae307fd4567f1f6c1b79857c6fdb23aba118ecd
marvinrusinek <marvinrusinek@gmail.com>
2023-08-08 19:55:54 -0400

    1 file modified

--- src/app/shared/services/explanation-text.service.ts
+++ src/app/shared/services/explanation-text.service.ts
@@ -16,7 +16,7 @@ export class ExplanationTextService {
     return this.explanationText$.asObservable();
   }
 
-  setExplanationText(
+  /* setExplanationText(
     selectedOptions: Option[],
     question?: QuizQuestion
   ): Observable<string> {
@@ -57,6 +57,73 @@ export class ExplanationTextService {
           this.explanationText$.next(text);
         } else {
           const text = `Options ${optionIndicesString} are correct because ${question.explanation}`;
+          this.explanationText$.next(text);
+        }
+      }
+  
+      return this.explanationText$;
+    } catch (error) {
+      console.error('Error occurred while getting explanation text:', error);
+      this.explanationText$.next('');
+      return this.explanationText$;
+    }
+  } */
+
+  setExplanationText(
+    selectedOptions: Option[],
+    question?: QuizQuestion
+  ): Observable<string> {
+    if (!Array.isArray(selectedOptions)) {
+      console.error('Error: selectedOptions is not an array');
+      return of('');
+    }
+  
+    try {
+      const correctOptions = question?.options?.filter((option) => option?.correct) || [];
+      const selectedCorrectOptions = selectedOptions.filter(
+        (option) => option?.correct !== undefined && option?.correct
+      );
+
+      console.log('Correct Options:', correctOptions);
+      console.log('Selected Correct Options:', selectedCorrectOptions);
+
+      if (selectedOptions.length === 0) {
+        console.log('Setting Explanation Text to empty');
+        this.explanationText$.next('');
+      } else if (correctOptions.length === selectedCorrectOptions.length) {
+        const correctOptionIndices = correctOptions.map(
+          (option) => question.options.indexOf(option) + 1
+        );
+
+        console.log('All Selected Options Are Correct');
+        console.log('Correct Option Indices:', correctOptionIndices);
+
+        if (correctOptions.length === 1) {
+          const text = `Option ${correctOptionIndices[0]} is correct because ${question.explanation}`;
+          console.log('Setting Explanation Text:', text);
+          this.explanationText$.next(text);
+        } else if (correctOptions.length > 1) {
+          const correctOptionsString = correctOptionIndices.join(' and ');
+          const text = `Options ${correctOptionsString} are correct because ${question.explanation}`;
+          console.log('Setting Explanation Text:', text);
+          this.explanationText$.next(text);
+        }
+      } else {
+        const correctOptionIndices = correctOptions.map(
+          (option) => question.options.indexOf(option) + 1
+        );
+        const optionIndicesString = correctOptionIndices.join(' and ');
+
+        console.log('Some Selected Options Are Correct');
+        console.log('Correct Option Indices:', correctOptionIndices);
+
+        if (correctOptions.length === 1) {
+          const text = `Option ${optionIndicesString} is correct because ${question.explanation}`;
+          console.log('Setting Explanation Text:', text);
+          this.explanationText$.next(text);
+        } else {
+          const text = `Options ${optionIndicesString} are correct because ${question.explanation}`;
+          console.log('Setting Explanation Text:', text);
           this.explanationText$.next(text);
         }
       }
