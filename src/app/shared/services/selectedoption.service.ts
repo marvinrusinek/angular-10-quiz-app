bbbf9cbd6a6f1fc7e74170d8c02d1483fba22168
marvinrusinek <marvinrusinek@gmail.com>
2023-08-15 09:08:05 -0400

    1 file added

---
+++ src/app/shared/services/selectedoption.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SelectedOptionService {
+  private selectedOptionExplanationSource = new BehaviorSubject<string>(null);
+  selectedOptionExplanation$ =
+    this.selectedOptionExplanationSource.asObservable();
+
+  setSelectedOptionExplanation(explanation: string): void {
+    this.selectedOptionExplanationSource.next(explanation);
+  }
+
+  getSelectedOptionExplanation(): string {
+    return this.selectedOption?.explanation || '';
+  }
+}
