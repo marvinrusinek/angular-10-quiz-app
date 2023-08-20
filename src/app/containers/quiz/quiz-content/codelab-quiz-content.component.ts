d1c4ff6c02adea55d60c6db5d312b18d7ab8f649
marvinrusinek <marvinrusinek@gmail.com>
2023-08-20 11:03:18 -0400

    1 file modified

--- src/app/containers/quiz/quiz-content/codelab-quiz-content.component.ts
+++ src/app/containers/quiz/quiz-content/codelab-quiz-content.component.ts
@@ -147,7 +147,7 @@ export class CodelabQuizContentComponent {
       })
     ); */
 
-    this.combinedText$ = combineLatest([
+    /* this.combinedText$ = combineLatest([
       this.quizService.nextQuestion$,
       this.explanationTextService.explanationText$,
       this.numberOfCorrectAnswers$,
@@ -164,7 +164,69 @@ export class CodelabQuizContentComponent {
     
         return combinedText;
       })
+    ); */
+
+    /* this.combinedText$ = combineLatest([
+      this.quizService.nextQuestion$,
+      this.explanationTextService.explanationText$,
+      this.numberOfCorrectAnswers$,
+      this.explanationTextService.isExplanationTextDisplayed$
+    ]).pipe(
+      map(([nextQuestion, explanationText, numberOfCorrectAnswers, isExplanationTextDisplayed]) => {
+        const nextQuestionText = nextQuestion?.questionText || '';
+        const correctAnswersText = this.getNumberOfCorrectAnswersText(+numberOfCorrectAnswers);
+    
+        // Use the explanation text directly if it's being displayed
+        const explanation = isExplanationTextDisplayed ? explanationText || '' : '';
+    
+        // Display nextQuestionText if explanation is not available
+        const combinedText = explanation || nextQuestionText;
+    
+        return combinedText;
+      })
+    ); */
+
+    /* this.combinedText$ = combineLatest([
+      this.quizService.nextQuestion$,
+      this.explanationTextService.explanationText$,
+      this.numberOfCorrectAnswers$,
+      this.explanationTextService.isExplanationTextDisplayed$
+    ]).pipe(
+      map(([nextQuestion, explanationText, numberOfCorrectAnswers, isExplanationTextDisplayed]) => {
+        const nextQuestionText = nextQuestion?.questionText || '';
+        const correctAnswersText = this.getNumberOfCorrectAnswersText(+numberOfCorrectAnswers);
+    
+        // Use the explanation text directly if it's being displayed
+        const explanation = isExplanationTextDisplayed ? explanationText || '' : '';
+    
+        // Display nextQuestionText if explanation is not available
+        const combinedText = explanation || nextQuestionText;
+    
+        return combinedText;
+      })
+    ); */
+
+    this.combinedText$ = combineLatest([
+      this.quizService.nextQuestion$,
+      this.explanationTextService.explanationText$,
+      this.numberOfCorrectAnswers$,
+      this.explanationTextService.isExplanationTextDisplayed$,
+    ]).pipe(
+      map(([nextQuestion, explanationText, numberOfCorrectAnswers, isExplanationTextDisplayed]) => {
+        const nextQuestionText = nextQuestion?.questionText || '';
+        const correctAnswersText = this.getNumberOfCorrectAnswersText(+numberOfCorrectAnswers);
+    
+        // Use the explanation text directly if it's being displayed
+        const explanation = isExplanationTextDisplayed ? explanationText || '' : '';
+    
+        // Display nextQuestionText if explanation is not available
+        const combinedText = explanation || nextQuestionText;
+    
+        return combinedText;
+      })
     );
+    
+    
     
     
 
