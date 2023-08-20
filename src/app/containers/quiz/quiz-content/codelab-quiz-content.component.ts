807389bfd0953849a9bc58abd6c2b336b2826227
marvinrusinek <marvinrusinek@gmail.com>
2023-08-20 17:43:39 -0400

    1 file modified

--- src/app/containers/quiz/quiz-content/codelab-quiz-content.component.ts
+++ src/app/containers/quiz/quiz-content/codelab-quiz-content.component.ts
@@ -130,7 +130,7 @@ export class CodelabQuizContentComponent {
       }
     });
 
-    /* this.nextQuestion$ = this.quizService.nextQuestion$;
+    this.nextQuestion$ = this.quizService.nextQuestion$;
     this.explanationText$ = this.explanationTextService.explanationText$;
     this.shouldDisplayExplanation$ = this.explanationTextService.shouldDisplayExplanation$;
 
@@ -154,9 +154,9 @@ export class CodelabQuizContentComponent {
           map(newNextQuestion => newNextQuestion.questionText)
         );
       })
-    ); */
+    );
 
-    this.nextQuestion$ = this.quizService.nextQuestion$;
+    /* this.nextQuestion$ = this.quizService.nextQuestion$;
     this.explanationText$ = this.explanationTextService.explanationText$;
     this.shouldDisplayExplanation$ = this.explanationTextService.shouldDisplayExplanation$;
 
@@ -179,7 +179,7 @@ export class CodelabQuizContentComponent {
           map(newExplanationText => newExplanationText || nextQuestion.questionText)
         );
       })
-    );    
+    ); */ 
   }
 
   
