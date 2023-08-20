cb5f3de8bc8bfd797f4ab8ecce728b9b4e9f269a
marvinrusinek <marvinrusinek@gmail.com>
2023-08-19 20:07:32 -0400

    1 file modified

--- src/app/shared/services/selectedoption.service.ts
+++ src/app/shared/services/selectedoption.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
+import { Option } from '../../shared/models/Option.model';
+
 @Injectable({
   providedIn: 'root',
 })
 export class SelectedOptionService {
+  selectedOption: Option;
   private selectedOptionExplanationSource = new BehaviorSubject<string>(null);
   selectedOptionExplanation$ =
     this.selectedOptionExplanationSource.asObservable();
