ff5d233cee53d1072b30503304043b1a910161f5
marvinrusinek <marvinrusinek@gmail.com>
2023-08-22 17:03:36 -0400

    1 file modified

--- src/app/containers/quiz/quiz.component.ts
+++ src/app/containers/quiz/quiz.component.ts
@@ -945,8 +945,12 @@ export class QuizComponent implements OnInit, OnDestroy {
   }
 
   shouldHideShowScoreNav(): boolean {
-    return this.questionIndex === this.totalQuestions - 1;
+    console.log('questionIndex:', this.questionIndex);
+    console.log('totalQuestions:', this.totalQuestions);
+    // return this.questionIndex === this.totalQuestions - 1;
+    return this.currentQuestionIndex === this.totalQuestions - 1;
   }
+  
 
   shouldHideProgressBar(): boolean {
     return this.totalQuestions < 1;
