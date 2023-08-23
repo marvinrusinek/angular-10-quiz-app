3c6d64e70f6039d268672e830c6b97c97b47625e
marvinrusinek <marvinrusinek@gmail.com>
2023-08-23 14:41:01 -0400

    1 file modified

--- src/app/router/guards/quiz.guard.ts
+++ src/app/router/guards/quiz.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, Router } from '@angular/router';
 import { Observable, of } from 'rxjs';
-import { catchError, map, switchMap, tap } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 
 import { Quiz } from '../../shared/models/Quiz.model';
 import { QuizDataService } from '../../shared/services/quizdata.service';
