e2e0ae003f36197c08801de7282d43fa13d144c6
marvinrusinek <marvinrusinek@gmail.com>
2023-08-23 13:02:09 -0400

    1 file modified

--- src/app/router/guards/quiz.guard.ts
+++ src/app/router/guards/quiz.guard.ts
@@ -19,43 +19,6 @@ export class QuizGuard implements CanActivate {
     const quizId = route.params['quizId'];
     const questionIndex = +route.params['questionIndex']; // Convert to a number
   
-    /* return this.quizDataService.selectedQuizSubject.pipe(
-      tap(selectedQuiz => console.log('Selected quiz in guard:', selectedQuiz)),
-      switchMap(selectedQuiz => {
-        console.log('quizId:', quizId);
-        console.log('questionIndex:', questionIndex);
-        if (!selectedQuiz) {
-          console.error('Selected quiz is null.');
-          this.router.navigate(['/select']);
-          return of(false);
-        }
-  
-        const totalQuestions = selectedQuiz.questions.length;
-  
-        // Check if it's the introduction route
-        if (questionIndex === 0) {
-          return of(true);
-        }
-  
-        // Check if questionIndex is out of range
-        if (questionIndex > totalQuestions) {
-          this.router.navigate(['/question', quizId, totalQuestions - 1]);
-          return of(false);
-        } else if (questionIndex < 1) {
-          this.router.navigate(['/question', quizId, 1]);
-          return of(false);
-        }
-  
-        // Allow navigation to the question route
-        return of(true);
-      }),
-      catchError(error => {
-        console.error(`Error fetching selected quiz: ${error}`);
-        this.router.navigate(['/select']);
-        return of(false);
-      })
-    ); */
-
     return this.quizDataService.getSelectedQuiz().pipe(
       map(selectedQuiz => {
         if (!selectedQuiz) {
