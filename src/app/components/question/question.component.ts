78578a8edfcdb5e98e9beae489adfc37c44c6eaf
marvinrusinek <marvinrusinek@gmail.com>
2023-09-01 19:53:54 -0400

    1 file modified

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1095,7 +1095,6 @@ export class QuizQuestionComponent implements OnInit, OnChanges, OnDestroy {
       .setExplanationText(options, currentQuestion)
       .subscribe(
         (explanationText: string) => {
-          console.log("explanationText:>>", explanationText);
           this.explanationText$.next(explanationText);
           this.explanationTextValue$.next(explanationText);
           this.isAnswerSelectedChange.emit(true);
