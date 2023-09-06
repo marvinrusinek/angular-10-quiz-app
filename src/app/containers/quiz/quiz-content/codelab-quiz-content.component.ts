c52fdf465066d142bd8112e81bb586963f813c89
marvinrusinek <marvinrusinek@gmail.com>
2023-09-06 17:29:36 -0400

    1 file modified

--- src/app/containers/quiz/quiz-content/codelab-quiz-content.component.ts
+++ src/app/containers/quiz/quiz-content/codelab-quiz-content.component.ts
@@ -499,6 +499,9 @@ export class CodelabQuizContentComponent {
             return of(nextQuestion).pipe(
               tap(() => console.log('EXPLTEXT', explanationText)),
               switchMap(() => {
+                console.log('shouldDisplayExplanation:', shouldDisplayExplanation);
+                console.log('explanationText:', explanationText);
+
                 if (shouldDisplayExplanation && explanationText !== '') {
                   console.log('Satisfying Condition: Displaying Explanation Text');
                   this.explanationTextService.setShouldDisplayExplanation(false);
