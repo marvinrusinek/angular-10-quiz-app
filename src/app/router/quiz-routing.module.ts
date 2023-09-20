5c2366efd4d9fd689dfb38055167514f86f97031
marvinrusinek <marvinrusinek@gmail.com>
2023-09-20 15:50:32 -0400

    1 file modified

--- src/app/router/quiz-routing.module.ts
+++ src/app/router/quiz-routing.module.ts
@@ -8,37 +8,37 @@ import { ResultsComponent } from '../containers/results/results.component';
 import { QuizGuard } from './guards/quiz.guard';
 
 const routes: Routes = [
-  { 
+  {
     path: '',
-    redirectTo: 'select', 
-    pathMatch: 'full'
+    redirectTo: 'select',
+    pathMatch: 'full',
   },
-  { 
-    path: 'select', 
-    component: QuizSelectionComponent, 
-    pathMatch: 'full'
+  {
+    path: 'select',
+    component: QuizSelectionComponent,
+    pathMatch: 'full',
   },
   {
     path: 'intro/:quizId',
     component: IntroductionComponent,
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: 'question/:quizId/:questionIndex',
     component: QuizComponent,
     pathMatch: 'full',
     canActivate: [QuizGuard],
-    runGuardsAndResolvers: 'always'
+    runGuardsAndResolvers: 'always',
   },
-  { 
-    path: 'results/:quizId', 
-    component: ResultsComponent, 
-    pathMatch: 'full' 
-  }
+  {
+    path: 'results/:quizId',
+    component: ResultsComponent,
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
 export class QuizRoutingModule {}
