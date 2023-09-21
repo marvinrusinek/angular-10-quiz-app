601a54231fb5e69e1db6a73db6df0812924e87d9
marvinrusinek <marvinrusinek@gmail.com>
2023-09-21 14:20:47 -0400

    1 file modified

--- src/app/shared/services/quiz.service.ts
+++ src/app/shared/services/quiz.service.ts
@@ -1244,48 +1244,46 @@ export class QuizService implements OnDestroy {
     this.currentQuestionIndexSource.next(this.currentQuestionIndex);
   
     // Get the total number of questions using an observable
-    this.getTotalQuestions().subscribe((totalQuestions: number) => {
-      console.log('Total Questions:', totalQuestions);
+    const totalQuestions: number = await this.getTotalQuestions().toPromise();
   
-      // Check if the next question index is within the valid range of questions
-      if (this.currentQuestionIndex < totalQuestions) {
-        const nextQuestionIndex = this.currentQuestionIndex;
-        const newUrl = `${QuizRoutes.QUESTION}${encodeURIComponent(this.quizId)}/${nextQuestionIndex + 1}`;
-        console.log('New URL:', newUrl);
+    console.log('Total Questions:', totalQuestions);
   
-        try {
-          // Use Router events to track navigation success or failure
-          const navigationSubscription = this.router.events
-            .pipe(filter(event => event instanceof NavigationEnd || event instanceof NavigationError || event instanceof NavigationCancel))
-            .subscribe((event) => {
-              if (event instanceof NavigationEnd) {
-                console.log('Navigation successful.');
-                navigationSubscription.unsubscribe(); // Unsubscribe to prevent memory leaks
-              } else if (event instanceof NavigationError) {
-                console.error('Navigation error:', event.error);
-                navigationSubscription.unsubscribe(); // Unsubscribe on error
-              } else if (event instanceof NavigationCancel) {
-                console.warn('Navigation canceled.');
-                navigationSubscription.unsubscribe(); // Unsubscribe on cancel
-              }
-            });
+    if (this.currentQuestionIndex < totalQuestions) {
+      const nextQuestionIndex = this.currentQuestionIndex;
+      const newUrl = `${QuizRoutes.QUESTION}${encodeURIComponent(this.quizId)}/${nextQuestionIndex + 1}`;
+      console.log('New URL:', newUrl);
   
-          // Initiate the navigation
-          await this.router.navigate([newUrl]);
-          console.log('Navigation initiated successfully.');
-          return true; // Navigation succeeded
-        } catch (error) {
-          console.error('Navigation initiation error:', error);
-          return false; // Navigation initiation error
-        }
-      } else {
-        // Handle the end of the quiz, e.g., navigate to the results page
-        this.router.navigate([`/results/${this.quizId}`]);
-        return false; // End of quiz reached
+      try {
+        // Use Router events to track navigation success or failure
+        const navigationSubscription = this.router.events
+          .pipe(filter(event => event instanceof NavigationEnd || event instanceof NavigationError || event instanceof NavigationCancel))
+          .subscribe(async (event) => { // Mark the subscribe callback as async
+            if (event instanceof NavigationEnd) {
+              console.log('Navigation successful.');
+              navigationSubscription.unsubscribe(); // Unsubscribe to prevent memory leaks
+            } else if (event instanceof NavigationError) {
+              console.error('Navigation error:', event.error);
+              navigationSubscription.unsubscribe(); // Unsubscribe on error
+            } else if (event instanceof NavigationCancel) {
+              console.warn('Navigation canceled.');
+              navigationSubscription.unsubscribe(); // Unsubscribe on cancel
+            }
+          });
+  
+        // Initiate the navigation
+        await this.router.navigate([newUrl]);
+        console.log('Navigation initiated successfully.');
+        return true; // Navigation succeeded
+      } catch (error) {
+        console.error('Navigation initiation error:', error);
+        return false; // Navigation initiation error
       }
-    });
+    } else {
+      // Handle the end of the quiz, e.g., navigate to the results page
+      this.router.navigate([`/results/${this.quizId}`]);
+      return false; // End of quiz reached
+    }
   }
-  
   
   navigateToPreviousQuestion() {
     this.quizCompleted = false;
