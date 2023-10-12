de47e11e649ddfd669d176b3f0b52e04b3fbfdf5
marvinrusinek <marvinrusinek@gmail.com>
2023-10-12 18:45:20 -0400

    1 file modified

--- src/app/containers/quiz/quiz.component.ts
+++ src/app/containers/quiz/quiz.component.ts
@@ -1158,78 +1158,116 @@ export class QuizComponent implements OnInit, OnDestroy {
       console.warn('Navigation already in progress. Aborting.');
       return;
     }
-
-    this.isNavigating = true;   // prevent multiple navigations
-
+  
+    // Prevent multiple navigations
+    this.isNavigating = true;
+  
     try {
+      console.log('Advance to Next Question Clicked');
+  
       if (!this.selectedQuiz) {
+        console.log(
+          'Advance to Next Question Aborted: Selected Quiz is not available.'
+        );
         return;
       }
-
+  
+      // Start animation or any other operations
+      console.log('Advance to Next Question Clicked');
       this.animationState$.next('animationStarted');
+  
+      console.log(
+        'Current Question Index (Before Advancing):',
+        this.currentQuestionIndex
+      );
+      console.log('Selected Quiz:', this.selectedQuiz);
+  
       this.onAnswerSelectedOrNextQuestionClicked();
-
-      const totalQuestions: number = await this.quizService.getTotalQuestions().toPromise();
-      
+  
       // Check if it's the last question
+      const totalQuestions: number = await this.quizService
+        .getTotalQuestions()
+        .toPromise();
+      console.log('Total Questions:', totalQuestions);
+  
       if (this.currentQuestionIndex >= totalQuestions) {
+        // navigate to the results page
         this.router.navigate([`${QuizRoutes.RESULTS}${this.quizId}`]);
         console.log('End of quiz reached.');
         return;
       }
-
+  
+      // Increment the currentQuestionIndex manually
       this.currentQuestionIndex++;
-
+  
       // Fetch the current question with explanation
       const { nextQuestion, explanationText } =
         await this.quizService.getNextQuestionWithExplanation();
-
+  
       // Clear explanation text for the current question
       this.clearExplanationText();
-
+  
       // Use the getQuestionTextForIndex method to fetch the question text
-      const nextQuestionText = this.quizService.getQuestionTextForIndex(this.currentQuestionIndex);
-      console.log('Next Question Text:>', nextQuestionText);
-
+      const nextQuestionText = this.quizService.getQuestionTextForIndex(
+        this.currentQuestionIndex
+      );
+  
       // Update the text for the next question
-      this.nextQuestionText = nextQuestionText || nextQuestion.questionText;
-
+      this.nextQuestionText = nextQuestionText;
+  
       // Set the explanation text for the next question
       this.explanationTextService.setNextExplanationText(explanationText);
       this.explanationTextService.setIsExplanationTextDisplayed(false);
-
+  
       // Notify any subscribers of the next question
       this.quizService.setCurrentQuestionAndNext(nextQuestion, explanationText);
       this.quizService.nextQuestionSource.next(nextQuestion);
-
+  
       // Fetch options for the next question
-      this.currentOptions = await this.quizService.getNextOptions(this.currentQuestionIndex);
-
+      this.currentOptions = await this.quizService.getNextOptions(
+        this.currentQuestionIndex
+      );
+  
       // Log to verify if options are set correctly
       console.log('Current Options:', this.currentOptions);
-
+  
       // Construct the URL for the next question
       const nextQuestionIndex = this.currentQuestionIndex + 1;
       console.log('Next Question Index:', nextQuestionIndex);
-
+  
       const newUrl = `${QuizRoutes.QUESTION}${encodeURIComponent(
         this.quizId
       )}/${nextQuestionIndex}`;
-      
+      console.log('New URL:', newUrl);
+  
+      // Update the current question index in the service
+      console.log(
+        'Before updating current question index:',
+        this.currentQuestionIndex
+      );
       this.quizService.updateCurrentQuestionIndex(this.currentQuestionIndex);
-      
+      console.log(
+        'After updating current question index:',
+        this.currentQuestionIndex
+      );
+  
       // Navigate to the new URL
+      console.log('Before navigation:', this.router.url);
       await this.router.navigateByUrl(newUrl);
+      console.log('After navigation:', this.router.url);
+  
+      console.log('Navigation completed successfully.');
     } catch (error) {
       console.error(
         'Error occurred while advancing to the next question:',
         error
       );
     } finally {
+      // Ensure that isNavigating is always set to false
       this.isNavigating = false;
     }
   }
-
+  
   advanceToPreviousQuestion() {
     this.answers = [];
     this.status = QuizStatus.CONTINUE;
