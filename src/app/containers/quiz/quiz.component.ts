5ef164c0891413ec70a60320b736d3644aa29357
marvinrusinek <marvinrusinek@gmail.com>
2023-10-30 19:47:12 -0400

    1 file modified

--- src/app/containers/quiz/quiz.component.ts
+++ src/app/containers/quiz/quiz.component.ts
@@ -1351,17 +1351,16 @@ export class QuizComponent implements OnInit, OnDestroy {
             
   async advanceToFirstQuestion(): Promise<void> {
     try {
-      if (this.currentQuestionIndex === 0) {
-        const firstQuestionOptions = await this.quizService.getOptionsForFirstQuestion(this.quizId);
-        if (firstQuestionOptions.length > 0) {
-          this.optionsToDisplay = firstQuestionOptions;
-          const firstQuestionText = await this.quizService.getQuestionTextForIndex(0);
-          this.questionToDisplay = firstQuestionText;
-          this.quizService.previousQuestionTextSubject.next(firstQuestionText);
-          this.quizService.previousOptionsSubject.next(this.optionsToDisplay);
-        } else {
-          console.error('Failed to retrieve options for the first question.');
-        }
+      const firstQuestionOptions = await this.quizService.getOptionsForFirstQuestion(this.quizId);
+      
+      if (firstQuestionOptions.length > 0) {
+        this.optionsToDisplay = firstQuestionOptions;
+        const firstQuestionText = await this.quizService.getQuestionTextForIndex(0);
+        this.questionToDisplay = firstQuestionText;
+        this.quizService.previousQuestionTextSubject.next(firstQuestionText);
+        this.quizService.previousOptionsSubject.next(this.optionsToDisplay);
+      } else {
+        console.error('Failed to retrieve options for the first question.');
       }
     } catch (error) {
       console.error('Error while fetching options for the first question:', error);
