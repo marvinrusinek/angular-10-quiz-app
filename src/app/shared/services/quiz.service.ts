ae51db472f7ed2e4ed4587d2e1dc18ad70528346
marvinrusinek <marvinrusinek@gmail.com>
2023-11-03 10:36:37 -0400

    2 files modified

--- src/app/shared/services/quiz.service.ts
+++ src/app/shared/services/quiz.service.ts
@@ -175,16 +175,6 @@ export class QuizService implements OnDestroy {
   private nextOptionsSubject = new BehaviorSubject<Option[]>(null);
   nextOptions$ = this.nextOptionsSubject.asObservable();
 
-  previousQuestionSubject = new BehaviorSubject<QuizQuestion | null>(null);
-  private previousQuestionSource = new BehaviorSubject<QuizQuestion | null>(null);
-  previousQuestion$ = this.previousQuestionSubject.asObservable();
-
-  previousOptionsSubject = new BehaviorSubject<Option[]>([]);
-  previousOptions$ = this.previousOptionsSubject.asObservable();
-
-  previousQuestionTextSubject = new BehaviorSubject<string>('');
-  previousQuestionText$ = this.previousQuestionTextSubject.asObservable();
-
   private currentQuizSubject = new BehaviorSubject<Quiz>(null);
   currentQuiz$ = this.currentQuizSubject.asObservable();
 
