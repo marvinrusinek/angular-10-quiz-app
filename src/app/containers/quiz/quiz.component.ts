7bbfda517da1e25951fad8b6f254a0a88988adea
marvinrusinek <marvinrusinek@gmail.com>
2023-11-05 12:01:24 -0500

    1 file modified

--- src/app/containers/quiz/quiz.component.ts
+++ src/app/containers/quiz/quiz.component.ts
@@ -1369,7 +1369,7 @@ export class QuizComponent implements OnInit, OnDestroy {
     await this.router.navigateByUrl(newUrl);
   }
 
-  async calculateAndSetCorrectAnswersText(question: any, options: Option[]): Promise<void> {
+  async calculateAndSetCorrectAnswersText(question: QuizQuestion, options: Option[]): Promise<void> {
     const multipleAnswers = this.quizStateService.isMultipleAnswer(question);
     if (multipleAnswers) {
       const numCorrectAnswers = this.quizQuestionManagerService.calculateNumberOfCorrectAnswers(options);
