870ffe57967d61b582fb73a3fe101a63264f0046
marvinrusinek <marvinrusinek@gmail.com>
2023-11-12 13:51:12 -0500

    1 file modified

--- src/app/containers/quiz/quiz-content/codelab-quiz-content.component.ts
+++ src/app/containers/quiz/quiz-content/codelab-quiz-content.component.ts
@@ -187,13 +187,13 @@ export class CodelabQuizContentComponent
       this.formattedExplanation = '';
     });
 
-    /* this.formattedExplanation$
+    this.formattedExplanation$
       .pipe(
         distinctUntilChanged(),
         takeUntil(this.destroy$)
       )
       .subscribe((formattedExplanation) => {
-        // Get the current question index directly from your service or wherever it's stored
+        // Fetch the current question index directly from your service or wherever it's stored
         const currentQuestionIndex = this.quizService.getCurrentQuestionIndex();
 
         console.log('Received new formatted explanation:', formattedExplanation);
@@ -205,32 +205,15 @@ export class CodelabQuizContentComponent
           this.formattedExplanation = formattedExplanation;
 
           // Update your explanation text service or any other logic here
+          this.explanationTextService.updateFormattedExplanation(
+            currentQuestionIndex,
+            formattedExplanation
+          );
 
           // Optional: Log or check other parts of your logic
           console.log('Formatted explanation updated for question index:', currentQuestionIndex);
         }
-      }); */
-
-      const questionIndex$ = of(this.quizService.getCurrentQuestionIndex());
-
-      this.formattedExplanation$
-        .pipe(
-          withLatestFrom(questionIndex$),
-          distinctUntilChanged(),
-          map(([formattedExplanation, questionIndex]) => ({ questionIndex, formattedExplanation })),
-          takeUntil(this.destroy$)
-        )
-        .subscribe(({ questionIndex, formattedExplanation }) => {
-          // Update your explanation text service or any other logic here
-          console.log('Received new formatted explanation:', formattedExplanation);
-          console.log('Current question index:', questionIndex);
-
-          // Call a method or update a property in your service to store the explanation
-          this.explanationTextService.updateFormattedExplanation(questionIndex, formattedExplanation);
-
-          // Optional: Log or check other parts of your logic
-          console.log('Formatted explanation updated for question index:', questionIndex);
-        });
+      });
   }
 
   ngOnChanges(): void {
