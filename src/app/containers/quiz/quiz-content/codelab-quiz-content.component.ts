5b5d7724934f5146bce43418637f3e5a2ea988f5
marvinrusinek <marvinrusinek@gmail.com>
2023-11-18 14:50:47 -0500

    1 file modified

--- src/app/containers/quiz/quiz-content/codelab-quiz-content.component.ts
+++ src/app/containers/quiz/quiz-content/codelab-quiz-content.component.ts
@@ -16,6 +16,7 @@ import {
   of,
   Subject,
   Subscription,
+  zip
 } from 'rxjs';
 import {
   distinctUntilChanged,
@@ -573,36 +574,33 @@ export class CodelabQuizContentComponent
     );
 
     if (!this.explanationTextService.formattedExplanationsDictionary) {
-      console.error('Formatted Explanations Dictionary is not properly initialized.');
-      return;
+        console.error('Formatted Explanations Dictionary is not properly initialized.');
+        return;
     }
-  
+
     const currentQuestionKey = `Q${this.currentQuestionIndexValue + 1}`;
-  
+
     // Log the dictionary and check if the observable is present
     console.log('Formatted Explanations Dictionary:', this.explanationTextService.formattedExplanationsDictionary);
-  
-    // Delay execution to give time for the dictionary entry to be properly initialized
-    setTimeout(() => {
-      if (this.explanationTextService.formattedExplanationsDictionary[currentQuestionKey]) {
-        const observable = this.explanationTextService.formattedExplanationsDictionary[currentQuestionKey];
-  
-        // Log the observable and check if it's defined
-        console.log(`Observable for ${currentQuestionKey}:`, observable);
-  
-        observable.subscribe(
-          (explanation) => {
-            console.log(`Unique Explanation for ${currentQuestionKey}:`, explanation);
-            // Update your UI to display the unique explanation text
-          },
-          (error) => {
-            console.error(`Error in observable for key ${currentQuestionKey}:`, error);
-          }
-        );
-      } else {
+
+    // Extract the observable and delay execution until it's available
+    const observable = this.explanationTextService.formattedExplanationsDictionary[currentQuestionKey];
+
+    if (observable && typeof observable.pipe === 'function') {
+        // Use zip to wait for the observable to emit
+        zip(observable)
+            .subscribe(
+                ([explanation]) => {
+                    console.log(`Unique Explanation for ${currentQuestionKey}:`, explanation);
+                    // Update your UI to display the unique explanation text
+                },
+                (error) => {
+                    console.error(`Error in observable for key ${currentQuestionKey}:`, error);
+                }
+            );
+    } else {
         console.error(`Observable not initialized for key ${currentQuestionKey}`);
-      }
-    }, 0);
+    }
   }
 
   private setupExplanationTextSubscription(): void {
