257dd2e642dad5e93c07a7feddae698bff869bc7
marvinrusinek <marvinrusinek@gmail.com>
2023-11-21 12:55:38 -0500

    1 file modified

--- src/app/shared/services/explanation-text.service.ts
+++ src/app/shared/services/explanation-text.service.ts
@@ -144,30 +144,32 @@ export class ExplanationTextService implements OnDestroy {
     }
   }
 
+  // Initialize formattedExplanations$ if it's not already initialized
   async initializeFormattedExplanations(numQuestions: number): Promise<void> {
     if (!this.formattedExplanations$ || this.formattedExplanations$.length !== numQuestions) {
       this.formattedExplanations$ = Array.from({ length: numQuestions }, () => new BehaviorSubject<string>(''));
       console.log('Formatted Explanations Array:', this.formattedExplanations$);
     }
-  
+
     // Call formatExplanationText() for each question before proceeding
     for (let questionIndex = 0; questionIndex < numQuestions; questionIndex++) {
       await this.formatExplanationTextForInitialization(questionIndex);
     }
-  
+
     // Wait for a short delay to ensure all observables have emitted
     await new Promise(resolve => setTimeout(resolve, 0));
-  
+
     // Populate the dictionary after all Observables have emitted
     this.formattedExplanationsDictionary = {};
     this.formattedExplanations$.forEach((subject, questionIndex) => {
       const questionKey = `Q${questionIndex + 1}`;
-      this.formattedExplanationsDictionary[questionKey] = subject;
+      // Explicitly type as BehaviorSubject<string>
+      this.formattedExplanationsDictionary[questionKey] = subject as BehaviorSubject<string>;
     });
-  
+
     // Set the flag to indicate initialization is complete
     this.isInitializationComplete = true;
-  
+
     console.log('Observables after initialization:', this.formattedExplanations$);
     console.log('Dictionary after initialization:', this.formattedExplanationsDictionary);
   }
