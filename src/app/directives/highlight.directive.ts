7e75865ffdd6f50500c0b92b26289b30bb1c2319
marvinrusinek <marvinrusinek@gmail.com>
2023-11-26 16:36:58 -0500

    2 files modified

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,31 +1,29 @@
-import { Directive, ElementRef, Input, OnChanges, OnInit, Renderer2, SimpleChanges } from '@angular/core';
+import { Directive, ElementRef, Input, OnChanges, Renderer2, SimpleChanges } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
-export class HighlightDirective implements OnInit, OnChanges {
+export class HighlightDirective implements OnChanges {
   @Input() isCorrect: boolean;
-  @Input() appHighlightInputType: string;
-
-  private isCheckboxOrRadioButton: boolean = false;
+  private answered = false;
 
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
-  ngOnInit() {
-    this.isCheckboxOrRadioButton = this.appHighlightInputType === 'checkbox' || this.appHighlightInputType === 'radio';
-    this.applyHighlight(); // Set initial background color
-  }
-
   ngOnChanges(changes: SimpleChanges) {
-    if (changes.isCorrect && !this.isCheckboxOrRadioButton) {
+    if (changes.isCorrect || changes.answered) {
       this.applyHighlight();
     }
   }
 
   private applyHighlight() {
-    const color = this.isCorrect ? 'green' : 'white';
-    if (!this.el.nativeElement.checked) {
-      this.renderer.setStyle(this.el.nativeElement, 'background-color', color);
-    }
+    const color = this.isCorrect ? 'green' : 'red';
+    const backgroundColor = this.answered ? color : 'white';
+    this.renderer.setStyle(this.el.nativeElement, 'background-color', backgroundColor);
+  }
+
+  // Call this method when the option is clicked
+  public markAnswered() {
+    this.answered = true;
+    this.applyHighlight();
   }
 }
