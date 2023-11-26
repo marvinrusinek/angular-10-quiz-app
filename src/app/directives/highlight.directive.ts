e1c41c12342db4ea488f97da60ef0036ef6c0429
marvinrusinek <marvinrusinek@gmail.com>
2023-11-26 14:45:22 -0500

    1 file modified

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -4,43 +4,29 @@ import { Directive, ElementRef, Input, OnChanges, Renderer2, SimpleChanges } fro
   selector: '[appHighlight]'
 })
 export class HighlightDirective implements OnChanges {
-  private _isCorrect: boolean;
-  private _inputType: string;
-
-  @Input() set isCorrect(value: boolean) {
-    if (value !== undefined && value !== null) {
-      this._isCorrect = value;
-      this.applyHighlight();
-    }
-  }
-
-  @Input() set appHighlightInputType(value: string) {
-    this._inputType = value;
-  }
+  @Input() isCorrect: boolean;
+  @Input() appHighlightInputType: string;
 
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   ngOnChanges(changes: SimpleChanges) {
-    if (changes['_isCorrect'] || changes['_inputType'] || '_isCorrect' in changes) {
+    if (changes.isCorrect || changes.appHighlightInputType) {
       this.applyHighlight();
     }
   }
 
   private applyHighlight() {
-    // Check if the input is a checkbox or radio button
-    const isCheckbox = this._inputType === 'checkbox';
-    const isRadioButton = this._inputType === 'radio';
-  
-    if (this._isCorrect) {
-      if (isCheckbox) {
-        this.renderer.setStyle(this.el.nativeElement, 'background-color', this.el.nativeElement.checked ? 'green' : 'initial');
-      } else if (isRadioButton) {
-        this.renderer.setStyle(this.el.nativeElement, 'background-color', this.el.nativeElement.checked ? 'green' : 'initial');
+    const isCheckbox = this.appHighlightInputType === 'checkbox';
+    const isRadioButton = this.appHighlightInputType === 'radio';
+
+    if (this.isCorrect !== undefined && this.isCorrect !== null) {
+      const color = this.isCorrect ? 'green' : 'initial';
+
+      if (isCheckbox || isRadioButton) {
+        this.renderer.setStyle(this.el.nativeElement, 'background-color', this.el.nativeElement.checked ? color : 'initial');
       } else {
-        this.renderer.setStyle(this.el.nativeElement, 'background-color', 'green');
+        this.renderer.setStyle(this.el.nativeElement, 'background-color', color);
       }
-    } else {
-      this.renderer.setStyle(this.el.nativeElement, 'background-color', 'red');
     }
   }
 }
