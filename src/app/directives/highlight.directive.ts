07509b38b4d04eb4d49342630ba923a25fc4e7be
marvinrusinek <marvinrusinek@gmail.com>
2023-11-26 14:16:23 -0500

    1 file modified

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -5,22 +5,38 @@ import { Directive, ElementRef, Input, OnChanges, Renderer2, SimpleChanges } fro
 })
 export class HighlightDirective implements OnChanges {
   private _isCorrect: boolean;
+  private _inputType: string;
 
   @Input() set isCorrect(value: boolean) {
     this._isCorrect = value;
     this.applyHighlight();
   }
 
+  @Input() set appHighlightInputType(value: string) {
+    this._inputType = value;
+  }
+
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   ngOnChanges(changes: SimpleChanges) {
-    if (changes['_isCorrect']) {
+    if (changes['_isCorrect'] || changes['_inputType']) {
       this.applyHighlight();
     }
   }
 
   private applyHighlight() {
-    const color = this._isCorrect ? 'green' : 'red';
-    this.renderer.setStyle(this.el.nativeElement, 'background-color', color);
+    // Check if the input is a checkbox or radio button
+    const isCheckbox = this._inputType === 'checkbox';
+    const isRadioButton = this._inputType === 'radio';
+
+    if (this._isCorrect) {
+      if (isCheckbox || (isRadioButton && this.el.nativeElement.checked)) {
+        this.renderer.setStyle(this.el.nativeElement, 'background-color', 'green');
+      } else {
+        this.renderer.setStyle(this.el.nativeElement, 'background-color', 'initial');
+      }
+    } else {
+      this.renderer.setStyle(this.el.nativeElement, 'background-color', 'red');
+    }
   }
 }
