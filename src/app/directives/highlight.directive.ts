1979454f4d99dcc04e85c33660b70796c8142e45
marvinrusinek <marvinrusinek@gmail.com>
2023-11-26 16:01:46 -0500

    1 file modified

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,34 +1,29 @@
-import { Directive, ElementRef, Input, OnChanges, Renderer2, SimpleChanges } from '@angular/core';
+import { Directive, ElementRef, Input, OnChanges, OnInit, Renderer2, SimpleChanges } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
-export class HighlightDirective implements OnChanges {
+export class HighlightDirective implements OnInit, OnChanges {
   @Input() isCorrect: boolean;
   @Input() appHighlightInputType: string;
 
+  private isCheckboxOrRadioButton: boolean = false;
+
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
+  ngOnInit() {
+    this.isCheckboxOrRadioButton = this.appHighlightInputType === 'checkbox' || this.appHighlightInputType === 'radio';
+    this.applyHighlight();
+  }
+
   ngOnChanges(changes: SimpleChanges) {
-    console.log('Directive ngOnChanges called', changes);
-    if (changes.isCorrect || changes.appHighlightInputType) {
+    if (changes.isCorrect && !this.isCheckboxOrRadioButton) {
       this.applyHighlight();
     }
   }
 
   private applyHighlight() {
-    console.log('Applying highlight with isCorrect:', this.isCorrect);
-    const isCheckbox = this.appHighlightInputType === 'checkbox';
-    const isRadioButton = this.appHighlightInputType === 'radio';
-
-    if (this.isCorrect !== undefined && this.isCorrect !== null) {
-      const color = this.isCorrect ? 'green' : 'red';
-
-      if (isCheckbox || isRadioButton) {
-        this.renderer.setStyle(this.el.nativeElement, 'background-color', this.el.nativeElement.checked ? color : 'white');
-      } else {
-        this.renderer.setStyle(this.el.nativeElement, 'background-color', color);
-      }
-    }
+    const color = this.isCorrect ? 'green' : 'white';
+    this.renderer.setStyle(this.el.nativeElement, 'background-color', color);
   }
 }
