8cbf4ab271377ff46412215e96f462996d78dbd6
marvinrusinek <marvinrusinek@gmail.com>
2023-11-26 16:46:39 -0500

    1 file modified

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,29 +1,19 @@
-import { Directive, ElementRef, Input, OnChanges, Renderer2, SimpleChanges } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
-export class HighlightDirective implements OnChanges {
+export class HighlightDirective {
   @Input() isCorrect: boolean;
-  private answered = false;
 
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes.isCorrect || changes.answered) {
-      this.applyHighlight();
-    }
+  @HostListener('click') onClick() {
+    this.applyHighlight();
   }
 
   private applyHighlight() {
-    const color = this.isCorrect ? 'green' : 'red';
-    const backgroundColor = this.answered ? color : 'white';
-    this.renderer.setStyle(this.el.nativeElement, 'background-color', backgroundColor);
-  }
-
-  // Call this method when the option is clicked
-  public markAnswered() {
-    this.answered = true;
-    this.applyHighlight();
+    const color = this.isCorrect ? '#006400' : '#ff0000';
+    this.renderer.setStyle(this.el.nativeElement, 'background-color', color);
   }
 }
