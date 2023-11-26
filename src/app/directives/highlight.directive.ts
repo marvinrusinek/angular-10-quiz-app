8f20ad98a75aa6a0fd6377c41af40881cdea53a7
marvinrusinek <marvinrusinek@gmail.com>
2023-11-26 17:28:28 -0500

    1 file modified

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -6,6 +6,10 @@ import { Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/
 export class HighlightDirective {
   @Input() isCorrect: boolean;
 
+  @Input() set appHighlightInputType(value: string) {
+    this.appHighlightInputType = value;
+  }
+
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   @HostListener('click') onClick() {
@@ -13,7 +17,20 @@ export class HighlightDirective {
   }
 
   private applyHighlight() {
-    const color = this.isCorrect ? '#43e756' : '#ff0000';
-    this.renderer.setStyle(this.el.nativeElement, 'background-color', color);
-  }
+    const isCheckbox = this.appHighlightInputType === 'checkbox';
+    const isRadioButton = this.appHighlightInputType === 'radio';
+  
+    if (this.isCorrect !== undefined && this.isCorrect !== null) {
+      const color = this.isCorrect ? '#43f756' : '#ff0000';
+  
+      if (isCheckbox || isRadioButton) {
+        this.renderer.setStyle(this.el.nativeElement, 'background-color', this.el.nativeElement.checked ? color : 'white');
+      } else {
+        this.renderer.setStyle(this.el.nativeElement, 'background-color', color);
+      }
+    } else {
+      // Reset background color to white if not supposed to be highlighted
+      this.renderer.setStyle(this.el.nativeElement, 'background-color', 'white');
+    }
+  }   
 }
