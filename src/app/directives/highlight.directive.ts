1792e6cdafa5f75e27c31f7b59e9ae2545168444
marvinrusinek <marvinrusinek@gmail.com>
2023-11-26 11:51:09 -0500

    1 file modified

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,14 +1,21 @@
-import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, Input, Renderer2, OnChanges, SimpleChanges } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
-export class HighlightDirective {
+export class HighlightDirective implements OnChanges {
   @Input() isCorrect: boolean = false;
 
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
-  ngOnChanges() {
+  ngOnChanges(changes: SimpleChanges) {
+    // Check if the 'isCorrect' input property changed
+    if (changes['isCorrect']) {
+      this.applyHighlight();
+    }
+  }
+
+  private applyHighlight() {
     if (this.isCorrect) {
       this.renderer.setStyle(this.el.nativeElement, 'background-color', 'green');
     } else {
