9f4238073d6377764f6254ffd11e1cd3b69f7155
marvinrusinek <marvinrusinek@gmail.com>
2023-11-26 11:06:44 -0500

    1 file modified

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,22 +1,20 @@
+// highlight.directive.ts
 import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
-  @Input('appHighlight') isCorrect: boolean; // Input to determine correctness
+  @Input() isCorrect: boolean = false;
 
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
-  ngOnInit() {
-    this.highlight();
-  }
-
-  private highlight() {
+  ngOnChanges() {
+    // Implement your logic to highlight the element based on the 'isCorrect' input.
     if (this.isCorrect) {
-      this.renderer.addClass(this.el.nativeElement, 'correct-highlight');
+      this.renderer.setStyle(this.el.nativeElement, 'background-color', 'green');
     } else {
-      this.renderer.addClass(this.el.nativeElement, 'wrong-highlight');
+      this.renderer.setStyle(this.el.nativeElement, 'background-color', 'red');
     }
   }
 }
