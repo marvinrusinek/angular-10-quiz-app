387a80bc327aa20c7213b0427ab2ecbbad7b2b33
marvinrusinek <marvinrusinek@gmail.com>
2023-11-26 12:08:13 -0500

    2 files modified

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -9,17 +9,13 @@ export class HighlightDirective implements OnChanges {
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   ngOnChanges(changes: SimpleChanges) {
-    // Check if the 'isCorrect' input property changed
     if (changes['isCorrect']) {
       this.applyHighlight();
     }
   }
 
   private applyHighlight() {
-    if (this.isCorrect) {
-      this.renderer.setStyle(this.el.nativeElement, 'background-color', 'green');
-    } else {
-      this.renderer.setStyle(this.el.nativeElement, 'background-color', 'red');
-    }
+    const color = this.isCorrect ? 'green' : 'red';
+    this.renderer.setStyle(this.el.nativeElement, 'background-color', color);
   }
 }
