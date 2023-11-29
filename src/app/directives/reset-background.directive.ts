7a43ba375f60bce65bb4eeba0f11fe24cd70d44d
marvinrusinek <marvinrusinek@gmail.com>
2023-11-28 20:10:36 -0500

    1 file modified

--- src/app/directives/reset-background.directive.ts
+++ src/app/directives/reset-background.directive.ts
@@ -1,16 +1,20 @@
 import { Directive, Input, ElementRef, Renderer2, OnChanges, SimpleChanges } from '@angular/core';
-import { ResetBackgroundService } from '../shared/services/reset-background.service';
 import { Subscription } from 'rxjs';
 
+import { ResetBackgroundService } from '../shared/services/reset-background.service';
+
 @Directive({
-  selector: '[appResetBackground]',
+  selector: '[appResetBackground]'
 })
 export class ResetBackgroundDirective implements OnChanges {
   @Input() appResetBackground: boolean;
 
   private subscription: Subscription;
 
-  constructor(private el: ElementRef, private renderer: Renderer2, private resetBackgroundService: ResetBackgroundService) {
+  constructor(
+    private el: ElementRef,
+    private renderer: Renderer2,
+    private resetBackgroundService: ResetBackgroundService) {
     this.subscription = this.resetBackgroundService.shouldResetBackground$.subscribe((value) => {
       if (value) {
         this.resetBackground();
@@ -20,12 +24,20 @@ export class ResetBackgroundDirective implements OnChanges {
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['appResetBackground']) {
-      // You can perform additional actions if needed
+      console.log('ngOnChanges triggered in ResetBackgroundDirective:', changes);
+
+      // can check the new value
+      const newValue = changes['appResetBackground'].currentValue;
+      console.log('New value of appResetBackground:', newValue);
+
+      // Perform additional logic based on the new value
+      if (newValue) {
+        console.log('Additional action: appResetBackground is true');
+      }
     }
   }
 
-  private resetBackground() {
-    console.log('Resetting background color to white');
+  private resetBackground(): void {
     this.renderer.setStyle(this.el.nativeElement, 'background-color', 'white');
   }
 
