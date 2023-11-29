26bcd7ebc6599d6ef7188b72d3758ece920cf401
marvinrusinek <marvinrusinek@gmail.com>
2023-11-29 12:45:45 -0500

    1 file modified

--- src/app/containers/quiz/quiz.component.ts
+++ src/app/containers/quiz/quiz.component.ts
@@ -1339,9 +1339,11 @@ export class QuizComponent implements OnInit, OnDestroy {
           this.quizService.previousQuestionSubject.next(previousQuestion);
           this.quizService.previousOptionsSubject.next(this.currentOptions);
 
-          this.highlightDirective.reset();
-          this.resetBackgroundService.setShouldResetBackground(true);
-          this.explanationTextService.resetExplanationState();
+          setTimeout(() => {
+            this.highlightDirective.reset();
+            this.resetBackgroundService.setShouldResetBackground(true);
+            this.explanationTextService.resetExplanationState();
+          }, 0);
 
           await this.navigateToQuestion(this.currentQuestionIndex);
         } else {
