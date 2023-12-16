a0a4c5e47139a6414c588247192634ae1252ec39
marvinrusinek <marvinrusinek@gmail.com>
2023-12-16 12:53:48 -0500

    1 file modified

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1092,7 +1092,7 @@ export class QuizQuestionComponent implements OnInit, OnChanges, OnDestroy {
 
     if (typeof questionIndex !== 'number' || 
         questionIndex < 0 || 
-        questionIndex >= this.quizService.totalQuestions) {
+        questionIndex > this.quizService.totalQuestions) {
       console.warn(`Invalid question index: ${questionIndex}`);
       return of('Invalid question index');
     }
