dfa4faaf6d70efc8b2212af368ffb794257950d3
marvinrusinek <marvinrusinek@gmail.com>
2023-12-20 16:09:24 -0500

    1 file modified

--- src/app/shared/models/FormattedExplanation.model.ts
+++ src/app/shared/models/FormattedExplanation.model.ts
@@ -1,4 +1,12 @@
-export interface FormattedExplanation {
-  questionIndex: number;
-  explanation: string;
+import { Option } from './Option.model';
+import { QuizQuestion } from './QuizQuestion.model';
+
+export interface CombinedDataType {
+  questionText: string;
+  explanationText?: string;
+  correctAnswersText?: string;
+  currentQuestion: QuizQuestion;
+  currentOptions: Option[];
+  isNavigatingToPrevious: boolean;
+  formattedExplanation?: string;
 }
