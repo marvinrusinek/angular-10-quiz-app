70e12f8dc288f885853e957cb7b55d5b42dea09e
marvinrusinek <marvinrusinek@gmail.com>
2023-12-23 17:45:29 -0500

    1 file modified

--- src/app/containers/scoreboard/scoreboard.component.ts
+++ src/app/containers/scoreboard/scoreboard.component.ts
@@ -62,7 +62,7 @@ export class ScoreboardComponent implements OnInit, OnChanges, OnDestroy {
     });
 
     // Subscribe to quizService.totalQuestions$ here
-    this.quizService.totalQuestions$.pipe(
+    /* this.quizService.totalQuestions$.pipe(
       takeUntil(this.unsubscribe$)
     ).subscribe((totalQuestions) => {
       this.totalQuestions = totalQuestions;
@@ -70,7 +70,7 @@ export class ScoreboardComponent implements OnInit, OnChanges, OnDestroy {
       if (this.questionNumber !== undefined) {
         this.updateBadgeText(this.questionNumber, totalQuestions); // Update badgeText here
       }
-    });
+    }); */
   }
   
   
