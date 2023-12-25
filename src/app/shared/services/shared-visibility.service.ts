d99e98484870bd7ed1be87072879d33e593c45c8
marvinrusinek <marvinrusinek@gmail.com>
2023-12-25 11:18:35 -0500

    1 file added

---
+++ src/app/shared/services/shared-visibility.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SharedVisibilityService {
+  private isPageHidden = false;
+  private pageVisibilitySubject = new Subject<boolean>();
+
+  pageVisibility$ = this.pageVisibilitySubject.asObservable();
+
+  constructor() {
+    // Add the visibility change event listener in the constructor
+    document.addEventListener('visibilitychange', () => {
+      this.isPageHidden = document.hidden;
+      this.pageVisibilitySubject.next(this.isPageHidden);
+    });
+  }
+
+  isPageHiddenNow() {
+    return this.isPageHidden;
+  }
+}
