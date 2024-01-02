11338b85902659d220e41c5e29e3393b2f9fa04d
marvinrusinek <marvinrusinek@gmail.com>
2024-01-02 16:03:10 -0500

    1 file modified

--- src/app/shared/services/quiz.service.ts
+++ src/app/shared/services/quiz.service.ts
@@ -1381,15 +1381,9 @@ export class QuizService implements OnDestroy {
   /********* reset functions ***********/
   resetQuestions(): void {
     let currentQuizData = this.quizInitialState.find(quiz => quiz.quizId === this.quizId);
-    if (currentQuizData) {
-      this.quizData = [_.cloneDeep(currentQuizData)]; // Ensure it's an array
-      this.questions = _.cloneDeep(currentQuizData.questions);
-    } else {
-      // Handle case where currentQuizData is not found
-      this.quizData = [];
-      this.questions = [];
-    }
-  }  
+    this.quizData = _.cloneDeep(currentQuizData);
+    this.questions = _.cloneDeep(currentQuizData.questions);
+  }
 
   resetUserSelection(): void {
     this.selectedOption$.next('');
