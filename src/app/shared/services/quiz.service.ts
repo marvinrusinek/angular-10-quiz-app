4f2e5336bb8507b76a9c41359404d8c98df71679
marvinrusinek <marvinrusinek@gmail.com>
2024-01-02 12:14:49 -0500

    1 file modified

--- src/app/shared/services/quiz.service.ts
+++ src/app/shared/services/quiz.service.ts
@@ -1374,7 +1374,7 @@ export class QuizService implements OnDestroy {
   resetQuestions(): void {
     this.quizData = _.cloneDeep(this.quizInitialState);
     this.questions = _.cloneDeep(this.quizInitialState);
-    console.log('Questions after reset:', this.questions);
+    console.log('Questions after reset:', this.quizInitialState);
   }
 
   resetUserSelection(): void {
