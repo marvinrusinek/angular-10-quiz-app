909c19fd47e45f7a8b005b677facbe8019a99f8e
marvinrusinek <marvinrusinek@gmail.com>
2024-01-07 14:31:33 -0500

    1 file modified

--- src/app/shared/services/timer.service.ts
+++ src/app/shared/services/timer.service.ts
@@ -1,71 +1,3 @@
-/* import { Injectable, OnInit } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { shareReplay } from 'rxjs/operators';
-
-import { CountdownService } from './countdown.service';
-
-@Injectable({
-  providedIn: 'root',
-})
-export class TimerService implements OnInit {
-  timePerQuestion = 20;
-  elapsedTime = 0;
-  elapsedTimes: number[] = [];
-  completionTime: number;
-  timeLeft = 0;
-
-  start$: Observable<number>;
-  reset$: Observable<number>;
-  stop$: Observable<number>;
-  timer: Observable<number>;
-  isStart = new BehaviorSubject<number>(0);
-  isStop = new BehaviorSubject<number>(1);
-  isReset = new BehaviorSubject<number>(1);
-  isTimerStart = false;
-
-  constructor(private countdownService: CountdownService) {
-    this.start$ = this.isStart.asObservable().pipe(shareReplay(1));
-    this.reset$ = this.isReset.asObservable();
-    this.stop$ = this.isStop.asObservable();
-  }
-
-  ngOnInit() {
-    this.countdownService.timeLeft$.subscribe((timeLeft) => {
-      this.timeLeft = timeLeft;
-    });
-  }
-
-  stopTimer(callback: (elapsedTime: number) => void): void {
-    if (!this.isTimerStart) {
-      return;
-    }
-    this.isTimerStart = false;
-    this.timePerQuestion = 0;
-    this.isStop.next(1);
-    this.elapsedTimes.push(this.elapsedTime);
-  }
-
-  resetTimer(): void {
-    if (!this.isTimerStart) {
-      this.isTimerStart = true;
-      this.isStart.next(1);
-    }
-    this.isTimerStart = true;
-    this.isReset.next(1);
-  }
-
-  setElapsed(time: number): void {
-    this.elapsedTime = time;
-  }
-
-  calculateTotalElapsedTime(elapsedTimes: number[]): number {
-    if (elapsedTimes.length > 0) {
-      this.completionTime = elapsedTimes.reduce((acc, cur) => acc + cur, 0);
-      return this.completionTime;
-    }
-  }
-} */
-
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, timer } from 'rxjs';
 import { shareReplay, takeUntil, takeWhile, tap } from 'rxjs/operators';
@@ -112,7 +44,6 @@ export class TimerService {
   }
 
   stopTimer(callback: (elapsedTime: number) => void): void {
-    console.log('Before stopping timer, isTimerRunning:', this.isTimerRunning);
     if (!this.isTimerRunning) {
       console.log('Timer is not running, returning.');
       return;
@@ -129,7 +60,6 @@ export class TimerService {
   }
 
   resetTimer(): void {
-    console.log('Resetting timer');
     if (this.isTimerRunning) {
       this.stopTimer(null);
     }
@@ -158,9 +88,7 @@ export class TimerService {
   }
 
   ngOnDestroy(): void {
-    if (this.timerSubscription) {
-      this.timerSubscription.unsubscribe();
-    }
+    this.timerSubscription?.unsubscribe();
   }
 }
 
