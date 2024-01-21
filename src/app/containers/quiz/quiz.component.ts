01f9a7529066803d370c02b4c646c79736bbc1f8
marvinrusinek <marvinrusinek@gmail.com>
2024-01-21 13:32:43 -0500

    1 file modified

--- src/app/containers/quiz/quiz.component.ts
+++ src/app/containers/quiz/quiz.component.ts
@@ -1256,7 +1256,7 @@ export class QuizComponent implements OnInit, OnDestroy {
 
     // Fetch the explanation text
     const explanation = await firstValueFrom(
-        this.explanationTextService.getExplanationTextForQuestionIndex(2)
+        this.explanationTextService.getExplanationTextForQuestionIndex(questionIndex)
     );
 
     let question: QuizQuestion = { questionText, options, explanation, type: null };
