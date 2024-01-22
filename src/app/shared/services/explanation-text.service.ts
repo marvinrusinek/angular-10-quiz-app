f437bad25605faf74b30585643b282ac6083e0c6
marvinrusinek <marvinrusinek@gmail.com>
2024-01-22 17:03:38 -0500

    1 file modified

--- src/app/shared/services/explanation-text.service.ts
+++ src/app/shared/services/explanation-text.service.ts
@@ -101,24 +101,6 @@ export class ExplanationTextService implements OnDestroy {
     return of({ questionIndex, explanation: formattedExplanation });
   }
 
-  private formatExplanation(question: QuizQuestion, correctOptionIndices: number[]): string {
-    if (correctOptionIndices.length > 1) {
-      question.type = QuestionType.MultipleAnswer;
-      
-      // Join all but the last index with ', ', and the last one with ' and '
-      let optionsText = correctOptionIndices.length > 2 
-        ? `${correctOptionIndices.slice(0, -1).join(', ')} and ${correctOptionIndices.slice(-1)}` 
-        : correctOptionIndices.join(' and ');
-  
-      return `Options ${optionsText} are correct because ${question.explanation}`;
-    } else if (correctOptionIndices.length === 1) {
-      question.type = QuestionType.SingleAnswer;
-      return `Option ${correctOptionIndices[0]} is correct because ${question.explanation}`;
-    } else {
-      return 'No correct option selected...';
-    }
-  }  
-  
   private syncFormattedExplanationState(questionIndex: number, formattedExplanation: string): void {
     if (!this.formattedExplanations$[questionIndex]) {
       // Initialize the BehaviorSubject if it doesn't exist at the specified index
@@ -138,6 +120,24 @@ export class ExplanationTextService implements OnDestroy {
       console.error(`No element at index ${questionIndex} in formattedExplanations$`);
     }
   }
+
+  private formatExplanation(question: QuizQuestion, correctOptionIndices: number[]): string {
+    if (correctOptionIndices.length > 1) {
+      question.type = QuestionType.MultipleAnswer;
+      
+      // Join all but the last index with ', ', and the last one with ' and '
+      let optionsText = correctOptionIndices.length > 2 
+        ? `${correctOptionIndices.slice(0, -1).join(', ')} and ${correctOptionIndices.slice(-1)}` 
+        : correctOptionIndices.join(' and ');
+  
+      return `Options ${optionsText} are correct because ${question.explanation}`;
+    } else if (correctOptionIndices.length === 1) {
+      question.type = QuestionType.SingleAnswer;
+      return `Option ${correctOptionIndices[0]} is correct because ${question.explanation}`;
+    } else {
+      return 'No correct option selected...';
+    }
+  }  
 
   private isQuestionValid(question: QuizQuestion): boolean {
     return question && question.questionText && !this.processedQuestions.has(question.questionText);
