a1d974832a7012c3fdbfccb6a068eb01f17330ce
marvinrusinek <marvinrusinek@gmail.com>
2024-01-21 19:48:21 -0500

    1 file modified

--- src/app/shared/services/quiz-resolver.service.ts
+++ src/app/shared/services/quiz-resolver.service.ts
@@ -1,34 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
 import { firstValueFrom } from 'rxjs';
-import { catchError, tap, throwError } from 'rxjs/operators';
 
-import { QuizData } from '../../shared/models/QuizData.model';
+import { QuizQuestion } from '../../shared/models/QuizQuestion.model';
 import { ExplanationTextService } from './explanation-text.service';
 import { QuizService } from './quiz.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class QuizResolverService implements Resolve<any> {
+export class QuizResolverService implements Resolve<QuizQuestion[]> {
   constructor(
     private quizService: QuizService,
     private explanationTextService: ExplanationTextService
   ) {}
 
-  async resolve(route: ActivatedRouteSnapshot): Promise<any> {
+  async resolve(route: ActivatedRouteSnapshot): Promise<QuizQuestion[]> {
     const quizId = route.params['quizId'];
-    const quizDataObservable = this.quizService.getQuestionsForQuiz(quizId).pipe(
-      tap((quizData: QuizData) => {
-        const explanations = quizData.questions.map(question => question.explanation);
-        this.explanationTextService.initializeExplanationTexts(explanations);
-        console.log('Resolver initialized explanations:', explanations);
-      }),
-      catchError((error: Error) => {
-        console.error('Resolver error:', error);
-        return throwError(error);
-      })
-    );
-    return await firstValueFrom(quizDataObservable);
+    const response = await firstValueFrom(this.quizService.getQuestionsForQuiz(quizId));
+    const questions = response.questions;
+
+    // Initialize explanations
+    const explanations = questions.map(question => question.explanation);
+    this.explanationTextService.initializeExplanationTexts(explanations);
+
+    return questions;
   }
 }
