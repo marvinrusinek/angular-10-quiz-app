0818a5649ae6b1f082c519cda99f4f915cbc7f69
marvinrusinek <marvinrusinek@gmail.com>
2024-02-19 19:55:37 -0500

    1 file modified

--- src/app/shared/services/selection-message.service.ts
+++ src/app/shared/services/selection-message.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class SelectionMessageService {
   private selectionMessageSubject: BehaviorSubject<string> = new BehaviorSubject<string>('');
   selectionMessage$: Observable<string> = this.selectionMessageSubject.asObservable();
