b8ac354968029c599985e90d7e10dfe139e9b5a3
marvinrusinek <marvinrusinek@gmail.com>
2024-03-02 18:02:26 -0500

    1 file modified

--- src/app/containers/quiz/quiz.component.ts
+++ src/app/containers/quiz/quiz.component.ts
@@ -1344,7 +1344,9 @@ export class QuizComponent implements OnInit, OnDestroy {
       console.warn('No explanation text found for question at index:', questionIndex);
     }
 
-    let question: QuizQuestion = { questionText, options, explanation, type: this.quizService.question.type };
+    const type = this.quizService.question.type;
+
+    let question: QuizQuestion = { questionText, options, explanation, type };
 
     this.quizDataService.setQuestionType(question);
 
