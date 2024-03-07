e684070d1a79c30d6b3e5e68dbb90ef5e3e78120
marvinrusinek <marvinrusinek@gmail.com>
2024-03-07 14:09:24 -0500

    1 file modified

--- src/app/containers/quiz/quiz.component.ts
+++ src/app/containers/quiz/quiz.component.ts
@@ -1235,6 +1235,23 @@ export class QuizComponent implements OnInit, OnDestroy {
       this.currentQuestionIndex--;
       this.updateNavigationAndExplanationState();
   
+      const questionIsAnswered = this.quizStateService.checkIfQuestionIsAnswered(this.currentQuestionIndex);
+
+      if (questionIsAnswered) {
+        this.explanationText = this.explanationTextService.getFormattedExplanationForQuestion(this.currentQuestionIndex);
+        this.shouldDisplayExplanation = true;
+        this.shouldDisplayOptions = false;
+      } else {
+        this.explanationText = '';
+        this.shouldDisplayExplanation = false;
+
+        // Make sure UI elements for answering the question are displayed
+        this.shouldDisplayOptions = true; // Assuming you use this flag to control the display of options
+
+        // Additionally, you might want to reset any selected options or answers to ensure the user is starting fresh
+        this.resetSelectedOptions();
+      }
+
       await this.fetchAndSetQuestionData(this.currentQuestionIndex);
       await this.setExplanationForQuestion(this.currentQuestionIndex);  // Set explanation text
   
