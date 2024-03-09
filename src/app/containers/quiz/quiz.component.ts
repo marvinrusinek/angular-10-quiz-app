63e3ec51d6ee53f82aff7b5f268f26a73a6fcbcf
marvinrusinek <marvinrusinek@gmail.com>
2024-03-09 12:58:37 -0500

    1 file modified

--- src/app/containers/quiz/quiz.component.ts
+++ src/app/containers/quiz/quiz.component.ts
@@ -602,17 +602,18 @@ export class QuizComponent implements OnInit, OnChanges, OnDestroy {
         .pipe(
           filter((quizzes: Quiz[]) => quizzes.length > 0),
           first()
-        .subscribe((quizzes: Quiz[]) => {
-          console.log("Quizzes data:", quizzes);
-          const currentQuiz = quizzes.find(quiz => quiz.quizId === quizId);
-          if (currentQuiz) {
-            this.quizDataService.setCurrentQuiz(currentQuiz);
-            this.currentQuiz = currentQuiz;
-            resolve();
-          } else {
-            reject(`Quiz with ID ${quizId} not found`);
-          }
-        });
+          .subscribe((quizzes: Quiz[]) => {
+            console.log("Quizzes data:", quizzes);
+            const currentQuiz = quizzes.find(quiz => quiz.quizId === quizId);
+            if (currentQuiz) {
+              this.quizDataService.setCurrentQuiz(currentQuiz);
+              this.currentQuiz = currentQuiz;
+              resolve();
+            } else {
+              reject(`Quiz with ID ${quizId} not found`);
+            }
+          })
+        )
     });
   }
   
