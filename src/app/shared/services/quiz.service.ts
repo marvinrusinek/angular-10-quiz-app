e3b18cc28df5424dfff8089454b740dc39ec3c63
marvinrusinek <marvinrusinek@gmail.com>
2024-03-11 20:04:08 -0400

    1 file modified

--- src/app/shared/services/quiz.service.ts
+++ src/app/shared/services/quiz.service.ts
@@ -2,8 +2,8 @@ import { Injectable, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { BehaviorSubject, combineLatest, firstValueFrom, from, 
-  Observable, of, Subject, Subscription } from 'rxjs';
-import { catchError, distinctUntilChanged, finalize, map, shareReplay, switchMap, takeUntil, tap, throwError } from 'rxjs/operators';
+  Observable, of, Subject, Subscription, throwError } from 'rxjs';
+import { catchError, distinctUntilChanged, finalize, map, shareReplay, switchMap, takeUntil, tap } from 'rxjs/operators';
 import { Howl } from 'howler';
 import _, { isEqual } from 'lodash';
 
