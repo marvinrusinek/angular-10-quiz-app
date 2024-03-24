dad3238ce60048b72f0867355c8d40572f47a857
marvinrusinek <marvinrusinek@gmail.com>
2024-03-24 16:54:47 -0400

    1 file modified

--- src/app/containers/quiz/quiz-content/codelab-quiz-content.component.ts
+++ src/app/containers/quiz/quiz-content/codelab-quiz-content.component.ts
@@ -5,7 +5,7 @@ import { ActivatedRoute, NavigationEnd, ParamMap, Router } from '@angular/router
 import { BehaviorSubject, combineLatest, firstValueFrom, forkJoin, Observable, of,
   Subject, Subscription } from 'rxjs';
 import { catchError, distinctUntilChanged, filter, map, mergeMap, startWith, switchMap,
-  take, takeUntil, tap, withLatestFrom } from 'rxjs/operators';
+  take, takeUntil, withLatestFrom } from 'rxjs/operators';
 
 import { CombinedQuestionDataType } from '../../../shared/models/CombinedQuestionDataType.model';
 import { Option } from '../../../shared/models/Option.model';
