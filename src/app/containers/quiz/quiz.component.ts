dcc11c1c22e878dd914b7a31c36e90c77ef6234b
marvinrusinek <marvinrusinek@gmail.com>
2024-03-27 19:57:46 -0400

    1 file modified

--- src/app/containers/quiz/quiz.component.ts
+++ src/app/containers/quiz/quiz.component.ts
@@ -1701,6 +1701,7 @@ export class QuizComponent implements OnInit, OnChanges, OnDestroy {
     // Step 1: Reset quiz-specific states and services
     this.quizService.resetAll();
     this.currentQuestionIndex = 0;  // Reset to the first question's index
+    this.progressPercentage = 0; // Reset the progressPercentage to 0
     this.explanationTextService.resetExplanationText();  // Clears any existing explanation text
 
     // Step 2: Reset the timer synchronously
