fa8ee75af3128d7403f95afa0671bbc7688a6e71
marvinrusinek <marvinrusinek@gmail.com>
2024-03-30 18:24:04 -0400

    1 file modified

--- src/app/containers/quiz/quiz.component.ts
+++ src/app/containers/quiz/quiz.component.ts
@@ -828,6 +828,7 @@ export class QuizComponent implements OnInit, OnChanges, OnDestroy {
   } */
 
   initializeFirstQuestion(): void {
+    console.log("Initializing first question...");
     this.resetQuestionState();
     
     this.quizDataService.getQuestionsForQuiz(this.quizId).pipe(take(1)).subscribe({
