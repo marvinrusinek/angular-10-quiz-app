4118cc223d31d1827acb7f66271888212ac2df97
marvinrusinek <marvinrusinek@gmail.com>
2024-04-03 15:59:53 -0400

    1 file modified

--- src/app/shared/services/quiz.service.ts
+++ src/app/shared/services/quiz.service.ts
@@ -674,7 +674,7 @@ export class QuizService implements OnDestroy {
       }
 
       // Validate the index
-      if (index >= 0 && index < questions.length) {
+      if (index >= 0 && index <= questions.length) {
         this.currentQuestionIndex = index;
         this.currentQuestionIndexSource.next(index);
         this.setCurrentQuestion(questions[index]);
