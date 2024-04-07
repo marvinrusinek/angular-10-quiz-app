a1cc7eb8334d05035a152549bd9334efbaf98ccf
marvinrusinek <marvinrusinek@gmail.com>
2024-04-07 18:38:24 -0400

    1 file modified

--- src/app/shared/services/selection-message.service.ts
+++ src/app/shared/services/selection-message.service.ts
@@ -3,7 +3,7 @@ import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class SelectionMessageService {
-  selectionMessageSubject: BehaviorSubject<string> = new BehaviorSubject<string>('Please select an option to continue...');
+  selectionMessageSubject: BehaviorSubject<string> = new BehaviorSubject<string>('');
   selectionMessage$: Observable<string> = this.selectionMessageSubject.asObservable();
 
   updateSelectionMessage(message: string): void {
