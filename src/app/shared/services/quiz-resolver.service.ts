cfcea7addda4d4a5a5656e81e6aa089f2ceafe50
marvinrusinek <marvinrusinek@gmail.com>
2024-04-07 20:57:28 -0400

    1 file modified

--- src/app/shared/services/quiz-resolver.service.ts
+++ src/app/shared/services/quiz-resolver.service.ts
@@ -4,32 +4,11 @@ import { Observable, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 
 import { Quiz } from '../../shared/models/Quiz.model';
-import { QuizQuestion } from '../../shared/models/QuizQuestion.model';
-import { ExplanationTextService } from './explanation-text.service';
 import { QuizService } from './quiz.service';
 
 @Injectable({ providedIn: 'root' })
 export class QuizResolverService implements Resolve<Quiz | null> {
-  constructor(
-    private quizService: QuizService,
-    private explanationTextService: ExplanationTextService
-  ) {}
-
-  /* async resolve(route: ActivatedRouteSnapshot): Promise<QuizQuestion[]> {
-    const quizId = route.params['quizId'];
-
-    const response = await firstValueFrom(this.quizService.getQuestionsForQuiz(quizId));
-    if (!response || !Array.isArray(response.questions)) {
-      console.error('Response is invalid or questions are not available');
-      return [];
-    }
-
-    const questions = response.questions;
-    const explanations = questions.map(question => question.explanation);
-    this.explanationTextService.initializeExplanationTexts(explanations);
-
-    return questions;
-  } */
+  constructor(private quizService: QuizService) {}
 
   resolve(route: ActivatedRouteSnapshot): Observable<Quiz | null> {
     const quizId = route.params['quizId'];
