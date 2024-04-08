9c81a5a8bcd01be87369dda6f8e3ab022362c95f
marvinrusinek <marvinrusinek@gmail.com>
2024-04-07 20:00:20 -0400

    1 file modified

--- src/app/shared/services/quiz-resolver.service.ts
+++ src/app/shared/services/quiz-resolver.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
-import { firstValueFrom } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 import { QuizQuestion } from '../../shared/models/QuizQuestion.model';
 import { ExplanationTextService } from './explanation-text.service';
@@ -13,20 +13,20 @@ export class QuizResolverService implements Resolve<QuizQuestion[]> {
     private explanationTextService: ExplanationTextService
   ) {}
 
-  async resolve(route: ActivatedRouteSnapshot): Promise<QuizQuestion[]> {
+  resolve(route: ActivatedRouteSnapshot): Observable<Quiz | null> {
     const quizId = route.params['quizId'];
-    try {
-      const response = await firstValueFrom(this.quizService.getQuestionsForQuiz(quizId));
-      if (response && Array.isArray(response.questions)) {
-        const questions = response.questions;
-        const explanations = questions.map(question => question.explanation);
-        this.explanationTextService.initializeExplanationTexts(explanations);
-        return questions;
-      }
-      console.error('Response is invalid or questions are not available');
-    } catch (error) {
-      console.error('Failed to fetch quiz questions:', error);
-    }
-    return [];
+    return this.quizService.getQuizData(quizId).pipe(
+      map(response => {
+        if (response && Array.isArray(response.questions)) {
+          return response;
+        }
+        console.error('Response is invalid or questions are not available');
+        return null;  // Return null or an empty object if the response is invalid
+      }),
+      catchError(error => {
+        console.error('Failed to fetch quiz data:', error);
+        return null;  // Handle error and return null or an empty object
+      })
+    );
   }
 }
