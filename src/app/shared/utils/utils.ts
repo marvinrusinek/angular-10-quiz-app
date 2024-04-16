25e486dc946ca1e885732b1e91050ecd0c76b2f9
marvinrusinek <marvinrusinek@gmail.com>
2024-04-16 13:00:10 -0400

    1 file modified

--- src/app/shared/utils/utils.ts
+++ src/app/shared/utils/utils.ts
@@ -1,6 +1,6 @@
 export class Utils {
   // Generically shuffle arrays in-place using Durstenfeld's shuffling algorithm
-  static shuffle<T>(array: T[]): T[] {
+  static shuffleArray<T>(array: T[]): T[] {
     for (let i = array.length - 1; i >= 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];  // perform the swap
