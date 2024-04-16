c13a77418b8f5ef8ac7beeb02827495999af5ff2
marvinrusinek <marvinrusinek@gmail.com>
2024-04-16 11:39:30 -0400

    1 file modified

--- src/app/containers/introduction/introduction.component.ts
+++ src/app/containers/introduction/introduction.component.ts
@@ -128,21 +128,8 @@ export class IntroductionComponent implements OnInit, OnDestroy {
   }
   
   onChange(event: any): void {
-    // console.log(event.target);  // Check what element is targeted
-    // console.log(event.target.checked);  // Verify it logs the expected true/false
     this.isChecked.next(event.checked); // Emit the checkbox state
   }
-
-
-  /* onChange(event: Event): void {
-    console.log('Event:', event);
-    const checkbox = event.target as HTMLInputElement;
-    if (checkbox) {
-      console.log('Checkbox checked:', checkbox.checked);
-    } else {
-      console.error('Event target is undefined');
-    }
-  } */
   
   onStartQuiz(quizId: string): void {
     if (!quizId) {
