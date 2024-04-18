b5bf949f029b5e75a9a6569161d19c84f7794eae
marvinrusinek <marvinrusinek@gmail.com>
2024-04-18 12:49:53 -0400

    1 file modified

--- src/app/router/quiz-routing.module.ts
+++ src/app/router/quiz-routing.module.ts
@@ -25,7 +25,7 @@ const routes: Routes = [
   {
     path: 'question/:quizId/:questionIndex',
     component: QuizComponent,
-    canActivate: [QuizGuard],
+    // canActivate: [QuizGuard],
     resolve: { quizData: QuizResolverService },
     runGuardsAndResolvers: 'always'
   },
@@ -36,7 +36,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
+  imports: [RouterModule.forRoot(routes, { enableTracing: false })],
   exports: [RouterModule]
 })
 export class QuizRoutingModule {}
