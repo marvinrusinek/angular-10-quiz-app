71b9b67e986f1286b500b21fef3ed463a1eb335e
marvinrusinek <marvinrusinek@gmail.com>
2024-04-19 19:34:53 -0400

    1 file modified

--- src/app/containers/quiz/quiz.component.ts
+++ src/app/containers/quiz/quiz.component.ts
@@ -1193,15 +1193,19 @@ export class QuizComponent implements OnInit, OnDestroy {
   handleRouteParams(params: ParamMap): Observable<{ quizId: string; questionIndex: number; quizData: Quiz }> {
     const quizId = params.get('quizId');
     const questionIndex = parseInt(params.get('questionIndex'), 10);
+    if (isNaN(questionIndex)) {
+      throw new Error('Question index is not a valid number');
+    }
+  
     return this.quizService.getQuizData(quizId).pipe(
-      map(quizData => ({
-        quizId,
-        questionIndex,
-        quizData  // Ensure quizData is of type Quiz
-      }))
+      map(quizData => {
+        if (!quizData || !quizData.questions) {
+          throw new Error('Quiz data is missing or incorrectly formatted');
+        }
+        return { quizId, questionIndex, quizData };
+      })
     );
   }
-  
 
   private handleQuizData(
     quiz: Quiz,
