d7802147d033672a240ebb99751fdb5f6bba55f1
marvinrusinek <marvinrusinek@gmail.com>
2024-04-21 17:00:11 -0400

    1 file modified

--- src/app/containers/quiz/quiz.component.ts
+++ src/app/containers/quiz/quiz.component.ts
@@ -1204,48 +1204,6 @@ export class QuizComponent implements OnInit, OnDestroy {
     }
   }
 
-  /* handleRouteParams(params: ParamMap): Observable<
-    { quizId: string; questionIndex: number;
-      quizData: { quizId: string; questions: QuizQuestion[] } }> {
-    const quizId = params.get('quizId');
-    const questionIndex = parseInt(params.get('questionIndex'), 10);
-
-    return this.quizService.getQuestionsForQuiz(quizId).pipe(
-      map((quizData: { quizId: string; questions: QuizQuestion[] }) => ({
-        quizId,
-        questionIndex,
-        quizData
-      }))
-    );
-  } */
-
-  /* handleRouteParams(params: ParamMap): Observable<{ quizId: string; questionIndex: number; quizData: Quiz }> {
-    const quizId = params.get('quizId');
-    const questionIndex = parseInt(params.get('questionIndex'), 10);
-    return this.quizService.getQuizData().pipe(
-      map(quizzes => {
-        const quizData = quizzes.find(quiz => quiz.quizId === quizId);
-        if (!quizData || !quizData.questions) {
-          throw new Error('Quiz data is missing or incorrectly formatted');
-        }
-        return { quizId, questionIndex, quizData };
-      })
-    );
-  } */
-
-  /* handleRouteParams(params: ParamMap): Observable<{ quizId: string; questionIndex: number; quizData: Quiz }> {
-    const quizId = params.get('quizId');
-    const questionIndex = parseInt(params.get('questionIndex'), 10);
-    return this.quizService.getQuizData(quizId).pipe(  // Assuming getQuizData now accepts a quizId
-        map(quizData => {
-            if (!quizData || !quizData.questions) {
-                throw new Error('Quiz data is missing or incorrectly formatted');
-            }
-            return { quizId, questionIndex, quizData };
-        })
-    );
-  } */
-
   handleRouteParams(params: ParamMap): Observable<{ quizId: string; questionIndex: number; quizData: Quiz }> {
     const quizId = params.get('quizId');
     const questionIndex = parseInt(params.get('questionIndex'), 10);
