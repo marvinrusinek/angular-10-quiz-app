8a9e21578047f35a423ebb3e1ee62dd1b94d3a8c
marvinrusinek <marvinrusinek@gmail.com>
2024-04-21 10:26:15 -0400

    1 file modified

--- src/app/shared/services/quiz-resolver.service.ts
+++ src/app/shared/services/quiz-resolver.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
-import { Observable, of } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 import { Quiz } from '../../shared/models/Quiz.model';
 import { QuizService } from './quiz.service';
@@ -12,23 +11,12 @@ export class QuizResolverService implements Resolve<Quiz | null> {
 
   resolve(route: ActivatedRouteSnapshot): Observable<Quiz | null> {
     const quizId = route.params['quizId'];
-
-    /* return this.quizService.getQuizData().pipe(
-      map(quizzes => {
-        console.log("Quizzes loaded:", quizzes);
-        return quizzes.find(quiz => quiz.quizId === quizId) || null;
-      }),
-      catchError(error => {
-        console.error(`Error resolving quiz data for ID ${quizId}:`, error);
-        return of(null);
-      })
-    ); */
-
     if (!quizId) {
       console.error('Quiz ID is missing in resolver');
       throw new Error('Quiz ID is required');
     }
+
     console.log('Resolving quiz data for ID:', quizId);
-    return this.quizService.getQuizData(quizId);
+    return this.quizService.getQuizData();
     }
 }
