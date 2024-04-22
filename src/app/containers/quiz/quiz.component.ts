854309b441113e7452c15599f82e38c5a7064bb4
marvinrusinek <marvinrusinek@gmail.com>
2024-04-22 12:17:34 -0400

    1 file modified

--- src/app/containers/quiz/quiz.component.ts
+++ src/app/containers/quiz/quiz.component.ts
@@ -322,7 +322,7 @@ export class QuizComponent implements OnInit, OnDestroy {
   }
 
   updateQuestionDisplayForShuffledQuestions(): void {
-    this.questionToDisplay = this.questions[this.currentQuestionIndex];
+    this.questionToDisplay = this.questions[this.currentQuestionIndex].questionText;
   }
 
   testSubscribeToQuestions(): void {
