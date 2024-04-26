205e092b6b36172876f25fa1bce6a3089a037901
marvinrusinek <marvinrusinek@gmail.com>
2024-04-26 17:56:48 -0400

    1 file modified

--- src/app/shared/services/quizdata.service.ts
+++ src/app/shared/services/quizdata.service.ts
@@ -352,40 +352,6 @@ export class QuizDataService implements OnDestroy {
     );
   }
 
-  /* getQuizQuestionByIdAndIndex(
-    quiz$: Observable<Quiz[]>,
-    quizId: string,
-    questionIndex: number = 0
-  ): Observable<QuizQuestion> {
-    const quizId$ = this.activatedRoute.params.pipe(
-      map((params) => params.quizId),
-      filter((quizId) => !!quizId),
-      take(1)
-    );
-
-    return quizId$.pipe(
-      switchMap((id) => {
-        return quiz$.pipe(
-          map((quizzes) => {
-            return quizzes.find((q: Quiz) => q.quizId === id);
-          }),
-          take(1),
-          switchMap((quiz) => {
-            return this.getQuestionFromQuiz(quiz, questionIndex);
-          })
-        );
-      }),
-      catchError((error: HttpErrorResponse) => {
-        console.log('Error:', error);
-        return of(null);
-      }),
-      distinctUntilChanged(
-        (prev, curr) => JSON.stringify(prev) === JSON.stringify(curr)
-      ),
-      shareReplay({ bufferSize: 1, refCount: true })
-    );
-  } */
-
   getQuizQuestionByIdAndIndex(quiz$: Observable<Quiz[]>, quizId: string, questionIndex: number): Observable<QuizQuestion | null> {
     return quiz$.pipe(
       map(quizzes => quizzes.find(quiz => quiz.quizId === quizId)),
