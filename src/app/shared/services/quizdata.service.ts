82e30f47b559dee542af3fae9811d6e1bd11f1c1
marvinrusinek <marvinrusinek@gmail.com>
2024-04-27 18:30:28 -0400

    1 file modified

--- src/app/shared/services/quizdata.service.ts
+++ src/app/shared/services/quizdata.service.ts
@@ -297,13 +297,13 @@ export class QuizDataService implements OnDestroy {
 
     // Fetch new data from the API
     return this.fetchQuizDataFromAPI().pipe(
-      switchMap(quizData => {
+      switchMap((quizData: any) => {
         if (!quizData || !Array.isArray(quizData) || quizData.length === 0) {
           console.error('Quiz data is empty, null, or improperly formatted');
           return throwError(() => new Error('Invalid or empty quiz data'));
         }
 
-        const quiz = quizData.find(quiz => quiz.quizId === quizId);
+        const quiz = quizData.find((quiz: Quiz) => quiz.quizId === quizId);
         if (!quiz) {
           console.error(`No quiz found for the quiz ID: '${quizId}'.`);
           return throwError(() => new Error(`Quiz not found for ID: ${quizId}`));
