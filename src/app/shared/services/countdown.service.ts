31194ba5c27e49632a27c7c838b06359de893b82
marvinrusinek <marvinrusinek@gmail.com>
2024-05-08 15:57:41 -0400

    1 file modified

--- src/app/shared/services/countdown.service.ts
+++ src/app/shared/services/countdown.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Input } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, of, timer } from 'rxjs';
 import { first, repeatWhen, scan, shareReplay, skip, switchMapTo, takeUntil, tap } from 'rxjs/operators';
 
