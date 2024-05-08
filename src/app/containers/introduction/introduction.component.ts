972167fac69a49edfbb9423f2f86e3f4667fbcf6
marvinrusinek <marvinrusinek@gmail.com>
2024-05-08 18:42:19 -0400

    1 file modified

--- src/app/containers/introduction/introduction.component.ts
+++ src/app/containers/introduction/introduction.component.ts
@@ -6,7 +6,7 @@ import {
   OnInit
 } from '@angular/core';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
-import { BehaviorSubject, of, Subject, Subscription, throwError } from 'rxjs';
+import { BehaviorSubject, of, Subject, throwError } from 'rxjs';
 import { catchError, switchMap, takeUntil, tap, withLatestFrom } from 'rxjs/operators';
 
 import { Quiz } from '../../shared/models/Quiz.model';
