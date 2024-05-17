b489094f4e0501904feac3acab6e63faa18afe3a
marvinrusinek <marvinrusinek@gmail.com>
2024-05-17 12:50:58 -0400

    1 file modified

--- src/app/shared/services/quiz-resolver.service.ts
+++ src/app/shared/services/quiz-resolver.service.ts
@@ -68,10 +68,12 @@ export class QuizResolverService implements Resolve<Quiz | null> {
       tap(quiz => {
         if (!quiz) {
           console.error(`Quiz with ID ${quizId} not found.`);
+          this.router.navigate(['/select']);
           throw new Error(`Quiz with ID ${quizId} not found.`);
         }
         console.log('QuizResolverService: Fetched quiz data:', quiz);
       }),
+      map(quiz => quiz as Quiz),
       catchError(error => {
         console.error('QuizResolverService: Error fetching quiz data:', error);
         this.router.navigate(['/select']);
