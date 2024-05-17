501a95364ef7570b5eff36cf5116500d02da0d3c
marvinrusinek <marvinrusinek@gmail.com>
2024-05-17 19:49:45 -0400

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+import { QuizDataService } from './shared/services/quizdata.service';
 
 @Component({
   selector: 'codelab-root',
@@ -7,4 +9,9 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent  {
+  constructor(private quizDataService: QuizDataService) {}
+
+  ngOnInit() {
+    this.quizDataService.loadQuizzesData(); // Ensure quizzes are loaded on app start
+  }
 }
