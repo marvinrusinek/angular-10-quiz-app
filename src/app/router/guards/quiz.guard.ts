e86e50f5a099911eb93e0e9f7169de769f3b865b
marvinrusinek <marvinrusinek@gmail.com>
2024-05-18 16:42:35 -0400

    1 file modified

--- src/app/router/guards/quiz.guard.ts
+++ src/app/router/guards/quiz.guard.ts
@@ -14,42 +14,6 @@ export class QuizGuard implements CanActivate {
     private router: Router
   ) {}
 
-  /* canActivate(route: ActivatedRouteSnapshot): Observable<boolean> {
-    const quizId = route.params['quizId'];
-    const questionIndex = +route.params['questionIndex'];
-
-    return this.quizDataService.isValidQuiz(quizId).pipe(
-      switchMap(isValid => {
-        if (!isValid) {
-          this.router.navigate(['/select']);
-          return of(false);
-        }
-        return this.quizDataService.getQuiz(quizId).pipe(
-          map(quiz => {
-            const totalQuestions = quiz.questions.length;
-            if (questionIndex > 0 && questionIndex <= totalQuestions) {
-              return true;
-            } else if (questionIndex > totalQuestions) {
-              this.router.navigate(['/results', quizId]);
-              return false;
-            } else {
-              this.router.navigate(['/intro', quizId]);
-              return false;
-            }
-          }),
-          catchError(() => {
-            this.router.navigate(['/select']);
-            return of(false);
-          })
-        );
-      }),
-      catchError(() => {
-        this.router.navigate(['/select']);
-        return of(false);
-      })
-    );
-  } */
-
   canActivate(route: ActivatedRouteSnapshot): Observable<boolean> {
     const quizId = route.params['quizId'];
     const questionIndex = +route.params['questionIndex'];
