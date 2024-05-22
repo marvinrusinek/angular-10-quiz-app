e7d7217d0961eac632ae1ae8d78e1d8777356f45
marvinrusinek <marvinrusinek@gmail.com>
2024-05-22 14:17:48 -0400

    1 file modified

--- src/app/shared/services/selection-message.service.ts
+++ src/app/shared/services/selection-message.service.ts
@@ -6,6 +6,16 @@ export class SelectionMessageService {
   selectionMessageSubject: BehaviorSubject<string> = new BehaviorSubject<string>('');
   selectionMessage$: Observable<string> = this.selectionMessageSubject.asObservable();
 
+  determineSelectionMessage(currentQuestionIndex: number, totalQuestions: number, isAnswered: boolean): string {
+    if (currentQuestionIndex === totalQuestions - 1) {
+      return isAnswered
+        ? 'Please click the Show Results button'
+        : 'Please select an option to continue...';
+    } else {
+      return 'Please click the next button to continue...';
+    }
+  }
+
   updateSelectionMessage(message: string): void {
     this.selectionMessageSubject.next(message);
   }
