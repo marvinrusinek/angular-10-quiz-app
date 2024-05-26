ac0ac5d43cd5cd0c64e2758e71273205994ff485
marvinrusinek <marvinrusinek@gmail.com>
2024-05-26 15:02:32 -0400

    1 file modified

--- src/app/shared/services/quiz.service.ts
+++ src/app/shared/services/quiz.service.ts
@@ -954,7 +954,6 @@ export class QuizService implements OnDestroy {
   // Method to check if the current question is answered
   isAnswered(questionIndex: number): Observable<boolean> {
     const isAnswered = this.getSelectedOptions(questionIndex).length > 0;
-    console.log(`isAnswered for questionIndex ${questionIndex}: ${isAnswered}, selectedOptions: ${JSON.stringify(this.selectedOptions)}`); // Debugging log
     return of(isAnswered);
   }
 
