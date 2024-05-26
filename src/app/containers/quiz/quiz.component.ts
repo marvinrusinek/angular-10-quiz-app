4da594acb0185abe07ad5197bdbe9800d04843cd
marvinrusinek <marvinrusinek@gmail.com>
2024-05-26 18:32:23 -0400

    1 file modified

--- src/app/containers/quiz/quiz.component.ts
+++ src/app/containers/quiz/quiz.component.ts
@@ -1355,7 +1355,7 @@ export class QuizComponent implements OnInit, OnDestroy {
       message = this.selectionMessageService.determineSelectionMessage(this.currentQuestionIndex, totalQuestions, isAnswered);
     } else {
       // If it's the first question and not answered, set the initial message
-      message = 'Please select the next button to continue...';
+      message = 'Please select an option to continue...';
     }
     
     console.log(`Determined selection message: ${message}`);
