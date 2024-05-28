b2692606427a4aa17c29259635df678eeb533d47
marvinrusinek <marvinrusinek@gmail.com>
2024-05-28 17:02:28 -0400

    1 file modified

--- src/app/shared/services/selection-message.service.ts
+++ src/app/shared/services/selection-message.service.ts
@@ -4,35 +4,7 @@ import { BehaviorSubject, Observable } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 export class SelectionMessageService {
   selectionMessageSubject: BehaviorSubject<string> = new BehaviorSubject<string>('');
-  // selectionMessage$: Observable<string> = this.selectionMessageSubject.asObservable();
-
-  get selectionMessage$(): Observable<string> {
-    return this.selectionMessageSubject.asObservable();
-  }
-
-  /* determineSelectionMessage(currentQuestionIndex: number, totalQuestions: number, isAnswered: boolean): string {
-    if (currentQuestionIndex === totalQuestions - 1) {
-      return isAnswered
-        ? 'Please click the Show Results button'
-        : 'Please select an option to continue...';
-    } else {
-      return 'Please click the next button to continue...';
-    }
-  } */
-
-  /* determineSelectionMessage(
-    currentQuestionIndex: number,
-    totalQuestions: number,
-    isAnswered: boolean
-  ): string {
-    if (!isAnswered) {
-      return 'Please select an option to continue...';
-    } else if (currentQuestionIndex < totalQuestions - 1) {
-      return 'Please click the next button to continue...';
-    } else {
-      return 'You have completed the quiz!';
-    }
-  } */
+  selectionMessage$: Observable<string> = this.selectionMessageSubject.asObservable();
 
   determineSelectionMessage(currentQuestionIndex: number, totalQuestions: number, isAnswered: boolean): string {
     if (currentQuestionIndex === totalQuestions - 1) {
