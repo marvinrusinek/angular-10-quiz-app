0f81917c95b19a6a4f2abfc9cd58085a6a78faba
marvinrusinek <marvinrusinek@gmail.com>
2024-05-29 19:24:58 -0400

    1 file modified

--- src/app/containers/quiz/quiz-content/codelab-quiz-content.component.ts
+++ src/app/containers/quiz/quiz-content/codelab-quiz-content.component.ts
@@ -121,7 +121,8 @@ export class CodelabQuizContentComponent implements OnInit, OnChanges, OnDestroy
               const validIndex = questionIndex < data.questions.length ? questionIndex : 0;
               console.log(`Using question index: ${validIndex}`);
 
-              const question = data.questions[validIndex];
+              const questions: QuizQuestion[] = data.questions;
+              const question = questions[validIndex];
               console.log('Retrieved question:', question);
 
               if (this.quizService.isValidQuizQuestion(question)) {
