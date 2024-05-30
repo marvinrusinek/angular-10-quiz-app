46d182ef978c9366bbb912d153a27d7fbd22ebc2
marvinrusinek <marvinrusinek@gmail.com>
2024-05-29 20:04:37 -0400

    1 file modified

--- src/app/containers/quiz/quiz-content/codelab-quiz-content.component.ts
+++ src/app/containers/quiz/quiz-content/codelab-quiz-content.component.ts
@@ -117,13 +117,17 @@ export class CodelabQuizContentComponent implements OnInit, OnChanges, OnDestroy
             console.log('Received data from service:', data);
 
             if (data && data.questions && data.questions.length > 0) {
-              console.log('Questions array length:', data.questions.length);
+              console.log('Full quiz data:', data); // Log full quiz data
               const validIndex = questionIndex < data.questions.length ? questionIndex : 0;
               console.log(`Using question index: ${validIndex}`);
 
               const questions: QuizQuestion[] = data.questions;
               const question = questions[validIndex];
               console.log('Extracted question:', question);
+
+              // Detailed logging of individual question
+              console.log('Question questionText:', question.questionText);
+              console.log('Question options:', question.options);
 
               if (this.quizService.isValidQuizQuestion(question)) {
                 console.log('Setting current question:', question);
