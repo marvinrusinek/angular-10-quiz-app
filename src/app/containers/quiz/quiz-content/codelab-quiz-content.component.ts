1e4ec7114f1f35851aedf15cb138e1b9089dba95
marvinrusinek <marvinrusinek@gmail.com>
2024-06-05 16:25:03 -0400

    1 file modified

--- src/app/containers/quiz/quiz-content/codelab-quiz-content.component.ts
+++ src/app/containers/quiz/quiz-content/codelab-quiz-content.component.ts
@@ -122,21 +122,14 @@ export class CodelabQuizContentComponent implements OnInit, OnDestroy {
       const zeroBasedIndex = questionIndex - 1;
       this.loadQuestion(this.quizId, zeroBasedIndex);
     });
-
-    this.currentQuestion.pipe(
-      debounceTime(200),
-      tap((question: QuizQuestion | null) => {
-        this.updateCorrectAnswersDisplay(question).subscribe();
-      })
-    ).subscribe();
   }
-
+  
   loadQuestion(quizId: string, zeroBasedIndex: number): void {
     this.quizDataService.getQuestionsForQuiz(quizId).subscribe(questions => {
       if (questions && questions.length > 0 && zeroBasedIndex >= 0 && zeroBasedIndex < questions.length) {
         const question = questions[zeroBasedIndex];
-        this.currentQuestion.next(question);
         this.isExplanationDisplayed = false; // Reset explanation display state
+        this.currentQuestion.next(question);
         this.updateCorrectAnswersDisplay(question).subscribe(() => {
           this.fetchAndDisplayExplanationText(question);
         });
