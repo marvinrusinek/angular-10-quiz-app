16e009924d627cb6c54e26c0378696a8f144ec34
marvinrusinek <marvinrusinek@gmail.com>
2024-06-08 15:24:34 -0400

    2 files modified

--- src/app/containers/quiz/quiz-content/codelab-quiz-content.component.ts
+++ src/app/containers/quiz/quiz-content/codelab-quiz-content.component.ts
@@ -388,6 +388,7 @@ export class CodelabQuizContentComponent implements OnInit, OnDestroy {
   }
 
 
+
   /* private async fetchAndDisplayExplanationText(question: QuizQuestion): Promise<void> {
     if (!question || !question.questionText) {
       console.error('Question is undefined or missing questionText');
@@ -769,6 +770,7 @@ export class CodelabQuizContentComponent implements OnInit, OnDestroy {
 
     return of(combinedQuestionData);
   }
+
     
   handleQuestionDisplayLogic(): void {
     this.combinedQuestionData$.pipe(
