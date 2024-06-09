4c318837d663b2f77e52c6aa09944cecec469dcb
marvinrusinek <marvinrusinek@gmail.com>
2024-06-09 19:11:40 -0400

    1 file modified

--- src/app/containers/quiz/quiz-content/codelab-quiz-content.component.ts
+++ src/app/containers/quiz/quiz-content/codelab-quiz-content.component.ts
@@ -165,7 +165,7 @@ export class CodelabQuizContentComponent implements OnInit, OnDestroy {
     });
   } */
 
-  loadQuestion(quizId: string, zeroBasedIndex: number): void {
+  /* loadQuestion(quizId: string, zeroBasedIndex: number): void {
     this.quizDataService.getQuestionsForQuiz(quizId).subscribe(questions => {
       if (questions && questions.length > 0 && zeroBasedIndex >= 0 && zeroBasedIndex < questions.length) {
         const question = questions[zeroBasedIndex];
@@ -187,7 +187,39 @@ export class CodelabQuizContentComponent implements OnInit, OnDestroy {
         console.error('Invalid question index:', zeroBasedIndex);
       }
     });
+  } */
+
+  private loadQuestion(quizId: string, zeroBasedIndex: number): void {
+    this.quizDataService.getQuestionsForQuiz(quizId).subscribe(questions => {
+      if (questions && questions.length > 0 && zeroBasedIndex >= 0 && zeroBasedIndex < questions.length) {
+        const question = questions[zeroBasedIndex];
+        this.currentQuestion.next(question);
+        this.isExplanationDisplayed = false; // Reset explanation display state
+
+        // Ensure isExplanationTextDisplayed$ is defined before subscribing
+        if (this.isExplanationTextDisplayed$) {
+          this.updateCorrectAnswersDisplay(question).subscribe(() => {
+            // Fetch and display explanation text
+            this.fetchAndDisplayExplanationText(question);
+          });
+
+          // Subscribe to isExplanationTextDisplayed$
+          this.isExplanationTextDisplayed$.subscribe(isDisplayed => {
+            this.isExplanationDisplayed = isDisplayed;
+            if (isDisplayed) {
+              this.correctAnswersTextSource.next('');
+              console.log('Explanation displayed, resetting correctAnswersTextSource.');
+            }
+          });
+        } else {
+          console.error('isExplanationTextDisplayed$ is not initialized.');
+        }
+      } else {
+        console.error('Invalid question index:', zeroBasedIndex);
+      }
+    });
   }
+
 
   initializeSubscriptions(): void {
     this.initializeQuestionIndexSubscription();
