29c11ad07ce429eabcbb57d1670506284d12ebdc
marvinrusinek <marvinrusinek@gmail.com>
2024-06-13 17:00:24 -0400

    1 file modified

--- src/app/containers/quiz/quiz-content/codelab-quiz-content.component.ts
+++ src/app/containers/quiz/quiz-content/codelab-quiz-content.component.ts
@@ -697,7 +697,7 @@ export class CodelabQuizContentComponent implements OnInit, OnDestroy {
   
         return this.calculateCombinedQuestionData(
           currentQuizData, 
-          numberOfCorrectAnswers, 
+          +numberOfCorrectAnswers, 
           isExplanationDisplayed, 
           formattedExplanation
         );
