879ab711f726c87a230740a150259faa1738e569
marvinrusinek <marvinrusinek@gmail.com>
2024-06-18 17:37:35 -0400

    1 file modified

--- src/app/shared/services/selectedoption.service.ts
+++ src/app/shared/services/selectedoption.service.ts
@@ -4,6 +4,20 @@ import { BehaviorSubject } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 export class SelectedOptionService {
   private selectedOptionExplanationSource = new BehaviorSubject<string>(null);
-  selectedOptionExplanation$ =
-    this.selectedOptionExplanationSource.asObservable();
+  selectedOptionExplanation$ = this.selectedOptionExplanationSource.asObservable();
+
+  private optionSelectedSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+
+  // Observable to get the current option selected state
+  get isOptionSelected$(): Observable<boolean> {
+    return this.optionSelectedSubject.asObservable();
+  }
+
+  // Method to set the option selected state
+  setOptionSelected(isSelected: boolean): void {
+    console.log('[setOptionSelected] Updating option selected state:', isSelected);
+    if (this.optionSelectedSubject.value !== isSelected) {
+      this.optionSelectedSubject.next(isSelected);
+    }
+  }
 }
