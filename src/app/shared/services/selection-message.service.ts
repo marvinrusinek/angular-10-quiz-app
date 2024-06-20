e77e1610781c9cd9858a010d4edcf073ce9fde74
marvinrusinek <marvinrusinek@gmail.com>
2024-06-20 18:46:05 -0400

    1 file modified

--- src/app/shared/services/selection-message.service.ts
+++ src/app/shared/services/selection-message.service.ts
@@ -10,15 +10,25 @@ export class SelectionMessageService {
     return this.selectionMessageSubject.asObservable();
   }
 
-  determineSelectionMessage(currentQuestionIndex: number, totalQuestions: number, isAnswered: boolean): string {
+  determineSelectionMessage(
+    currentQuestionIndex: number,
+    totalQuestions: number,
+    isAnswered: boolean
+  ): string {
     if (currentQuestionIndex === 0) {
-      return isAnswered ? 'Please click the next button to continue...' : 'Please start the quiz by selecting an option.';
+      return isAnswered
+        ? 'Please click the next button to continue...'
+        : 'Please start the quiz by selecting an option.';
     } else if (currentQuestionIndex === totalQuestions - 1) {
-      return isAnswered ? 'Please click the Show Results button.' : 'Please select an option to continue...';
+      return isAnswered
+        ? 'Please click the Show Results button.'
+        : 'Please select an option to continue...';
     } else {
-      return isAnswered ? 'Please click the next button to continue...' : 'Please select an option to continue...';
+      return isAnswered
+        ? 'Please click the next button to continue...'
+        : 'Please select an option to continue...';
     }
-  }
+  }  
 
   updateSelectionMessage(message: string): void {
     console.log('[updateSelectionMessage] Updating selection message:', message);
