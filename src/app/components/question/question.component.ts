2cd843ddbe406a8b3fdb7883f6c277c720f6fee6
marvinrusinek <marvinrusinek@gmail.com>
2024-06-28 14:03:07 -0400

    1 file modified

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -406,6 +406,7 @@ export class QuizQuestionComponent implements OnInit, OnChanges, OnDestroy {
         });
     }
   }
+  
 
   private async initializeQuizQuestionsAndAnswers(): Promise<void> {    
     try {
