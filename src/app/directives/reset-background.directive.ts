4a3a78bc68db2427f9f68d34dffedc2fc98c42bd
marvinrusinek <marvinrusinek@gmail.com>
2024-07-04 13:34:39 -0400

    1 file modified

--- src/app/directives/reset-background.directive.ts
+++ src/app/directives/reset-background.directive.ts
@@ -1,30 +1,37 @@
-import { Directive, Input, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, Input, ElementRef, Renderer2, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 
 import { ResetBackgroundService } from '../shared/services/reset-background.service';
+import { SelectedOptionService } from '../shared/services/selectedoption.service';
 
 @Directive({
   selector: '[appResetBackground]'
 })
-export class ResetBackgroundDirective {
+export class ResetBackgroundDirective implements OnDestroy {
   @Input() appResetBackground: boolean;
   private resetBackgroundSubscription: Subscription;
 
   constructor(
     private el: ElementRef,
     private renderer: Renderer2,
-    private resetBackgroundService: ResetBackgroundService
+    private resetBackgroundService: ResetBackgroundService,
+    private selectedOptionService: SelectedOptionService
   ) {
     this.resetBackgroundSubscription = 
       this.resetBackgroundService.shouldResetBackground$.subscribe((value) => {
         if (value) {
           this.resetBackground();
+          this.clearFeedbackIcons();
         }
       });
   }
 
   private resetBackground(): void {
     this.renderer.setStyle(this.el.nativeElement, 'background-color', 'white');
+  }
+
+  private clearFeedbackIcons(): void {
+    this.selectedOptionService.clearSelectedOption();
   }
 
   ngOnDestroy(): void {
