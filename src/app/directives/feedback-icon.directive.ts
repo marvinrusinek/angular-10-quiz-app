06abfad348a0f2c74f61d8b9f251f9d376be2ed3
marvinrusinek <marvinrusinek@gmail.com>
2024-07-03 20:07:00 -0400

    1 file added

---
+++ src/app/directives/feedback-icon.directive.ts
@@ -0,0 +1,29 @@
+import { Directive, ElementRef, Input, Renderer2, OnChanges } from '@angular/core';
+import { SelectedOption } from '../models/SelectedOption.model';
+
+@Directive({
+  selector: '[appFeedbackIcon]'
+})
+export class FeedbackIconDirective implements OnChanges {
+  @Input() option: SelectedOption;
+  @Input() selectedOptions: SelectedOption[];
+  @Input() showFeedbackForOption: { [optionId: number]: boolean };
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngOnChanges() {
+    this.updateIcon();
+  }
+
+  private updateIcon() {
+    const isSelected = this.selectedOptions.includes(this.option);
+    const showFeedback = this.showFeedbackForOption && this.showFeedbackForOption[this.option.optionId];
+
+    if (isSelected && showFeedback) {
+      const icon = this.option.correct ? 'done' : 'clear';
+      this.renderer.setProperty(this.el.nativeElement, 'innerHTML', icon);
+    } else {
+      this.renderer.setProperty(this.el.nativeElement, 'innerHTML', '');
+    }
+  }
+}
