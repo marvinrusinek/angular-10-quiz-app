9301f1d66603d6b8d8fe38ef3918c22e4c9dfae8
marvinrusinek <marvinrusinek@gmail.com>
2024-07-04 19:51:21 -0400

    1 file modified

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1363,11 +1363,14 @@ export class QuizQuestionComponent implements OnInit, OnChanges, OnDestroy {
   }
 
   selectOption(currentQuestion: QuizQuestion, option: SelectedOption, optionIndex: number): void {
-    this.selectedOptions = [{ ...option, questionIndex: this.currentQuestionIndex }];
+    /* this.selectedOptions = [{ ...option, questionIndex: this.currentQuestionIndex }];
     this.showFeedbackForOption = { [option.optionId]: true };
     this.showFeedback = true;
     this.selectedOption = option;
-    this.selectedOptionService.setSelectedOption(option as SelectedOption);
+    this.selectedOptionService.setSelectedOption(option as SelectedOption); */
+
+    const selectedOption = { ...option, optionId: index } as SelectedOption;
+    this.selectedOptionService.setSelectedOption(selectedOption);
   
     this.explanationTextService.setIsExplanationTextDisplayed(true);
   
