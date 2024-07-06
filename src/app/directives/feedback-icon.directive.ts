ca42935bcc875d096fd199b9b8834e78542d9cd3
marvinrusinek <marvinrusinek@gmail.com>
2024-07-06 10:11:37 -0400

    1 file modified

--- src/app/directives/feedback-icon.directive.ts
+++ src/app/directives/feedback-icon.directive.ts
@@ -1,51 +1,38 @@
-import { Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/core';
-
-import { Option } from '../shared/models/Option.model';
-import { SelectedOption } from '../shared/models/SelectedOption.model';
+import { Directive, ElementRef, Input, Renderer2, OnChanges, SimpleChanges } from '@angular/core';
 import { SelectedOptionService } from '../shared/services/selectedoption.service';
+import { Option } from '../shared/models/Option.model';
 
 @Directive({
   selector: '[appFeedbackIcon]'
 })
-export class HighlightOptionDirective {
+export class FeedbackIconDirective implements OnChanges {
   @Input() option: Option;
-  @Input() isCorrect: boolean;
-
-  private isAnswered = false;
-  private iconElement: HTMLElement;
+  @Input() index: number;
+  @Input() selectedOption: Option | null;
+  @Input() showFeedbackForOption: { [optionId: number]: boolean };
 
   constructor(
-    private el: ElementRef, 
-    private renderer: Renderer2, 
-    private selectedOptionService: SelectedOptionService) {
-      // Create the icon element
-      this.iconElement = this.renderer.createElement('span');
-      this.renderer.appendChild(this.el.nativeElement, this.iconElement);
+    private el: ElementRef,
+    private renderer: Renderer2,
+    private selectedOptionService: SelectedOptionService
+  ) {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.updateIcon();
   }
 
-  @HostListener('click') onClick() {
-    this.isAnswered = true;
-    this.applyIcon();
+  private updateIcon() {
+    if (this.index === undefined) {
+      return;
+    }
 
-    // Set selected option in the service
-    this.selectedOptionService.setSelectedOption(this.option as SelectedOption);
-  }
+    const isSelected = this.selectedOptionService.isSelectedOption(this.option);
 
-  private applyIcon() {
-    if (this.isAnswered) {
-      const icon = this.isCorrect ? '✔️' : '❌';
-
-      // Apply the feedback icon
+    if (isSelected) {
+      const icon = this.option.correct ? '✔️' : '❌';
       this.renderer.setProperty(this.el.nativeElement, 'innerText', icon);
     } else {
-      // Clear the feedback icon
       this.renderer.setProperty(this.el.nativeElement, 'innerText', '');
     }
-  }
-
-  // Reset the state in-between questions
-  public reset(): void {
-    this.isAnswered = false;
-    this.renderer.setProperty(this.el.nativeElement, 'innerText', '');
   }
 }
