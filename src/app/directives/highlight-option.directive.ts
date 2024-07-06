2527bde9d76e615a76640a3a4ea7a493abf4de36
marvinrusinek <marvinrusinek@gmail.com>
2024-07-06 09:49:38 -0400

    1 file modified

--- src/app/directives/highlight-option.directive.ts
+++ src/app/directives/highlight-option.directive.ts
@@ -11,33 +11,24 @@ export class HighlightOptionDirective {
   @Output() resetBackground = new EventEmitter<boolean>();
   @Input() option: Option;
   @Input() isCorrect: boolean;
-
   private isAnswered = false;
-  private iconElement: HTMLElement;
 
   constructor(
     private el: ElementRef, 
     private renderer: Renderer2, 
     private selectedOptionService: SelectedOptionService) {
-      // Create the icon element
-      this.iconElement = this.renderer.createElement('span');
-      this.renderer.appendChild(this.el.nativeElement, this.iconElement);
   }
 
   @HostListener('click') onClick() {
     this.isAnswered = true;
     this.applyHighlight();
     this.resetBackground.emit(true);
-
-    // Set selected option in the service
-    this.selectedOptionService.setSelectedOption(this.option as SelectedOption);
   }
 
   private applyHighlight() {
     if (this.isAnswered) {
       // Set the color based on whether the answer is correct
       const color = this.isCorrect ? '#43f756' : '#ff0000';
-      const icon = this.isCorrect ? '✔️' : '❌';
   
       // Apply background color to the element
       this.renderer.setStyle(
@@ -45,9 +36,6 @@ export class HighlightOptionDirective {
         'background-color',
         color
       );
-
-      // Apply the feedback icon
-      this.renderer.setProperty(this.el.nativeElement, 'innerText', icon);
     } else {
       // Reset the background color to white when not answered
       this.renderer.setStyle(
@@ -55,9 +43,6 @@ export class HighlightOptionDirective {
         'background-color',
         'white'
       );
-
-      // Clear the feedback icon
-      this.renderer.setProperty(this.el.nativeElement, 'innerText', '');
     }
   }
 
@@ -65,6 +50,5 @@ export class HighlightOptionDirective {
   public reset(): void {
     this.isAnswered = false;
     this.renderer.setStyle(this.el.nativeElement, 'background-color', 'white');
-    this.renderer.setProperty(this.el.nativeElement, 'innerText', '');
   }
 }
