ffa54faffb46413052cc8a32b2b1cbcd7d4f93da
marvinrusinek <marvinrusinek@gmail.com>
2024-07-07 17:35:49 -0400

    1 file modified

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -52,7 +52,6 @@ export class QuizQuestionComponent implements OnInit, OnChanges, OnDestroy {
   @Input() questionData: QuizQuestion;
   @Input() question!: QuizQuestion;
   @Input() question$: Observable<QuizQuestion>;
-  // @Input() questions: Observable<QuizQuestion[]>;
   @Input() questions: Observable<QuizQuestion[]>;
   @Input() options: Option[];
   @Input() optionsToDisplay: Option[];
