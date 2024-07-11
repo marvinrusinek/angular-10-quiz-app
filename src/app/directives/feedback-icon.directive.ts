0b622f26727d16b38763940eebd6cbf5082aebfe
marvinrusinek <marvinrusinek@gmail.com>
2024-07-11 17:53:27 -0400

    1 file modified

--- src/app/directives/feedback-icon.directive.ts
+++ src/app/directives/feedback-icon.directive.ts
@@ -42,9 +42,9 @@ export class FeedbackIconDirective implements OnChanges {
     const isSelected = this.selectedOptionService.isSelectedOption(this.option);
     console.log('isSelected:', isSelected, 'showFeedback:', this.showFeedbackForOption[this.option.optionId]);
 
-    // if (this.showFeedbackForOption && this.showFeedbackForOption[this.option.optionId]) {
+    if (this.showFeedbackForOption && this.showFeedbackForOption[this.option.optionId]) {
     // if (this.isAnswered && isSelected) {
-    if (this.isAnswered && this.showFeedbackForOption[this.option.optionId]) {
+    // if (this.isAnswered && this.showFeedbackForOption[this.option.optionId]) {
       const icon = this.option.correct ? '✔️' : '❌';
       console.log('Setting icon to', icon);
       this.renderer.setProperty(this.el.nativeElement, 'innerText', icon);
