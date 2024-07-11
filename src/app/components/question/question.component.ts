666522f35ca4e1f97efcadb5ac9202aea1cce838
marvinrusinek <marvinrusinek@gmail.com>
2024-07-11 14:29:50 -0400

    1 file modified

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -530,18 +530,18 @@ export class QuizQuestionComponent implements OnInit, OnChanges, OnDestroy {
 
         if (questions && questions.length > 0) {
             this.questions = of(questions);
-            
+
             // Ensure option IDs are set
             questions.forEach((question, qIndex) => {
-                console.log(`Question ${qIndex}:`, question);
-                if (question.options) {
-                    question.options.forEach((option, oIndex) => {
-                        option.optionId = oIndex;
-                        console.log(`Option ${oIndex} for Question ${qIndex}:`, option);
-                    });
-                } else {
-                    console.error(`Options are not properly defined for question: ${question.questionText}`);
-                }
+              console.log(`Question ${qIndex}:`, question);
+              if (question.options) {
+                question.options.forEach((option, oIndex) => {
+                  option.optionId = oIndex;
+                  console.log(`Option ${oIndex} for Question ${qIndex}:`, option);
+                });
+              } else {
+                console.error(`Options are not properly defined for question: ${question.questionText}`);
+              }
             });
 
             // Handle explanation texts for previously answered questions
