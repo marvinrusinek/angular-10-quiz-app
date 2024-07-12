ff8d3fd357b1cf2421bc7ff260377c4c105c4d23
marvinrusinek <marvinrusinek@gmail.com>
2024-07-12 13:29:45 -0400

    1 file modified

--- src/app/containers/quiz/quiz.component.ts
+++ src/app/containers/quiz/quiz.component.ts
@@ -39,7 +39,7 @@ type AnimationState = 'animationStarted' | 'none';
   styleUrls: ['./quiz.component.scss'],
   animations: [ChangeRouteAnimation.changeRoute],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [QuizService, QuizDataService, QuizStateService, HighlightOptionDirective]
+  providers: [QuizService, QuizDataService, QuizStateService, FeedbackIconDirective, HighlightOptionDirective]
 })
 export class QuizComponent implements OnInit, OnDestroy {
   @Input() data: {
