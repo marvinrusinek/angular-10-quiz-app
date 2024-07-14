67b3d6567c86f97592aa387db15b49227a141314
marvinrusinek <marvinrusinek@gmail.com>
2024-07-14 15:34:24 -0400

    1 file modified

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1003,7 +1003,11 @@ export class QuizQuestionComponent implements OnInit, OnChanges, OnDestroy {
       this.showFeedback = true;
       this.showFeedbackForOption = { [this.selectedOption.optionId]: true };
       this.updateFeedbackForOption(option);
-      
+
+      console.log('onOptionClicked - showFeedbackForOption:', this.showFeedbackForOption);
+
+      console.log('onOptionClicked - SelectedOption set:', this.selectedOption);
+
       // Update selected option in service
       this.updateSelectedOption(option);
       this.selectedOptionService.setOptionSelected(true);
@@ -1024,6 +1028,7 @@ export class QuizQuestionComponent implements OnInit, OnChanges, OnDestroy {
       }
 
       this.cdRef.detectChanges();
+      console.log('onOptionClicked - Change detection triggered');
   
       // Process state changes
       this.processCurrentQuestionState(currentQuestion, option, index);
