2b5c1a17ba0082e164d1a2fdc728ec8cc990fa10
marvinrusinek <marvinrusinek@gmail.com>
2024-07-18 18:26:45 -0400

    1 file modified

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -4,9 +4,6 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { BehaviorSubject, firstValueFrom, Observable, of, ReplaySubject, Subject, Subscription } from 'rxjs';
 import { catchError, debounceTime, distinctUntilChanged, filter, map, take, takeUntil, tap } from 'rxjs/operators';
 
-import { MultipleAnswerComponent } from './question-type/multiple-answer/multiple-answer.component';
-import { SingleAnswerComponent } from './question-type/single-answer/single-answer.component';
-
 import { Utils } from '../../shared/utils/utils';
 import { AudioItem } from '../../shared/models/AudioItem.model';
 import { FormattedExplanation } from '../../shared/models/FormattedExplanation.model';
@@ -24,6 +21,8 @@ import { SelectedOptionService } from '../../shared/services/selectedoption.serv
 import { SelectionMessageService } from '../../shared/services/selection-message.service';
 import { SharedVisibilityService } from '../../shared/services/shared-visibility.service';
 import { TimerService } from '../../shared/services/timer.service';
+import { MultipleAnswerComponent } from './question-type/multiple-answer/multiple-answer.component';
+import { SingleAnswerComponent } from './question-type/single-answer/single-answer.component';
 
 @Component({
   selector: 'codelab-quiz-question',
