9b92fb1e7bb2bb347c0b35ac6d1a4ce8ed37b422
marvinrusinek <marvinrusinek@gmail.com>
2024-07-17 20:23:58 -0400

    1 file modified

--- src/app/shared/services/reset-state.service.ts
+++ src/app/shared/services/reset-state.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable({ providedIn: 'root' })
 export class ResetStateService {
   private resetStateSource = new Subject<void>();
   resetState$ = this.resetStateSource.asObservable();
