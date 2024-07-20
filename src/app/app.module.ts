42c909c5017fa5a4e931038c138cfd9b140cc609
marvinrusinek <marvinrusinek@gmail.com>
2024-07-20 13:06:28 -0400

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,7 @@ import { AppComponent } from './app.component';
 import { IntroductionComponent } from './containers/introduction/introduction.component';
 import { QuizComponent } from './containers/quiz/quiz.component';
 import { QuizQuestionComponent } from './components/question/question.component';
+import { TestComponent } from './components/question/test.component';
 import { MultipleAnswerComponent } from './components/question/question-type/multiple-answer/multiple-answer.component';
 import { SingleAnswerComponent } from './components/question/question-type/single-answer/single-answer.component';
 import { FeedbackComponent } from './components/question/feedback/feedback.component';
@@ -68,6 +69,7 @@ import { QuizGuard } from './router/guards/quiz.guard';
     AppComponent,
     IntroductionComponent,
     QuizQuestionComponent,
+    TestComponent,
     QuizComponent,
     MultipleAnswerComponent,
     SingleAnswerComponent,
