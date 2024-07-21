fea8dd4a0846093e6562df4095d265e84a16e6bd
marvinrusinek <marvinrusinek@gmail.com>
2024-07-21 12:36:41 -0400

    1 file modified

--- src/app/containers/results/summary-report/summary-report.component.ts
+++ src/app/containers/results/summary-report/summary-report.component.ts
@@ -32,7 +32,7 @@ export class SummaryReportComponent implements OnInit {
   };
   elapsedMinutes: number;
   elapsedSeconds: number;
-  checkedShuffle: Observable<boolean>;
+  checkedShuffle$: Observable<boolean>;
   highScores: QuizScore[];
   codelabUrl = 'https://www.codelab.fun';
 
@@ -49,7 +49,7 @@ export class SummaryReportComponent implements OnInit {
       map((segments) => this.quizService.getQuizName(segments))
     );
     this.quizId = this.quizService.quizId;
-    this.checkedShuffle = this.quizService.checkedShuffle.asObservable();
+    this.checkedShuffle$ = this.quizService.checkedShuffle$;
     this.calculateElapsedTime();
     this.quizService.saveHighScores();
     this.highScores = this.quizService.highScores;
