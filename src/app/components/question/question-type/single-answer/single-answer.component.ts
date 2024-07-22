ee1e3c01bfde105ad0361a5287af339d67af1a99
marvinrusinek <marvinrusinek@gmail.com>
2024-07-22 17:45:17 -0400

    2 files modified

--- src/app/components/question/question-type/single-answer/single-answer.component.ts
+++ src/app/components/question/question-type/single-answer/single-answer.component.ts
@@ -142,10 +142,7 @@ export class SingleAnswerComponent extends BaseQuestionComponent implements OnIn
     this.destroyed$.complete();
   }
 
-  onOptionClicked(option: Option, index: number) {
-    super.onOptionClicked(option, index);
-    this.showFeedbackForOption[index] = true;
-  }
+  
 
   initializeFeedbackForOptions() {
     if (this.optionsToDisplay) {
