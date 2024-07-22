e46a1579514dde8bc3ee6207c64089170cf86f14
marvinrusinek <marvinrusinek@gmail.com>
2024-07-22 19:11:58 -0400

    1 file modified

--- src/app/shared/services/dynamic-component.service.ts
+++ src/app/shared/services/dynamic-component.service.ts
@@ -1,21 +1,19 @@
 import { Injectable, ComponentFactoryResolver, ViewContainerRef, Type } from '@angular/core';
-import { SingleAnswerComponent } from '../../components/question/question-type/single-answer/single-answer.component';
+
 import { MultipleAnswerComponent } from '../../components/question/question-type/multiple-answer/multiple-answer.component';
+import { SingleAnswerComponent } from '../../components/question/question-type/single-answer/single-answer.component';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DynamicComponentService {
-
   constructor(private componentFactoryResolver: ComponentFactoryResolver) {}
 
-  loadComponent(container: ViewContainerRef, component: Type<any>) {
+  loadComponent(container: ViewContainerRef, multipleAnswer: boolean): any {
+    const component = multipleAnswer ? MultipleAnswerComponent : SingleAnswerComponent;
     const componentFactory = this.componentFactoryResolver.resolveComponentFactory(component);
     container.clear();
     return container.createComponent(componentFactory);
-  }
-
-  getComponent(multipleAnswer: boolean) {
-    return multipleAnswer ? MultipleAnswerComponent : SingleAnswerComponent;
   }
 }
