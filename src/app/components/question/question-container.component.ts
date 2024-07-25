5ab05b458dc422f2b9cc17b8b46d610da21c4e66
marvinrusinek <marvinrusinek@gmail.com>
2024-07-25 18:27:23 -0400

    1 file added

---
+++ src/app/components/question/question-container.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, ViewChild, ViewContainerRef, AfterViewInit } from '@angular/core';
+import { QuizQuestion } from '../../shared/models/QuizQuestion.model';
+import { DynamicComponentService } from '../../shared/services/dynamic-component.service';
+
+@Component({
+  selector: 'app-question-container',
+  template: '<div #dynamicComponentContainer></div>',
+  styleUrls: ['./question-container.component.css']
+})
+export class QuestionContainerComponent implements AfterViewInit {
+  @ViewChild('dynamicComponentContainer', { read: ViewContainerRef, static: true }) dynamicComponentContainer!: ViewContainerRef;
+  @Input() question!: QuizQuestion;
+
+  constructor(private dynamicComponentService: DynamicComponentService) {}
+
+  ngAfterViewInit(): void {
+    this.loadDynamicComponent();
+  }
+
+  async loadDynamicComponent(): Promise<void> {
+    const hasMultipleAnswers = this.question.options.filter(option => option.correct).length > 1;
+    const componentRef = await this.dynamicComponentService.loadComponent(this.dynamicComponentContainer, hasMultipleAnswers);
+    componentRef.instance.question = this.question;
+  }
+}
