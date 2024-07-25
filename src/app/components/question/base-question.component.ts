e7c3944ffd605800b55c2684fb7108a2a26cdedf
marvinrusinek <marvinrusinek@gmail.com>
2024-07-25 18:56:39 -0400

    1 file modified, 2 deleted

--- src/app/components/question/base-question.component.ts
+++ src/app/components/question/base-question.component.ts
@@ -1,27 +1,70 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, ViewChild, ViewContainerRef, ComponentFactoryResolver, OnInit, AfterViewInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
 import { Option } from '../../shared/models/Option.model';
 import { QuizQuestion } from '../../shared/models/QuizQuestion.model';
+import { DynamicComponentService } from '../../shared/services/dynamic-component.service';
+import { MultipleAnswerComponent } from '../../components/question/question-type/multiple-answer/multiple-answer.component';
+import { SingleAnswerComponent } from '../../components/question/question-type/single-answer/single-answer.component';
 
 @Component({
-  selector: 'app-base-question',
-  templateUrl: './base-question.component.html'
+  template: ''
 })
-export class BaseQuestionComponent implements OnInit {
+export class BaseQuestionComponent implements OnInit, AfterViewInit {
+  @ViewChild('dynamicComponentContainer', { read: ViewContainerRef, static: true }) dynamicComponentContainer!: ViewContainerRef;
+
   @Input() question!: QuizQuestion;
+  multipleAnswer: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   questionForm: FormGroup;
   optionsToDisplay: Option[] = [];
 
-  constructor(protected fb: FormBuilder) {
-    this.questionForm = this.fb.group({});
+  constructor(
+    private componentFactoryResolver: ComponentFactoryResolver,
+    private fb: FormBuilder,
+    private dynamicComponentService: DynamicComponentService  // Ensure DynamicComponentService is injected here
+  ) {
+    console.log('FormBuilder instance:', this.fb);  // Debugging log to check FormBuilder instance
+    if (typeof this.fb.group !== 'function') {
+      console.error('FormBuilder group method is not a function');  // Additional check
+    } else {
+      this.questionForm = this.fb.group({});  // Initialize the form group here
+      console.log('Initialized questionForm:', this.questionForm);  // Debugging log to check form group initialization
+    }
   }
 
   ngOnInit(): void {
+    console.log('BaseQuestionComponent ngOnInit called');
+    console.log('question input:', this.question);
     if (this.question) {
       this.optionsToDisplay = this.question.options;
+      const hasMultipleAnswers = this.question.options.filter(option => option.correct).length > 1;
+      this.multipleAnswer.next(hasMultipleAnswers);
+      console.log('ngOnInit: multipleAnswer', this.multipleAnswer.value);  // Debugging log
     } else {
       console.error('Question input is undefined');
     }
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      console.log('ngAfterViewInit: dynamicComponentContainer', this.dynamicComponentContainer);
+      if (!this.dynamicComponentContainer) {
+        console.error('dynamicComponentContainer is still undefined in ngAfterViewInit');
+        return;
+      }
+      this.loadDynamicComponent();
+    }, 0);
+  }
+
+  async loadDynamicComponent(): Promise<void> {
+    console.log('Loading dynamic component with question:', this.question);
+    const hasMultipleAnswers = this.multipleAnswer.value;
+    console.log('Has multiple answers:', hasMultipleAnswers);  // Debugging log
+    const componentRef = await this.dynamicComponentService.loadComponent(this.dynamicComponentContainer, hasMultipleAnswers);
+    console.log('Component ref:', componentRef);  // Debugging log
+    componentRef.instance.questionForm = this.questionForm;
+    componentRef.instance.question = this.question;
+    componentRef.instance.optionsToDisplay = this.optionsToDisplay;
   }
 }
 
