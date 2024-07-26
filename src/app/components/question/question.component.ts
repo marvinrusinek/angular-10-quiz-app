7f8f5b565d403335acc4e1dd09e166093736f875
marvinrusinek <marvinrusinek@gmail.com>
2024-07-26 12:34:18 -0400

    1 file modified

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1056,6 +1056,8 @@ export class QuizQuestionComponent
     option: SelectedOption,
     index: number
   ): Promise<void> {
+    console.log('OPTION CLICKED:', option, index);
+
     this.showFeedbackForOption[index] = true;
     try {
       // Set selected option and show feedback
