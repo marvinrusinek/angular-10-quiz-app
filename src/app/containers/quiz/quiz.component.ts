e9d95241b4ed233428a63b07757954f0f4876146
marvinrusinek <marvinrusinek@gmail.com>
2024-07-26 13:48:24 -0400

    1 file modified

--- src/app/containers/quiz/quiz.component.ts
+++ src/app/containers/quiz/quiz.component.ts
@@ -1512,7 +1512,9 @@ export class QuizComponent implements OnInit, OnDestroy {
       this.optionsToDisplay = [...this.question.options];
       console.log('Loaded question:', this.question);
       console.log('Options to display:', this.optionsToDisplay);
-      this.cdRef.detectChanges();
+      this.ngZone.run(() => {
+        this.cdRef.detectChanges();
+      });
     } else {
       console.error('Failed to load question at index:', this.currentQuestionIndex);
     }
