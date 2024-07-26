1a58e507c6e6f4c123f8913c3fba6793777ed9f9
marvinrusinek <marvinrusinek@gmail.com>
2024-07-26 12:03:17 -0400

    1 file modified

--- src/app/components/question/base-question.component.ts
+++ src/app/components/question/base-question.component.ts
@@ -65,11 +65,20 @@ export class BaseQuestionComponent implements OnInit, AfterViewInit {
     componentRef.instance.optionsToDisplay = this.optionsToDisplay;
   }
 
-  handleOptionClick(option: SelectedOption, index: number): void {
+  /* handleOptionClick(option: SelectedOption, index: number): void {
     if (this['onOptionClicked']) {
       this['onOptionClicked'](option, index);
     } else {
       console.error('onOptionClicked method not found');
     }
+  } */
+
+  handleOptionClick(option: SelectedOption, index: number): void {
+    this.onOptionClicked(option, index);
+  }
+
+  // Abstract method to be implemented in child components
+  onOptionClicked(option: SelectedOption, index: number): void {
+    throw new Error('onOptionClicked method not implemented');
   }
 }
