1314c2cb4d70e236ed2f3e4d1abcd9b8bd398731
marvinrusinek <marvinrusinek@gmail.com>
2024-07-26 13:13:21 -0400

    1 file modified

--- src/app/shared/services/quiz.service.ts
+++ src/app/shared/services/quiz.service.ts
@@ -334,7 +334,7 @@ export class QuizService implements OnDestroy {
     this.questionsSubject.next(questions);
   }
 
-  getQuestions(index: number) {
+  getQuestion(index: number) {
     console.log("Accessing questions at index:", index);
     if (this.questions && index >= 0 && index < this.questions.length) {
       return this.questions[index];
