c23c299ee54f7e81d80ef1d39e642b4b23cd59ca
marvinrusinek <marvinrusinek@gmail.com>
2024-07-27 17:56:12 -0400

    2 files modified

--- src/app/components/question/base-question.component.ts
+++ src/app/components/question/base-question.component.ts
@@ -59,7 +59,16 @@ export abstract class BaseQuestionComponent implements OnInit, AfterViewInit {
     this.loadDynamicComponent();
   }
 
-  abstract loadDynamicComponent(): void;
+  // abstract loadDynamicComponent(): void;
+
+  loadDynamicComponent(): void {
+    console.log('QuizQuestionComponent: loadDynamicComponent called');
+    this.dynamicComponentService.loadComponent(this.dynamicComponentContainer, true).then(componentRef => {
+      componentRef.instance.questionForm = this.questionForm;
+      componentRef.instance.question = this.question;
+      componentRef.instance.optionsToDisplay = this.optionsToDisplay;
+    });
+  }
 
   protected initializeOptions(): void {
     if (this.question && this.question.options) {
