39a044647a83d51b6879e2e6e96882a83b82f5f9
marvinrusinek <marvinrusinek@gmail.com>
2024-07-27 12:15:59 -0400

    1 file modified

--- src/app/components/question/question-type/single-answer/single-answer.component.ts
+++ src/app/components/question/question-type/single-answer/single-answer.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, Optional, SkipSelf, Host } from '@angular/core';
+import { ChangeDetectorRef, Component, Optional, Inject, forwardRef } from '@angular/core';
 import { BaseQuestionComponent } from '../../base-question.component';
 import { FormBuilder } from '@angular/forms';
 
@@ -20,7 +20,7 @@ export class SingleAnswerComponent extends BaseQuestionComponent {
     protected selectedOptionService: SelectedOptionService,
     protected fb: FormBuilder,
     protected cdRef: ChangeDetectorRef,
-    @Optional() @Host() @SkipSelf() private quizQuestionComponent: QuizQuestionComponent
+    @Optional() @Inject(forwardRef(() => QuizQuestionComponent)) private quizQuestionComponent: QuizQuestionComponent
   ) {
     super(selectedOptionService, fb);
     //super();
