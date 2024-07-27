1e912fdf8ff87ad7370ac8d7288ac6469ac79fca
marvinrusinek <marvinrusinek@gmail.com>
2024-07-27 11:37:59 -0400

    2 files modified

--- src/app/components/question/question-type/shared-option.component.ts
+++ src/app/components/question/question-type/shared-option.component.ts
@@ -33,6 +33,11 @@ export class SharedOptionComponent implements OnInit {
     this.optionClicked.emit({ option, index });
   }
 
+  handleOptionClick(option: Option, index: number): void {
+    console.log('SharedOptionComponent: Option clicked:', option, index);
+    this.optionClicked.emit({ option, index });
+  }
+
   trackByOption(index: number, item: Option): number {
     return item.optionId;
   }
