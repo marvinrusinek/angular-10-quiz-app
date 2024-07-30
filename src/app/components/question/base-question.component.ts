79e61af2b0ae32d8f611c533a6242a24372b0341
marvinrusinek <marvinrusinek@gmail.com>
2024-07-30 14:30:12 -0400

    1 file modified

--- src/app/components/question/base-question.component.ts
+++ src/app/components/question/base-question.component.ts
@@ -30,7 +30,7 @@ export abstract class BaseQuestionComponent implements OnInit, OnChanges, AfterV
     protected fb: FormBuilder,
     protected dynamicComponentService: DynamicComponentService,
     protected quizStateService: QuizStateService,
-    protected selectedOptionService: SelectedOptionService,
+    protected selectedOptionService: SelectedOptionService
   ) {
     if (typeof this.fb.group !== 'function') {
       console.error('FormBuilder group method is not a function');  // Additional check
