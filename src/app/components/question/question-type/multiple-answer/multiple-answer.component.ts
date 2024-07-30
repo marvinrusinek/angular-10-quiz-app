7dfc105f8f4192a74048bf98be9a8e08639b249c
marvinrusinek <marvinrusinek@gmail.com>
2024-07-30 14:17:31 -0400

    2 files modified

--- src/app/components/question/question-type/multiple-answer/multiple-answer.component.ts
+++ src/app/components/question/question-type/multiple-answer/multiple-answer.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, Optional, Inject, forwardRef } from '@angular/core';
+import { ChangeDetectorRef, Component, forwardRef, Inject, Optional } from '@angular/core';
 import { BaseQuestionComponent } from '../../base-question.component';
 import { FormBuilder } from '@angular/forms';
 
