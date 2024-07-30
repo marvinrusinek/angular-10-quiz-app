92fb7fe0ee07c277abe7839c846e6c3852e18967
marvinrusinek <marvinrusinek@gmail.com>
2024-07-30 19:17:49 -0400

    1 file modified

--- src/app/components/question/base-question.component.ts
+++ src/app/components/question/base-question.component.ts
@@ -81,6 +81,7 @@ export abstract class BaseQuestionComponent implements OnInit, OnChanges, AfterV
         }
       });
       this.optionsToDisplay = this.question.options;
+      console.log('Options initialized:', this.optionsToDisplay);
     } else {
       console.error('Question or options are undefined');
     }
