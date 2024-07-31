42673bbef09e4116b9c7bf1a17864aae9199fa7a
marvinrusinek <marvinrusinek@gmail.com>
2024-07-31 18:41:50 -0400

    1 file modified

--- src/app/components/question/base-question.component.ts
+++ src/app/components/question/base-question.component.ts
@@ -127,7 +127,7 @@ export abstract class BaseQuestionComponent implements OnInit, OnChanges, AfterV
     }
   } */
 
-  protected initializeOptions(currentQuestion: QuizQuestion): void {
+  /* protected initializeOptions(currentQuestion: QuizQuestion): void {
     if (currentQuestion && currentQuestion.options) {
       console.log('initializeOptions - Question:', currentQuestion);
       this.questionForm = this.fb.group({});
@@ -139,6 +139,17 @@ export abstract class BaseQuestionComponent implements OnInit, OnChanges, AfterV
       this.optionsToDisplay = currentQuestion.options || [];
       console.log('initializeOptions - Options initialized:', this.optionsToDisplay);
       this.cdRef.detectChanges();
+    } else {
+      console.error('initializeOptions - Question or options are undefined', { question: currentQuestion });
+    }
+  } */
+
+  protected initializeOptions(currentQuestion: QuizQuestion): void {
+    if (currentQuestion && currentQuestion.options) {
+      console.log('initializeOptions - Question:', currentQuestion);
+      this.optionsToDisplay = currentQuestion.options;
+      console.log('initializeOptions - Options initialized:', this.optionsToDisplay);
+      this.cdRef.detectChanges(); // Ensure change detection
     } else {
       console.error('initializeOptions - Question or options are undefined', { question: currentQuestion });
     }
