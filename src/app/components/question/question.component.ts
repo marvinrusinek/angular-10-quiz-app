69024d7534119ef0f63823954e55df67ac78f624
marvinrusinek <marvinrusinek@gmail.com>
2024-07-30 22:12:11 -0400

    1 file modified

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -159,7 +159,7 @@ export class QuizQuestionComponent extends BaseQuestionComponent implements OnIn
     protected ngZone: NgZone,
     protected el: ElementRef
   ) {
-   super(fb, dynamicComponentService, selectedOptionService, quizStateService, cdRef);
+   super(fb, dynamicComponentService, quizStateService, selectedOptionService);
 
     this.questionForm = this.fb.group({});
   
