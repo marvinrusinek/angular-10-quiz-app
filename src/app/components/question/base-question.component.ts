2c261892aad995611d355f81b01ba57738917930
marvinrusinek <marvinrusinek@gmail.com>
2024-08-01 15:17:06 -0400

    1 file modified

--- src/app/components/question/base-question.component.ts
+++ src/app/components/question/base-question.component.ts
@@ -1,4 +1,134 @@
-import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild, ViewContainerRef } from '@angular/core';
+import { AfterViewInit, Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild, ViewContainerRef } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
+
+import { Option } from '../../shared/models/Option.model';
+import { QuizQuestion } from '../../shared/models/QuizQuestion.model';
+import { SelectedOption } from '../../shared/models/SelectedOption.model';
+import { DynamicComponentService } from '../../shared/services/dynamic-component.service';
+import { QuizStateService } from '../../shared/services/quizstate.service';
+import { SelectedOptionService } from '../../shared/services/selectedoption.service';
+
+@Component({
+  selector: 'app-base-question',
+  template: ''
+})
+export abstract class BaseQuestionComponent implements OnInit, OnChanges, AfterViewInit {
+  @ViewChild('dynamicComponentContainer', { read: ViewContainerRef })
+  dynamicComponentContainer!: ViewContainerRef;
+  @Input() question!: QuizQuestion;
+  @Input() optionsToDisplay!: Option[] = [];
+  @Input() correctMessage = '';
+  @Input() showFeedback = false;
+  @Input() shouldResetBackground = false;
+  @Input() type: 'single' | 'multiple' = 'single';
+  questionForm: FormGroup;
+  multipleAnswer: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  showFeedbackForOption: { [optionId: number]: boolean } = {};
+  optionsInitialized = false;
+
+  constructor(
+    protected fb: FormBuilder,
+    protected dynamicComponentService: DynamicComponentService,
+    protected quizStateService: QuizStateService,
+    protected selectedOptionService: SelectedOptionService
+  ) {
+    if (!this.fb || typeof this.fb.group !== 'function') {
+      console.error('FormBuilder group method is not a function or FormBuilder is not instantiated properly:', this.fb);
+    } else {
+      this.questionForm = this.fb.group({});
+    }
+    this.initializeOptions();
+  }
+
+  ngOnInit(): void {
+    if (this.question) {
+      this.initializeOptions();
+      this.optionsInitialized = true;
+    } else {
+      console.error('Question input is undefined');
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.question && changes.question.currentValue) {
+      this.question = changes.question.currentValue;
+      this.initializeOptions();
+      this.optionsInitialized = true;
+    } else {
+      console.error('ngOnChanges - Question or options are undefined:', changes.question);
+    }
+
+    if (changes.optionsToDisplay && changes.optionsToDisplay.currentValue) {
+      this.optionsToDisplay = changes.optionsToDisplay.currentValue;
+    }
+  }
+
+  ngAfterViewInit(): void {
+    console.log('BaseQuestionComponent ngAfterViewInit: dynamicComponentContainer', this.dynamicComponentContainer);
+    if (!this.dynamicComponentContainer) {
+      console.error('dynamicComponentContainer is still undefined in ngAfterViewInit');
+      return;
+    } else {
+      console.log('dynamicComponentContainer is defined');
+      this.loadDynamicComponent();
+    }
+  }
+
+  protected initializeOptions(): void {
+    if (this.question) {
+      console.log('initializeOptions - Question:', this.question);
+      if (this.question.options) {
+        this.question.options.forEach(option => {
+          if (!this.questionForm.contains(option.text)) {
+            this.questionForm.addControl(option.text, this.fb.control(false));
+          }
+        });
+        this.optionsToDisplay = this.question.options || [];
+        console.log('initializeOptions - Options initialized:', this.optionsToDisplay);
+      } else {
+        console.error('initializeOptions - Options are undefined', { question: this.question });
+      }
+    } else {
+      console.error('initializeOptions - Question is undefined');
+    }
+  }
+
+  protected abstract loadDynamicComponent(): void;
+
+  // Abstract method to be implemented in child components
+  protected abstract onOptionClicked(option: SelectedOption, index: number): void {
+    if (this.quizQuestionComponent) {
+      this.quizQuestionComponent.onOptionClicked(option, index);
+    } else {
+      console.error('QuizQuestionComponent is not available');
+    }
+
+    if (!this.showFeedbackForOption) {
+      console.error('showFeedbackForOption is not initialized');
+      this.showFeedbackForOption = {};
+    }
+
+    this.showFeedbackForOption[option.optionId] = true;
+    this.selectedOption = option;
+    this.showFeedback = true;
+    this.cdRef.markForCheck();
+  }
+  
+  handleOptionClick(option: SelectedOption, index: number): void {
+    if (this['onOptionClicked']) {
+      (this['onOptionClicked'] as any)(option, index);
+    } else {
+      console.error('onOptionClicked method not found');
+    }
+  }
+
+  isSelectedOption(option: Option): boolean {
+    return this.selectedOptionService.isSelectedOption(option);
+  }
+}
+
+/* import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild, ViewContainerRef } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { BehaviorSubject } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
@@ -26,7 +156,7 @@ export abstract class BaseQuestionComponent implements OnInit, OnChanges, AfterV
   @Input() shouldResetBackground = false;
   @Input() type: 'single' | 'multiple' = 'single';
   questionForm: FormGroup;
-  selectedOption: Option;
+  selectedOption!: SelectedOption;
   multipleAnswer: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   showFeedbackForOption: { [optionId: number]: boolean } = {};
   optionsInitialized = false;
@@ -165,4 +295,4 @@ export abstract class BaseQuestionComponent implements OnInit, OnChanges, AfterV
   isSelectedOption(option: Option): boolean {
     return this.selectedOptionService.isSelectedOption(option);
   }
-}
+} */
