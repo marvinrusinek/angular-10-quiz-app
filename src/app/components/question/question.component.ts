3da07cf13e6886cf9e97e86ebe83f1e2535e35dc
marvinrusinek <marvinrusinek@gmail.com>
2024-08-03 16:09:28 -0400

    3 files modified

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1047,6 +1047,7 @@ export class QuizQuestionComponent extends BaseQuestionComponent implements OnIn
       this.showFeedback = true;
       this.showFeedbackForOption = { [this.selectedOption.optionId]: true };
       this.updateFeedbackForOption(option);
+      this.correctMessage = this.setCorrectMessage();
 
       console.log(
         'onOptionClicked - showFeedbackForOption:',
