5e488c03f7603af2e04491e447448b810f4067df
marvinrusinek <marvinrusinek@gmail.com>
2024-08-04 12:03:14 -0400

    1 file modified

--- src/app/shared/services/quiz.service.ts
+++ src/app/shared/services/quiz.service.ts
@@ -1207,6 +1207,7 @@ export class QuizService implements OnDestroy {
   // Populate correctOptions when questions are loaded
   setCorrectOptions(options: Option[]): void {
     this.correctOptions = options.filter(option => option.isCorrect);
+    console.log("MY CORRECT OPTIONS", this.correctOptions);
   }
 
   setCorrectAnswers(
