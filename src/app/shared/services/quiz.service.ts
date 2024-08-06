7cf7e5a1f40fdd1cfd1441f05af3913f49fac82c
marvinrusinek <marvinrusinek@gmail.com>
2024-08-06 17:48:18 -0400

    2 files modified

--- src/app/shared/services/quiz.service.ts
+++ src/app/shared/services/quiz.service.ts
@@ -1016,38 +1016,7 @@ export class QuizService implements OnDestroy {
       }
       return acc;
     }, [] as Option[]);
-  }
-
-  setCorrectMessage(correctOptions: Option[], optionsToDisplay: Option[]): string {  
-    if (!correctOptions || correctOptions.length === 0) {
-      return 'No correct answers found for the current question.';
-    }
-  
-    const correctOptionIndices = correctOptions.map((correctOption) => {
-      const originalIndex = optionsToDisplay.findIndex(
-        (option) => option.text.trim() === correctOption.text.trim()
-      );
-      return originalIndex !== -1 ? originalIndex + 1 : undefined; // +1 to make it 1-based index for display
-    });
-  
-    const uniqueIndices = [...new Set(correctOptionIndices.filter(index => index !== undefined))]; // Remove duplicates and undefined
-  
-    if (uniqueIndices.length === 0) {
-      return 'No correct answers found for the current question.';
-    }
-  
-    const optionsText =
-      uniqueIndices.length === 1 ? 'answer is Option' : 'answers are Options';
-    const optionStrings =
-      uniqueIndices.length > 1
-        ? uniqueIndices.slice(0, -1).join(', ') +
-          ' and ' +
-          uniqueIndices.slice(-1)
-        : `${uniqueIndices[0]}`;
-  
-    const correctMessage = `The correct ${optionsText} ${optionStrings}.`;
-    return correctMessage;
-  }
+  }  
 
   setCorrectAnswerOptions(optionOptions: Option[]): void {
     const correctAnswerOptions = this.convertToOptions(optionOptions);
