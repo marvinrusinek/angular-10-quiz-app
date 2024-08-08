a6b2fb72037e83c48b3b890792a0cb62d4c82217
marvinrusinek <marvinrusinek@gmail.com>
2024-08-08 19:55:40 -0400

    2 files modified

--- src/app/components/question/question-type/shared-option.component.ts
+++ src/app/components/question/question-type/shared-option.component.ts
@@ -45,7 +45,8 @@ export class SharedOptionComponent implements OnInit {
   handleOptionClick(option: SelectedOption, index: number): void {
     console.log('handleOptionClick called in SharedOptionComponent', option, index);
     this.optionClicked.emit({ option, index });
-    this.showFeedbackForOption[idx] = true; 
+    this.selectedOption = option;
+    this.showFeedbackForOption[idx] = true;
   }
 
   trackByOption(index: number, item: Option): number {
