c909535b91adcea151f16aaf59b852fe3094e380
marvinrusinek <marvinrusinek@gmail.com>
2024-08-09 19:08:56 -0400

    1 file modified

--- src/app/components/question/base-question.component.ts
+++ src/app/components/question/base-question.component.ts
@@ -57,10 +57,6 @@ export abstract class BaseQuestionComponent implements OnInit, OnChanges, OnDest
 
   ngOnInit(): void {
     console.log('ngOnInit - ExplanationTextService:', this.explanationTextService);
-    /* if (this.question) {
-      this.quizStateService.setCurrentQuestion(this.question);
-      this.initializeQuestion();
-    } */
     this.initializeQuestion();
     this.subscribeToQuestionChanges();
   }
