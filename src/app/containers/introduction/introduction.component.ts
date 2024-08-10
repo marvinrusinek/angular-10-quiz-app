3c8667c1f0ada1d877ada5993d57544c5495d165
marvinrusinek <marvinrusinek@gmail.com>
2024-08-10 11:28:28 -0400

    1 file modified

--- src/app/containers/introduction/introduction.component.ts
+++ src/app/containers/introduction/introduction.component.ts
@@ -145,6 +145,10 @@ export class IntroductionComponent implements OnInit, OnDestroy {
   
   onCheckboxChange(event: { checked: boolean }): void {
     console.log('Checkbox change event:', event);
+
+    // Update the user preference for highlighting correct answers
+    this.userPreferenceService.setHighlightPreference(event.checked);
+
     this.isCheckedSubject.next(event.checked);
     this.quizService.setCheckedShuffle(event.checked);
     this.highlightCorrectAfterIncorrect = event.checked;
