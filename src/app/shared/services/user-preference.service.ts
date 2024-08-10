c02f94ad00133d1f4ed9f0f4f0b81b476053c043
marvinrusinek <marvinrusinek@gmail.com>
2024-08-10 13:49:02 -0400

    1 file modified

--- src/app/shared/services/user-preference.service.ts
+++ src/app/shared/services/user-preference.service.ts
@@ -2,15 +2,15 @@ import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class UserPreferenceService {
-  private highlightCorrectAfterIncorrect = false;
+  private highlightPreference = false;
 
   setHighlightPreference(value: boolean): void {
     console.log('Setting highlight preference to:', value);
-    this.highlightCorrectAfterIncorrect = value;
+    this.highlightPreference = value;
   }
   
   getHighlightPreference(): boolean {
-    console.log('Getting highlight preference:', this.highlightCorrectAfterIncorrect);
-    return this.highlightCorrectAfterIncorrect;
+    console.log('Getting highlight preference:', this.highlightPreference);
+    return this.highlightPreference;
   }  
 }
