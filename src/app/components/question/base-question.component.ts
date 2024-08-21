045ee146bc050f027e1c2b48eba33e0516d27886
marvinrusinek <marvinrusinek@gmail.com>
2024-08-20 21:39:33 -0400

    1 file modified

--- src/app/components/question/base-question.component.ts
+++ src/app/components/question/base-question.component.ts
@@ -174,7 +174,8 @@ export abstract class BaseQuestionComponent implements OnInit, OnChanges, OnDest
       this.showFeedbackForOption = { [this.selectedOption.optionId]: true };
   
       const correctOptions = this.optionsToDisplay.filter(opt => opt.correct);
-      this.correctMessage = this.setCorrectMessage(correctOptions);
+      this.correctMessage = this.quizService.setCorrectMessage(correctOptions, this.optionsToDisplay);
+      this.cdRef.detectChanges();
   
       console.log('Calling formatExplanationText');
       console.log('ExplanationTextService:', this.explanationTextService);
