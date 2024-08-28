4070207647c27f722b073360d6a700d610627c38
marvinrusinek <marvinrusinek@gmail.com>
2024-08-28 17:10:05 -0400

    1 file modified

--- src/app/shared/models/SharedOptionConfig.model.ts
+++ src/app/shared/models/SharedOptionConfig.model.ts
@@ -1,9 +1,11 @@
 import { Option } from './Option.model';
+import { QuizQuestion } from './QuizQuestion.model';
+import { SelectedOption } from './SelectedOption.model';
 
 export interface SharedOptionConfig {
   optionsToDisplay: Option[];
-  selectedOption: any;
-  currentQuestion: any;
+  selectedOption: SelectedOption;
+  currentQuestion: QuizQuestion;
   showFeedback: boolean;
   type: 'single' | 'multiple';
   shouldResetBackground: boolean;
