0df0d1f68afcd509ec74c6fb7c42cd31dfb32cc9
marvinrusinek <marvinrusinek@gmail.com>
2024-08-31 15:30:09 -0400

    1 file modified

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -14,7 +14,7 @@ import {
   OnInit,
   Output,
   SimpleChange,
-  SimpleChanges,
+  SimpleChanges
 } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
