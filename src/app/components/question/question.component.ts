75de20ec10c0dca0eed5e724e31e01faff21d09d
marvinrusinek <marvinrusinek@gmail.com>
2024-08-31 16:24:08 -0400

    1 file modified

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -573,7 +573,7 @@ export class QuizQuestionComponent
     }
   }
 
-  async loadDynamicComponent(): Promise<void> {
+  /* async loadDynamicComponent(): Promise<void> {
     console.log('Loading dynamic component in QuizQuestionComponent');
     if (this.dynamicComponentContainer) {
       this.dynamicComponentContainer.clear();
@@ -591,6 +591,39 @@ export class QuizQuestionComponent
         componentRef.instance.question = this.question;
         componentRef.instance.optionsToDisplay = [...this.optionsToDisplay];
 
+        componentRef.changeDetectorRef.markForCheck();
+      }
+    } else {
+      console.error(
+        'dynamicComponentContainer is still undefined in QuizQuestionComponent'
+      );
+    }
+  } */
+  async loadDynamicComponent(): Promise<void> {
+    console.log('Loading dynamic component in QuizQuestionComponent');
+    if (this.dynamicComponentContainer) {
+      this.dynamicComponentContainer.clear();
+  
+      const isMultipleAnswer = await firstValueFrom(
+        this.quizStateService.isMultipleAnswerQuestion(this.question)
+      );
+      const componentRef = await this.dynamicComponentService.loadComponent(
+        this.dynamicComponentContainer,
+        isMultipleAnswer
+      );
+  
+      if (componentRef.instance) {
+        componentRef.instance.questionForm = this.questionForm;
+        componentRef.instance.question = this.question;
+        componentRef.instance.optionsToDisplay = [...this.optionsToDisplay];
+  
+        // Check if the instance is SingleAnswerComponent or MultipleAnswerComponent
+        if (componentRef.instance instanceof SingleAnswerComponent || 
+            componentRef.instance instanceof MultipleAnswerComponent) {
+          // Pass the onOptionClicked method to the dynamic component
+          componentRef.instance.quizQuestionComponentOnOptionClicked = this.onOptionClicked.bind(this);
+        }
+  
         componentRef.changeDetectorRef.markForCheck();
       }
     } else {
