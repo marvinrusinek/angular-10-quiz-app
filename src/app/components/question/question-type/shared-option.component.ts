e2071d63162c7e31c8448caf8e3fa0b755c9701b
marvinrusinek <marvinrusinek@gmail.com>
2024-09-01 13:02:44 -0400

    1 file modified

--- src/app/components/question/question-type/shared-option.component.ts
+++ src/app/components/question/question-type/shared-option.component.ts
@@ -259,10 +259,10 @@ export class SharedOptionComponent implements OnInit, OnChanges {
       this.selectedOptions.clear();
       this.selectedOptions.add(option.optionId);
       
-      for (const opt of this.optionsToDisplay) {
+      for (const [idx, opt] of this.optionsToDisplay.entries()) {
         opt.selected = opt.optionId === option.optionId;
-        // Show icon for all clicked options, not just the currently selected one
-        this.showIconForOption[opt.optionId] = this.clickedOptionIds.has(opt.optionId);
+        this.showIconForOption[opt.optionId] = true; // Always show icon for clicked options
+        this.updateOptionBinding(opt, idx);
       }
     } else {
       // For multiple-select, toggle the selection
@@ -272,18 +272,15 @@ export class SharedOptionComponent implements OnInit, OnChanges {
       } else {
         this.selectedOptions.delete(option.optionId);
       }
-    }
-  
-    // Update showIconForOption for all options
-    for (const opt of this.optionsToDisplay) {
-      this.showIconForOption[opt.optionId] = this.clickedOptionIds.has(opt.optionId);
+      // Always show icon for clicked options, regardless of current selection state
+      this.showIconForOption[option.optionId] = true;
+      this.updateOptionBinding(option, index);
     }
   
     this.showFeedback = true;
   
     console.log('Updated selectedOptions:', Array.from(this.selectedOptions));
     console.log('Clicked options:', Array.from(this.clickedOptionIds));
-    console.log('showIconForOption:', this.showIconForOption);
     console.log('showFeedback:', this.showFeedback);
   
     this.optionClicked.emit({ option, index });
