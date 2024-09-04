9d36476424193adaf4762e5d2b56559ff29c75c5
marvinrusinek <marvinrusinek@gmail.com>
2024-09-04 19:12:37 -0400

    1 file modified

--- src/app/components/question/question-type/shared-option.component.ts
+++ src/app/components/question/question-type/shared-option.component.ts
@@ -363,30 +363,36 @@ export class SharedOptionComponent implements OnInit, OnChanges {
     this.lastSelectedOptionIndex = index;
     this.showFeedback = true;
   
-    const optionBinding = this.optionBindings[index];
-    optionBinding.option.showIcon = true;
-    this.iconVisibility[option.optionId] = true;
-    this.showFeedbackForOption[option.optionId] = true;
-    this.clickedOptionIds.add(option.optionId ?? index);
-  
     if (this.type === 'single') {
-      // For single-select, deselect all options and select only the clicked one
+      // For single-select, deselect all options first
       for (const binding of this.optionBindings) {
-        const isClickedOption = binding.option.optionId === option.optionId;
-        binding.isSelected = isClickedOption;
-        binding.option.selected = isClickedOption;
-        binding.showFeedback = this.showFeedback && isClickedOption;
-        this.showIconForOption[binding.option.optionId] = isClickedOption;
-        this.iconVisibility[binding.option.optionId] = isClickedOption;
+        binding.isSelected = false;
+        binding.option.selected = false;
+        binding.showFeedback = false;
+        this.showIconForOption[binding.option.optionId] = false;
+        this.iconVisibility[binding.option.optionId] = false;
       }
+  
+      // Now select only the clicked option
+      const optionBinding = this.optionBindings[index];
+      optionBinding.isSelected = true;
+      optionBinding.option.selected = true;
+      optionBinding.showFeedback = true;
+      optionBinding.option.showIcon = true;
+      this.showIconForOption[option.optionId] = true;
+      this.iconVisibility[option.optionId] = true;
+      this.showFeedbackForOption[option.optionId] = true;
+  
       this.selectedOption = option;
       this.selectedOptions.clear();
       this.selectedOptions.add(option.optionId);
     } else {
       // For multiple-select, toggle the selection of the clicked option
+      const optionBinding = this.optionBindings[index];
       optionBinding.isSelected = !optionBinding.isSelected;
       optionBinding.option.selected = optionBinding.isSelected;
       optionBinding.showFeedback = this.showFeedback && optionBinding.isSelected;
+      optionBinding.option.showIcon = optionBinding.isSelected;
       
       if (optionBinding.isSelected) {
         this.selectedOptions.add(option.optionId);
@@ -394,7 +400,11 @@ export class SharedOptionComponent implements OnInit, OnChanges {
         this.selectedOptions.delete(option.optionId);
       }
       this.showIconForOption[option.optionId] = optionBinding.isSelected;
+      this.iconVisibility[option.optionId] = optionBinding.isSelected;
+      this.showFeedbackForOption[option.optionId] = optionBinding.isSelected;
     }
+  
+    this.clickedOptionIds.add(option.optionId ?? index);
   
     this.updateHighlighting();
   
