7fc9930ae9c313e61dab22e48e0201d3be21e9c5
marvinrusinek <marvinrusinek@gmail.com>
2024-09-05 20:36:51 -0400

    1 file modified

--- src/app/components/question/question-type/shared-option.component.ts
+++ src/app/components/question/question-type/shared-option.component.ts
@@ -143,24 +143,6 @@ export class SharedOptionComponent implements OnInit, OnChanges {
     this.cdRef.detectChanges();
   }
 
-  private restoreSelectionState(previousSelections: Set<number>): void {
-    for (const binding of this.optionBindings) {
-      if (previousSelections.has(binding.option.optionId)) {
-        binding.isSelected = true;
-        binding.option.selected = true;
-        this.selectedOptions.add(binding.option.optionId);
-        this.showIconForOption[binding.option.optionId] = true;
-        this.iconVisibility[binding.option.optionId] = true;
-        
-        if (this.type === 'single') {
-          this.selectedOption = binding.option;
-          break; // Only select one option for single-select questions
-        }
-      }
-    }
-    this.updateHighlighting();
-  }
-
   getOptionAttributes(optionBinding: OptionBindings) {
     return {
       appHighlightOption: '',
