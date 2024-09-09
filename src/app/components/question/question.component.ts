5844006b709607d5abe78ceb8d8e36a8b10f7c54
marvinrusinek <marvinrusinek@gmail.com>
2024-09-09 15:00:30 -0400

    1 file modified

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -225,6 +225,7 @@ export class QuizQuestionComponent
     console.log('QuizStateService injected:', !!this.quizStateService);
 
     this.questionForm = this.fb.group({});
+    this.shouldDisplayContainer = true;
 
     this.sharedVisibilitySubscription =
       this.sharedVisibilityService.pageVisibility$.subscribe((isHidden) => {
