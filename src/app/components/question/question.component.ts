4b2e6786b7fa2014cb784398146c83ce97bf8794
marvinrusinek <marvinrusinek@gmail.com>
2024-09-16 19:30:27 -0400

    1 file modified

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1309,7 +1309,9 @@ export class QuizQuestionComponent
   }
 
   public async onOptionClicked(option: SelectedOption, index: number): Promise<void> {
+    console.log('QQC: onOptionClicked called', option, index);
     console.log("MY LOG 12345");
+    
     if (!option) {
       console.error('Option is undefined');
       return;
