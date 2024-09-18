e1641022b5c4253a531888a19e66e1db03ffda6c
marvinrusinek <marvinrusinek@gmail.com>
2024-09-18 17:45:27 -0400

    1 file modified

--- src/app/containers/quiz/quiz.component.ts
+++ src/app/containers/quiz/quiz.component.ts
@@ -460,6 +460,19 @@ export class QuizComponent implements OnInit, OnDestroy, OnChanges {
     this.isAnswered = this.selectedOptions.length > 0;
     this.quizStateService.setAnswerSelected(this.isAnswered);
     
+    // Update the SelectedOptionService
+    if (this.isAnswered) {
+      this.selectedOptionService.setSelectedOption(this.selectedOptions[0]); // For single answer
+    } else {
+      this.selectedOptionService.clearSelectedOption();
+    }
+  
+    // Update other services
+    this.quizStateService.setAnswered(this.isAnswered);
+    
+    // Emit the selected option(s)
+    this.optionSelected.emit(event.option);
+  
     console.log("isAnswered updated:", this.isAnswered);
     console.log("selectedOptions:", this.selectedOptions);
   
