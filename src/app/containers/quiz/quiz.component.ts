dc14639e13686390e762058a5c416623a748559a
marvinrusinek <marvinrusinek@gmail.com>
2024-09-20 17:12:53 -0400

    1 file modified

--- src/app/containers/quiz/quiz.component.ts
+++ src/app/containers/quiz/quiz.component.ts
@@ -388,6 +388,7 @@ export class QuizComponent implements OnInit, OnDestroy, OnChanges {
     this.quizStateService.setAnswerSelected(isAnswered);
     this.selectedOptionService.setSelectedOption(event.option);
     this.manualOverrideSubject.next(true);
+    console.log('QuizComponent: Manual override set to', this.manualOverrideSubject.value);
 
     console.log('QuizComponent: Updated selectedOptions', this.selectedOptions);
     console.log('QuizComponent: Calling setAnswerSelected with', isAnswered);
