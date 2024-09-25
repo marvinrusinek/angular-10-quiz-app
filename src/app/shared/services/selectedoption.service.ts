29e7f22ee1ed48c60a4d95070f9e0bc192bad066
marvinrusinek <marvinrusinek@gmail.com>
2024-09-25 19:15:30 -0400

    1 file modified

--- src/app/shared/services/selectedoption.service.ts
+++ src/app/shared/services/selectedoption.service.ts
@@ -33,6 +33,7 @@ export class SelectedOptionService {
     return this.isOptionSelectedSubject.getValue();
   }
 
+  // Method to update the selected option state - not sure if this is being used...
   /* selectOption(optionId: number): void {
     // Update selected option logic
     this.isOptionSelectedSubject.next(true); // An option is selected
