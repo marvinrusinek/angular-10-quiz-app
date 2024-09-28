5ca23f281330cd8d0feeba55a457a5c55981f9bd
marvinrusinek <marvinrusinek@gmail.com>
2024-09-28 19:13:45 -0400

    1 file modified

--- src/app/components/question/question-type/shared-option.component.ts
+++ src/app/components/question/question-type/shared-option.component.ts
@@ -535,7 +535,7 @@ export class SharedOptionComponent implements OnInit, OnChanges {
   }
 
   getFeedbackBindings(option: Option, idx: number): any {
-    console.log("OPT BIN", option.feedback); // Add this log to check feedback
+    console.log("OPT BIN", option.feedback);
     const feedbackProps = {
       options: this.optionsToDisplay,
       question: this.currentQuestion,
