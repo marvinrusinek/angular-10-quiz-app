cd1d3a61a1beb340743600b32e9dbb2f9d6b19c8
marvinrusinek <marvinrusinek@gmail.com>
2024-10-07 19:55:06 -0400

    1 file modified

--- src/app/shared/services/dynamic-component.service.ts
+++ src/app/shared/services/dynamic-component.service.ts
@@ -1,4 +1,4 @@
-import { ComponentRef, Injectable, ComponentFactoryResolver, ViewContainerRef, Type } from '@angular/core';
+/* import { ComponentRef, Injectable, ComponentFactoryResolver, ViewContainerRef, Type } from '@angular/core';
 
 import { MultipleAnswerComponent } from '../../components/question/question-type/multiple-answer/multiple-answer.component';
 import { SingleAnswerComponent } from '../../components/question/question-type/single-answer/single-answer.component';
@@ -30,4 +30,43 @@ export class DynamicComponentService {
       };
     }
   }
-} 
+} */
+
+import { ComponentRef, Injectable, ComponentFactoryResolver, ViewContainerRef, Type } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class DynamicComponentService {
+  constructor(private componentFactoryResolver: ComponentFactoryResolver) {}
+
+  public async loadComponent<T>(
+    container: ViewContainerRef,
+    multipleAnswer: boolean
+  ): Promise<ComponentRef<T>> {
+    const component = multipleAnswer
+      ? (await this.importComponent('multiple')).MultipleAnswerComponent as Type<T>
+      : (await this.importComponent('single')).SingleAnswerComponent as Type<T>;
+
+    const componentFactory = this.componentFactoryResolver.resolveComponentFactory(component);
+    container.clear();
+    return container.createComponent(componentFactory);
+  }
+
+  private async importComponent(
+    type: string
+  ): Promise<{
+    MultipleAnswerComponent?: Type<any>;
+    SingleAnswerComponent?: Type<any>;
+  }> {
+    if (type === 'multiple') {
+      const module = await import(
+        '../../components/question/question-type/multiple-answer/multiple-answer.component'
+      );
+      return { MultipleAnswerComponent: module.MultipleAnswerComponent };
+    } else {
+      const module = await import(
+        '../../components/question/question-type/single-answer/single-answer.component'
+      );
+      return { SingleAnswerComponent: module.SingleAnswerComponent };
+    }
+  }
+}
