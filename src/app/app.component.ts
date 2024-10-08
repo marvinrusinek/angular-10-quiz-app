6ab70b9f08fac947a7979efef3e1eba05a2fe033
marvinrusinek <marvinrusinek@gmail.com>
2024-10-08 14:03:20 -0400

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent  {
+  constructor() {
+    // Override console.error to log the stack trace
+    const originalError = console.error;
+    console.error = function() {
+      originalError.apply(console, arguments);
+      console.log('Stack trace:', new Error().stack);
+    };
+  }
 }
