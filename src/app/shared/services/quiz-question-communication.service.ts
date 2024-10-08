6a228084e44c03791287fcacebfb477fa0478197
marvinrusinek <marvinrusinek@gmail.com>
2024-10-08 10:58:23 -0400

    1 file added

---
+++ src/app/shared/services/quiz-question-communication.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { SelectedOption } from '../models/SelectedOption.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuizQuestionCommunicationService {
+  private optionClickedSource = new Subject<{option: SelectedOption, index: number, checked: boolean}>();
+
+  optionClicked$ = this.optionClickedSource.asObservable();
+
+  emitOptionClicked(option: SelectedOption, index: number, checked: boolean) {
+    this.optionClickedSource.next({option, index, checked});
+  }
+}
