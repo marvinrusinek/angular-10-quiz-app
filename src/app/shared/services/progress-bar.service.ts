bb3f4273b2dfff3df07fe2758a80f21caeb3c680
marvinrusinek <marvinrusinek@gmail.com>
2024-10-14 19:45:46 -0400

    1 file modified

--- src/app/shared/services/progress-bar.service.ts
+++ src/app/shared/services/progress-bar.service.ts
@@ -3,11 +3,12 @@ import { BehaviorSubject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class ProgressBarService {
-  private progressSubject = new BehaviorSubject<number>(0);
-  progress$ = this.progressSubject.asObservable();
+  // Use BehaviorSubject to store progress value
+  private progressPercentageSubject = new BehaviorSubject<number>(0);
+  progress$ = this.progressPercentageSubject.asObservable(); // Expose it as an Observable
 
   // Method to update the progress
   setProgress(progress: number): void {
-    this.progressSubject.next(progress); // Emit the new progress value
+    this.progressPercentageSubject.next(progress); // Emit the new progress value
   }
 }
