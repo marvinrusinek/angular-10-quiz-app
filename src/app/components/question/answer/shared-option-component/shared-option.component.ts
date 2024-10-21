1ce27b410b5c922969420d1b01ec6c52ef56f37b
marvinrusinek <marvinrusinek@gmail.com>
2024-10-21 19:05:27 -0400

    1 file modified

--- src/app/components/question/answer/shared-option-component/shared-option.component.ts
+++ src/app/components/question/answer/shared-option-component/shared-option.component.ts
@@ -425,7 +425,7 @@ export class SharedOptionComponent implements OnInit, OnChanges {
   
     // Update option state, handle selection, and display feedback
     console.log('Updating option state...');
-    this.updateOptionState(clonedOption, index, optionId);
+    this.updateOptionState(clonedOption, index, optionId ?? index);
     this.handleSelection(clonedOption, index, optionId);
     this.displayFeedbackForOption(clonedOption, index, optionId);
     this.triggerChangeDetection();
