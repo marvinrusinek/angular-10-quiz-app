382fdebdf266546c5eb1d35b09ee42987677aebe
marvinrusinek <marvinrusinek@gmail.com>
2024-10-23 19:58:51 -0400

    1 file modified

--- src/app/containers/quiz/quiz.component.ts
+++ src/app/containers/quiz/quiz.component.ts
@@ -2344,7 +2344,7 @@ export class QuizComponent implements OnInit, OnDestroy, OnChanges {
   
         this.resetUIAndNavigate(this.currentQuestionIndex);
       
-        // Re-enable the button
+        // Re-enable the next button
         const shouldEnableNextButton = this.isAnyOptionSelected();
         this.updateAndSyncNextButtonState(shouldEnableNextButton);
 
