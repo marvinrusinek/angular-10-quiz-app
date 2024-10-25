bff974255a09d42627d29f72945ec74dff4199e0
marvinrusinek <marvinrusinek@gmail.com>
2024-10-25 18:14:55 -0400

    1 file modified

--- src/app/containers/quiz/quiz.component.ts
+++ src/app/containers/quiz/quiz.component.ts
@@ -446,14 +446,9 @@ export class QuizComponent implements OnInit, OnDestroy, OnChanges {
     });
   }
   
-  /* private evaluateNextButtonState(): boolean {
-    return (
-      this.isOptionSelected && 
-      !this.isLoading && 
-      !this.isNavigating
-    );
-  } */
   private evaluateNextButtonState(): boolean {
+    this.resetOptionState(); // Reset before checking next button state
+
     const isAnswered = this.selectedOptionService.isAnsweredSubject.value;
     const isLoading = !this.quizStateService.isLoadingSubject.value;
     const isNavigating = !this.quizStateService.isNavigatingSubject.value;
