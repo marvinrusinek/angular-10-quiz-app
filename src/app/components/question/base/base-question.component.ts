8c4b99599cb1757771cc27b924e11357db49261b
marvinrusinek <marvinrusinek@gmail.com>
2024-10-26 20:27:32 -0400

    1 file modified

--- src/app/components/question/base/base-question.component.ts
+++ src/app/components/question/base/base-question.component.ts
@@ -218,6 +218,16 @@ export abstract class BaseQuestionComponent implements OnInit, OnChanges, OnDest
       onQuestionAnswered: () => {}
     };
   }
+  
+  protected onQuestionAnswered(event: { option: SelectedOption }): void {      
+    if (this.selectedOption !== undefined) {
+      this.selectedOption = event.option;
+    }
+  
+    if (this.showFeedback !== undefined) {
+      this.showFeedback = true;
+    }
+  }
 
   protected subscribeToQuestionChanges(): void {
     if (this.quizStateService) {
