0cfa2f2d7c052e975fdf43e43d1a5bfb7050f78d
marvinrusinek <marvinrusinek@gmail.com>
2024-10-29 11:53:22 -0400

    1 file modified

--- src/app/shared/services/selection-message.service.ts
+++ src/app/shared/services/selection-message.service.ts
@@ -29,15 +29,19 @@ export class SelectionMessageService {
     if (questionIndex === 0 && !isAnswered) {
       return 'Please select an option to start the quiz.';
     }
+  
     if (isMultipleAnswer && !isAnswered) {
       return 'Please select an option to continue...';
     }
+  
     if (isAnswered && questionIndex < totalQuestions - 1) {
       return 'Please click the next button to continue.';
     }
+  
     if (questionIndex === totalQuestions - 1 && !isAnswered) {
       return 'Please select an option to continue...';
     }
+  
     return 'Please click the Show Results button.';
   }
 
