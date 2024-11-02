893cfbbec08133b2aae0aedc897c96d4a9294b48
marvinrusinek <marvinrusinek@gmail.com>
2024-11-02 15:03:54 -0400

    1 file modified

--- src/app/components/question/quiz-question/quiz-question.component.ts
+++ src/app/components/question/quiz-question/quiz-question.component.ts
@@ -683,60 +683,6 @@ export class QuizQuestionComponent extends BaseQuestionComponent
     }
   }
 
-  /* private restoreQuizState(): void {
-    const storedIndex = sessionStorage.getItem('currentQuestionIndex');
-    const storedQuestion = sessionStorage.getItem('currentQuestion');
-    const storedOptions = sessionStorage.getItem('optionsToDisplay');
-    const storedIsAnswered = sessionStorage.getItem('isAnswered');
-
-    if (storedIndex !== null && storedQuestion !== null && storedOptions !== null) {
-      try {
-        // Parse stored data with validation checks
-        this.currentQuestionIndex = +storedIndex;
-        const parsedQuestion = JSON.parse(storedQuestion);
-        const parsedOptions = JSON.parse(storedOptions);
-
-        // Validate parsed question structure
-        if (typeof parsedQuestion === 'object' && parsedQuestion.questionText) {
-          this.currentQuestion = parsedQuestion;
-        } else {
-          throw new Error('Invalid question format');
-        }
-
-          // Validate parsed options structure
-          if (Array.isArray(parsedOptions) && parsedOptions.every(option => 'optionText' in option && 'correct' in option)) {
-            this.optionsToDisplay = parsedOptions;
-          } else {
-            throw new Error('Invalid options format');
-          }
-
-          this.isAnswered = storedIsAnswered === 'true';
-
-          console.log('Restoring question:', this.currentQuestion);
-          console.log('Restored isAnswered:', this.isAnswered);
-
-          // Display logic based on `isAnswered` state
-          if (this.currentQuestion) {
-            if (this.isAnswered) {
-              console.log('Displaying explanation since the question is answered.');
-              this.showExplanationText();
-            } else {
-              console.log('Displaying question text since the question is not answered.');
-              this.showQuestionText();
-            }
-          } else {
-            console.warn('Restored question is null or undefined. Loading default question...');
-            this.loadQuestion();
-          }
-        } catch (error) {
-          console.error('Error parsing stored data or invalid data format:', error);
-          this.loadQuestion();
-        }
-    } else {
-      console.warn('Stored state is incomplete, loading default question');
-      this.loadQuestion(); // Fallback to load default question if parsing fails
-    }
-  } */
   private restoreQuizState(): void {
     const storedIndex = sessionStorage.getItem('currentQuestionIndex');
     const storedQuestion = sessionStorage.getItem('currentQuestion');
@@ -744,52 +690,51 @@ export class QuizQuestionComponent extends BaseQuestionComponent
     const storedIsAnswered = sessionStorage.getItem('isAnswered');
 
     if (storedIndex !== null && storedQuestion !== null && storedOptions !== null) {
-        try {
-            this.currentQuestionIndex = +storedIndex;
+      try {
+        this.currentQuestionIndex = +storedIndex;
 
-            // Check if storedQuestion is a valid JSON string
-            const parsedQuestion = JSON.parse(storedQuestion);
-            if (parsedQuestion && typeof parsedQuestion === 'object' && 'questionText' in parsedQuestion && parsedQuestion.questionText) {
-                this.currentQuestion = parsedQuestion;
-            } else {
-                console.error('Parsed question structure is invalid or null:', parsedQuestion);
-                throw new Error('Invalid or null question format');
-            }
+        // Check if storedQuestion is a valid JSON string
+        const parsedQuestion = JSON.parse(storedQuestion);
+        if (parsedQuestion && typeof parsedQuestion === 'object' && 'questionText' in parsedQuestion && parsedQuestion.questionText) {
+          this.currentQuestion = parsedQuestion;
+        } else {
+          console.error('Parsed question structure is invalid or null:', parsedQuestion);
+          throw new Error('Invalid or null question format');
+        }
 
-            // Validate parsedOptions structure
-            const parsedOptions = JSON.parse(storedOptions);
-            if (Array.isArray(parsedOptions) && parsedOptions.length > 0 && parsedOptions.every(option => option && typeof option === 'object' && 'optionText' in option && 'correct' in option)) {
-                this.optionsToDisplay = parsedOptions;
-            } else {
-                console.error('Parsed options are not valid or empty:', parsedOptions);
-                throw new Error('Invalid or null options format');
-            }
+        // Validate parsedOptions structure
+        const parsedOptions = JSON.parse(storedOptions);
+        if (Array.isArray(parsedOptions) && parsedOptions.length > 0 && parsedOptions.every(option => option && typeof option === 'object' && 'optionText' in option && 'correct' in option)) {
+          this.optionsToDisplay = parsedOptions;
+        } else {
+          console.error('Parsed options are not valid or empty:', parsedOptions);
+          throw new Error('Invalid or null options format');
+        }
 
-            this.isAnswered = storedIsAnswered === 'true';
+        this.isAnswered = storedIsAnswered === 'true';
 
-            console.log('Restoring question:', this.currentQuestion);
-            console.log('Restored isAnswered:', this.isAnswered);
+        console.log('Restoring question:', this.currentQuestion);
+        console.log('Restored isAnswered:', this.isAnswered);
 
-            if (this.currentQuestion) {
-                if (this.isAnswered) {
-                    console.log('Displaying explanation since the question is answered.');
-                    this.showExplanationText();
-                } else {
-                    console.log('Displaying question text since the question is not answered.');
-                    this.showQuestionText();
-                }
-            } else {
-                console.warn('Restored question is null or undefined. Loading default question...');
-                this.loadQuestion();
-            }
-
-        } catch (error) {
-            console.error('Error parsing stored data or invalid data format:', error);
-            this.loadQuestion();
+        if (this.currentQuestion) {
+          if (this.isAnswered) {
+            console.log('Displaying explanation since the question is answered.');
+            this.showExplanationText();
+          } else {
+            console.log('Displaying question text since the question is not answered.');
+            this.showQuestionText();
+          }
+        } else {
+          console.warn('Restored question is null or undefined. Loading default question...');
+          this.loadQuestion();
+        }
+      } catch (error) {
+        console.error('Error parsing stored data or invalid data format:', error);
+        this.loadQuestion();
         }
     } else {
-        console.warn('Stored state is incomplete, loading default question');
-        this.loadQuestion();
+      console.warn('Stored state is incomplete, loading default question');
+      this.loadQuestion();
     }
   }
 
