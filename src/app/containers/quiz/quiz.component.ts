545ffd15d4a3843ffd5d5cb5b5ea803b78a8006d
marvinrusinek <marvinrusinek@gmail.com>
2024-11-16 21:12:35 -0500

    1 file modified

--- src/app/containers/quiz/quiz.component.ts
+++ src/app/containers/quiz/quiz.component.ts
@@ -747,9 +747,17 @@ export class QuizComponent implements OnInit, OnDestroy, OnChanges, AfterViewIni
   }
 
   // Public getter methods for determining UI state based on current quiz and question data.
-  public get isContentAvailable(): boolean {
+  /* public get isContentAvailable(): boolean {
     return !!this.currentQuestion && this.options?.length > 0;
-  }
+  } */
+  public get isContentAvailable(): boolean {
+    const isAvailable = !!this.currentQuestion && this.options?.length > 0;
+    console.log('isContentAvailable:', isAvailable, {
+      currentQuestion: this.currentQuestion,
+      options: this.options,
+    });
+    return isAvailable;
+  }  
 
   public get shouldDisplayContent(): boolean {
     return !!this.data?.questionText && !!this.questionToDisplay;
