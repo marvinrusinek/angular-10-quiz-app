f1253d3a8993a76b986bd45f7aec764222e31b30
marvinrusinek <marvinrusinek@gmail.com>
2024-11-25 20:18:08 -0500

    1 file modified

--- src/app/shared/services/timer.service.ts
+++ src/app/shared/services/timer.service.ts
@@ -117,14 +117,10 @@ export class TimerService {
 
   resetTimer(): void {
     console.log("Attempting to reset timer...");
-    if (!this.isTimerRunning) {
-      console.warn("Timer is already reset. Skipping redundant reset.");
+    if (this.isTimerRunning) {
+      console.log("Timer is running. Stopping before resetting...");
       this.stopTimer();
-      // return;
     }
-  
-    console.log("Timer is running. Stopping before resetting...");
-    // this.stopTimer(); // Stop the timer only if it is running
 
     this.elapsedTime = 0; // Reset elapsed time
     this.isTimerRunning = false; // Ensure timer state is reset
