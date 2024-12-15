a0f978e7b3b89d7ed0c5767972a02d21898d0b33
marvinrusinek <marvinrusinek@gmail.com>
2024-12-15 14:39:45 -0500

    1 file modified

--- src/app/shared/services/selectedoption.service.ts
+++ src/app/shared/services/selectedoption.service.ts
@@ -110,7 +110,6 @@ export class SelectedOptionService {
 
   /** Adds an option to the selectedOptionsMap */
   addOption(questionIndex: number, option: SelectedOption, source: string = 'unknown'): void {
-    this.trackOptionLifecycle(option, `addOption (BEFORE) source=${source}`);
     // 1️⃣ Check if option is valid
     if (!option) {
       console.error('❌ [addOption] Option is undefined. Cannot add it to selectedOptionsMap.');
