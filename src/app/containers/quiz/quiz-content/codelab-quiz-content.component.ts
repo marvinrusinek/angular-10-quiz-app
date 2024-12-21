cb7567374b62a2f45c0a2390eb9e55e2e9e0e542
marvinrusinek <marvinrusinek@gmail.com>
2024-12-20 19:26:30 -0500

    1 file modified

--- src/app/containers/quiz/quiz-content/codelab-quiz-content.component.ts
+++ src/app/containers/quiz/quiz-content/codelab-quiz-content.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewChecked, AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
+import { AfterViewChecked, ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { BehaviorSubject, combineLatest, firstValueFrom, forkJoin, isObservable, Observable, of, Subject, Subscription } from 'rxjs';
 import { catchError, debounceTime, distinctUntilChanged, map, mergeMap, startWith, switchMap, take, takeUntil, tap, withLatestFrom } from 'rxjs/operators';
@@ -21,7 +21,7 @@ import { QuizQuestionComponent } from '../../../components/question/quiz-questio
   styleUrls: ['./codelab-quiz-content.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CodelabQuizContentComponent implements OnInit, OnDestroy, AfterViewChecked, AfterViewInit {
+export class CodelabQuizContentComponent implements OnInit, OnDestroy, AfterViewChecked {
   @ViewChild('quizQuestionComponent', { static: false })
   quizQuestionComponent!: QuizQuestionComponent | undefined;
   @Output() isContentAvailableChange = new EventEmitter<boolean>();
@@ -284,18 +284,6 @@ export class CodelabQuizContentComponent implements OnInit, OnDestroy, AfterView
     this.configureDisplayLogic();
     this.setupCorrectAnswersTextDisplay();
   }
-
-  ngAfterViewInit(): void {
-    console.log('[CodelabQuizContentComponent] ngAfterViewInit triggered.');
-  
-    this.waitForQuizQuestionComponent().subscribe((ready) => {
-      if (ready) {
-        this.setupDisplayStateSubscription();
-      } else {
-        console.error('[CodelabQuizContentComponent] Initialization failed. Content or component not ready.');
-      }
-    });
-  }  
   
   ngAfterViewChecked(): void {
     if (this.currentQuestion && !this.questionRendered.getValue()) {
