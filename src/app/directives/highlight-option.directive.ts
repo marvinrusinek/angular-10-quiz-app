7d92a609559343925af666f8f49ce91dd3b8f4a2
marvinrusinek <marvinrusinek@gmail.com>
2024-12-26 18:28:15 -0500

    1 file modified

--- src/app/directives/highlight-option.directive.ts
+++ src/app/directives/highlight-option.directive.ts
@@ -178,9 +178,8 @@ export class HighlightOptionDirective implements OnChanges {
         return; // Prioritize selected state over others
     }
 
+    // Highlight incorrect option(s)
     if (!this.isCorrect) {
-      // Default background for other options
-      console.log('[updateHighlight] Applying default background for unselected option:', this.option);
       this.setBackgroundColor('#a3a3a3'); // Dark gray background
       this.setPointerEvents('auto'); // Enable interactions
     }
