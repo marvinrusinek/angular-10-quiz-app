ff36400d2b5c89d907f65d5e3e1d31b44e9e2e02
marvinrusinek <marvinrusinek@gmail.com>
2025-01-01 13:22:27 -0500

    1 file modified

--- src/app/components/question/quiz-question/quiz-question.component.ts
+++ src/app/components/question/quiz-question/quiz-question.component.ts
@@ -2,8 +2,7 @@ import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, C
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { BehaviorSubject, firstValueFrom, from, Observable, of, ReplaySubject, Subject, Subscription } from 'rxjs';
-import { catchError, debounceTime, distinctUntilChanged,
-  filter, map, take, takeUntil, tap } from 'rxjs/operators';
+import { catchError, debounceTime, distinctUntilChanged, filter, map, take, takeUntil, tap } from 'rxjs/operators';
 
 import { Utils } from '../../../shared/utils/utils';
 import { AudioItem } from '../../../shared/models/AudioItem.model';
