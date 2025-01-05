172123a31f1ce435fe9a7bd87653dad7584cf8ce
marvinrusinek <marvinrusinek@gmail.com>
2025-01-05 18:59:16 -0500

    1 file modified

--- src/app/components/question/quiz-question/quiz-question.component.ts
+++ src/app/components/question/quiz-question/quiz-question.component.ts
@@ -1795,7 +1795,7 @@ export class QuizQuestionComponent
         ...opt,
         active: opt.correct, // Correct options remain active
         feedback: opt.correct ? undefined : 'x', // Feedback for incorrect options
-        showIcon: opt.correct // Show icon for correct options
+        showIcon: opt.correct || opt.showIcon // Show icon for correct options
       }));
     } else {
       this.optionsToDisplay = this.optionsToDisplay.map(opt =>
