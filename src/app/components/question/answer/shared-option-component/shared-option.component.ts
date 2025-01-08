eadd162d43618c15dbe6ee631594719dc999ce13
marvinrusinek <marvinrusinek@gmail.com>
2025-01-08 12:59:31 -0500

    1 file modified

--- src/app/components/question/answer/shared-option-component/shared-option.component.ts
+++ src/app/components/question/answer/shared-option-component/shared-option.component.ts
@@ -292,7 +292,7 @@ export class SharedOptionComponent implements OnInit, OnChanges {
 
   getOptionAttributes(optionBinding: OptionBindings): OptionBindings {
     return {
-      appHighlightOption: '',
+      appHighlightOption: false,
       ariaLabel: optionBinding.ariaLabel,
       isSelected: optionBinding.isSelected,
       isCorrect: optionBinding.isCorrect,
@@ -892,7 +892,7 @@ export class SharedOptionComponent implements OnInit, OnChanges {
       highlightCorrectAfterIncorrect: this.highlightCorrectAfterIncorrect,
       allOptions: this.optionsToDisplay,
       type: this.type,
-      appHighlightOption: '',
+      appHighlightOption: false,
       appHighlightInputType: this.type === 'multiple' ? 'checkbox' : 'radio',
       appHighlightReset: this.shouldResetBackground,
       appResetBackground: this.shouldResetBackground,
