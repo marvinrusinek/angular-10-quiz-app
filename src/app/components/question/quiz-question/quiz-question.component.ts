6b9f0cc257dd09a709e06934206fe7772d1b2289
marvinrusinek <marvinrusinek@gmail.com>
2025-01-20 14:32:28 -0500

    1 file modified

--- src/app/components/question/quiz-question/quiz-question.component.ts
+++ src/app/components/question/quiz-question/quiz-question.component.ts
@@ -4,7 +4,6 @@ import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { BehaviorSubject, firstValueFrom, from, Observable, of, ReplaySubject, Subject, Subscription } from 'rxjs';
 import { catchError, debounceTime, distinctUntilChanged, filter, first, map, take, takeUntil, tap } from 'rxjs/operators';
 
-
 import { Utils } from '../../../shared/utils/utils';
 import { AudioItem } from '../../../shared/models/AudioItem.model';
 import { FormattedExplanation } from '../../../shared/models/FormattedExplanation.model';
