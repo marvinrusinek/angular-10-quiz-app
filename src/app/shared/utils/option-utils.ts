bd232375731fe5891fc179e70f0dd05e07537be7
marvinrusinek <marvinrusinek@gmail.com>
2025-01-26 17:17:50 -0500

    1 file modified

--- src/app/shared/utils/option-utils.ts
+++ src/app/shared/utils/option-utils.ts
@@ -1,4 +1,4 @@
-import { Option } from '../models/option.model';
+import { Option } from '../models/Option.model';
 
 export function isValidOption(option: Option): boolean {
   return option && typeof option === 'object' && 'text' in option && 'correct' in option;
