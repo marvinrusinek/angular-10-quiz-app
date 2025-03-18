ae5bbe89bfb4cfdedf113fd3d2f94105b80e4c11
marvinrusinek <marvinrusinek@gmail.com>
2025-03-18 19:20:11 -0400

    1 file modified

--- src/app/containers/quiz/quiz.component.ts
+++ src/app/containers/quiz/quiz.component.ts
@@ -3524,7 +3524,7 @@ export class QuizComponent implements OnInit, OnDestroy, OnChanges, AfterViewIni
       if (this.currentQuestionIndex < this.totalQuestions - 1) {
         // Increment question index before fetching
         this.currentQuestionIndex += 1;
-        this.quizStateService.setCurrentQuestionIndex(this.currentQuestionIndex);
+        this.setCurrentQuestionIndex(this.currentQuestionIndex);
         
         // Fetch and set next question
         const questionLoaded = await this.fetchAndSetNextQuestion();
