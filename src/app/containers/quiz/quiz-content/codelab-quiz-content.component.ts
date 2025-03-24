064041a7aed8bc1bba08fe40d0876967c7f189c3
marvinrusinek <marvinrusinek@gmail.com>
2025-03-24 14:24:09 -0400

    1 file modified

--- src/app/containers/quiz/quiz-content/codelab-quiz-content.component.ts
+++ src/app/containers/quiz/quiz-content/codelab-quiz-content.component.ts
@@ -972,7 +972,7 @@ export class CodelabQuizContentComponent implements OnInit, OnDestroy, AfterView
       ),
       this.quizStateService.currentQuestionIndex$.pipe(startWith(0), distinctUntilChanged())
     ]).pipe(
-      debounceTime(10),
+      debounceTime(10), // gives state time to settle
       switchMap(([nextQ, prevQ, formattedExplanation, shouldShowExplanation, currentIndex]) =>
         this.determineTextToDisplay([nextQ, prevQ, formattedExplanation, shouldShowExplanation, currentIndex])
       ),
