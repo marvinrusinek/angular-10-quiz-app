e2d0ff2dd66c46b52632f15f14be58b8b29ac140
marvinrusinek <marvinrusinek@gmail.com>
2025-03-27 19:24:40 -0400

    1 file modified

--- src/app/containers/quiz/quiz-content/codelab-quiz-content.component.ts
+++ src/app/containers/quiz/quiz-content/codelab-quiz-content.component.ts
@@ -997,7 +997,7 @@ export class CodelabQuizContentComponent implements OnInit, OnDestroy, AfterView
     ) as Observable<string>;
   }
   
-private determineTextToDisplay(
+  private determineTextToDisplay(
     [nextQuestion, previousQuestion, formattedExplanation, shouldDisplayExplanation, currentIndex, currentQuestion]: [
       QuizQuestion | null,
       QuizQuestion | null,
