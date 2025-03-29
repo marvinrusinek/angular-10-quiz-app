41c0beb62c29b2b3eda819fe3fc74cf24c81c17f
marvinrusinek <marvinrusinek@gmail.com>
2025-03-29 15:33:06 -0400

    1 file modified

--- src/app/components/question/quiz-question/quiz-question.component.ts
+++ src/app/components/question/quiz-question/quiz-question.component.ts
@@ -2658,12 +2658,6 @@ export class QuizQuestionComponent extends BaseQuestionComponent
   
       this.showFeedbackForOption[event.option?.optionId || 0] = true;
   
-      // ✅ Set answered state and trigger change detection
-      /* if (!this.selectedOptionService.isAnsweredSubject.getValue()) {
-        this.selectedOptionService.isAnsweredSubject.next(true); // uses .next(true) and saves to session
-        console.log('[✅ QQC] isAnsweredSubject set to TRUE');
-        this.cdRef.detectChanges(); // 🔄 Force change detection
-      } */
       const isAnsweredCurrent = this.selectedOptionService.isAnsweredSubject.getValue();
       console.log('[🧪 BEFORE answered]', isAnsweredCurrent);
 
