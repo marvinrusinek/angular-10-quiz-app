81a6a68ab3f9134fd077d329fca6bfa037657d88
marvinrusinek <marvinrusinek@gmail.com>
2025-04-05 19:38:08 -0400

    1 file modified

--- src/app/containers/quiz/quiz-content/codelab-quiz-content.component.ts
+++ src/app/containers/quiz/quiz-content/codelab-quiz-content.component.ts
@@ -314,13 +314,13 @@ export class CodelabQuizContentComponent implements OnInit, OnChanges, OnDestroy
       map(([state, explanationText]) => {
         const explanation = explanationText?.trim();
         const question = this.questionToDisplay?.trim();
-
-        console.log('[combinedText$]:::', { state, explanation, question });
-    
-        return state.mode === 'explanation'
-          ? (explanation?.trim() || 'No explanation available')
-          : (question?.trim() || 'No question available');
-      }),
+      
+        const showExplanation = state.mode === 'explanation' && !!explanation;
+      
+        return showExplanation
+          ? explanation
+          : (question || 'No question available');
+      }),      
       distinctUntilChanged()
     );
     /* this.combinedText$ = combineLatest([
