900e128d8502b15af6a1d98b25dfd529067e90a4
marvinrusinek <marvinrusinek@gmail.com>
2025-04-09 13:53:09 -0400

    1 file modified

--- src/app/components/question/quiz-question/quiz-question.component.ts
+++ src/app/components/question/quiz-question/quiz-question.component.ts
@@ -186,7 +186,7 @@ export class QuizQuestionComponent extends BaseQuestionComponent
     answered: boolean;
   }>({
     mode: 'question',
-    answered: false,
+    answered: false
   });
   displayState$ = this.displayStateSubject.asObservable();
 
