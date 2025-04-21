8d6421338e2dfff525783f718c1571850d054543
marvinrusinek <marvinrusinek@gmail.com>
2025-04-21 18:54:27 -0400

    1 file modified

--- src/app/shared/models/OptionBindings.model.ts
+++ src/app/shared/models/OptionBindings.model.ts
@@ -23,5 +23,5 @@ export interface OptionBindings {
   change: (element: MatCheckbox | MatRadioButton) => void;
   disabled: boolean;
   ariaLabel: string;
-  optionAlreadyHandled: boolean;
+  optionAlreadyHandled?: boolean;
 }
