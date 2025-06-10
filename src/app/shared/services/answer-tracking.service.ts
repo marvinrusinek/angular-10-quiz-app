3c894c936da1f5806927c38bd08ef148164232ee
marvinrusinek <marvinrusinek@gmail.com>
2025-06-10 09:48:55 -0400

    1 file modified

--- src/app/shared/services/answer-tracking.service.ts
+++ src/app/shared/services/answer-tracking.service.ts
@@ -1,6 +1,18 @@
 import { Injectable } from '@angular/core';
 
+import { SelectedOptionService } from '../../shared/services/selectedoption.service';
+
 @Injectable({ providedIn: 'root' })
 export class AnswerTrackingService {
+  isOptionSelected = false;
 
+  constructor(private selectedOptionService: SelectedOptionService) {}
+
+  private resetOptionState(): void {
+    this.isOptionSelected = false;
+  
+    // Clear both selection and answered state
+    this.selectedOptionService.setOptionSelected(false);
+    // this.selectedOptionService.setAnswered(false);
+  }
 }
