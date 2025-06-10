dc584306ada013e708458fe5ddc57acddefa27a7
marvinrusinek <marvinrusinek@gmail.com>
2025-06-10 16:42:46 -0400

    1 file modified

--- src/app/shared/services/quiz-navigation.service.ts
+++ src/app/shared/services/quiz-navigation.service.ts
@@ -374,7 +374,7 @@ export class QuizNavigationService {
 
   private handleQuizCompletion(): void {
     this.quizService.submitQuizScore(this.answers).subscribe(() => {
-      this.router.navigate(['quiz', 'result']);
+      this.router.navigate(['results', this.quizService.getQuizId()]);
     });
   }
 
