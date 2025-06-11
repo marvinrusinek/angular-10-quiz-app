59346eee4d206167cbd12984851dd784fa12fa87
marvinrusinek <marvinrusinek@gmail.com>
2025-06-11 10:59:08 -0400

    1 file modified

--- src/app/shared/services/answer-tracking.service.ts
+++ src/app/shared/services/answer-tracking.service.ts
@@ -57,30 +57,6 @@ export class AnswerTrackingService {
     sessionStorage.setItem('isAnswered', 'true');
     sessionStorage.setItem(`displayMode_${questionIndex}`, 'explanation');
     sessionStorage.setItem('displayExplanation', 'true');
-  } 
-
-  // move to SMS!!
-  async setSelectionMessage(isAnswered: boolean): Promise<void> {
-    try {
-      const index = this.currentQuestionIndex;
-      const total = this.totalQuestions;
-  
-      if (typeof index !== 'number' || isNaN(index) || total <= 0) {
-        console.warn('[❌ setSelectionMessage] Invalid index or totalQuestions');
-        return;
-      }
-  
-      const newMessage = this.selectionMessageService.determineSelectionMessage(index, total, isAnswered);
-      const current = this.selectionMessageService.getCurrentMessage();
-  
-      if (newMessage !== current) {
-        this.selectionMessageService.updateSelectionMessage(newMessage);
-      } else {
-        console.log(`[⏸️ Skipping update — message already "${current}"`);
-      }
-    } catch (error) {
-      console.error('[❌ setSelectionMessage ERROR]', error);
-    }
   }
 
   public resetOptionState(): void {
