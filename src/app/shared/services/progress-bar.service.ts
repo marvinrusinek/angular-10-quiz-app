97cb0418025b0a7bdd9ea8aadf4fde79a4cdcef7
marvinrusinek <marvinrusinek@gmail.com>
2025-06-11 11:51:02 -0400

    1 file modified

--- src/app/shared/services/progress-bar.service.ts
+++ src/app/shared/services/progress-bar.service.ts
@@ -1,14 +1,31 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
+import { QuizService } from './quiz.service'; 
+
 @Injectable({ providedIn: 'root' })
 export class ProgressBarService {
   // Use BehaviorSubject to store progress value
   private progressPercentageSubject = new BehaviorSubject<number>(0);
   progress$ = this.progressPercentageSubject.asObservable();
 
+  constructor(private quizService: QuizService) {}
+
   // Method to update the progress
   setProgress(progress: number): void {
     this.progressPercentageSubject.next(progress); // emit the new progress value
   }
+
+  initializeProgressTracking(): void {
+    this.setProgress(0); // initialize to 0%
+  
+    // Subscribe to index changes and update progress
+    this.quizService.currentQuestionIndex$.subscribe(index => {
+      const totalQuestions = this.quizService.getTotalQuestionsCountSync(); // synchronous getter or store a cached value
+      if (totalQuestions > 0) {
+        const percentage = Math.round(((index + 1) / totalQuestions) * 100);
+        this.progress$.next(percentage);
+      }
+    });
+  }  
 }
