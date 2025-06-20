3652144f8ea517711e7250955ef516889c7114d4
marvinrusinek <marvinrusinek@gmail.com>
2025-06-20 19:10:12 -0400

    1 file modified

--- src/app/router/quiz-routing.module.ts
+++ src/app/router/quiz-routing.module.ts
@@ -39,7 +39,8 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes, { 
       enableTracing: false, 
-      onSameUrlNavigation: 'reload'
+      onSameUrlNavigation: 'reload',
+      scrollPositionRestoration: 'enabled'
     })
   ],
   exports: [RouterModule]
