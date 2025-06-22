05d29c57b9bbc3f98100c7f4246b52dd4d4407df
marvinrusinek <marvinrusinek@gmail.com>
2025-06-22 12:30:42 -0400

    1 file modified

--- src/app/shared/services/quiz-navigation.service.ts
+++ src/app/shared/services/quiz-navigation.service.ts
@@ -154,7 +154,9 @@ export class QuizNavigationService {
   }
 
   private async navigateWithOffset(offset: number): Promise<void> {
-    const currentIndex = this.quizService.getCurrentQuestionIndex();
+    // const currentIndex = this.quizService.getCurrentQuestionIndex();
+    const routeParam = Number(this.activatedRoute.snapshot.paramMap.get('questionIndex'));
+    const currentIndex = !isNaN(routeParam) ? routeParam - 1 : this.quizService.getCurrentQuestionIndex();
     const targetIndex = currentIndex + offset;
 
     // Block if going out of bounds
