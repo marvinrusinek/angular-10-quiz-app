787312e4aea7c2c70c5bc950f19323d73c27c624
marvinrusinek <marvinrusinek@gmail.com>
2025-06-30 18:44:24 -0400

    1 file modified

--- src/app/containers/quiz/quiz.component.ts
+++ src/app/containers/quiz/quiz.component.ts
@@ -24,7 +24,6 @@ import { QuizQuestionComponent } from '../../components/question/quiz-question/q
 import { SharedOptionComponent } from '../../components/question/answer/shared-option-component/shared-option.component';
 import { QuizService } from '../../shared/services/quiz.service';
 import { QuizDataService } from '../../shared/services/quizdata.service';
-import { QuizDisplayService } from '../../shared/services/quiz-display.service';
 import { QuizInitializationService } from '../../shared/services/quiz-initialization.service';
 import { QuizNavigationService } from '../../shared/services/quiz-navigation.service';
 import { QuizStateService } from '../../shared/services/quizstate.service';
@@ -253,7 +252,6 @@ export class QuizComponent implements OnInit, OnDestroy, OnChanges, AfterViewIni
   constructor(
     private quizService: QuizService,
     private quizDataService: QuizDataService,
-    private quizDisplayService: QuizDisplayService,
     private quizInitializationService: QuizInitializationService,
     private quizNavigationService: QuizNavigationService,
     private quizQuestionLoaderService: QuizQuestionLoaderService,
@@ -998,9 +996,6 @@ export class QuizComponent implements OnInit, OnDestroy, OnChanges, AfterViewIni
   
   // REMOVE!!
   private resetQuestionState(): void {
-    // Wipe heading immediately so old text can’t flash 
-    this.quizDisplayService.clearQuestionText('resetQuestionState');
-    
     // Remove stale question so template can’t render old text
     this.currentQuestion = null; 
     
