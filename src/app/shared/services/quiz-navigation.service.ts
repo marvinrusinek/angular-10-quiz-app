d1026d4dee8971df0ac3449179be1ec4e0240935
marvinrusinek <marvinrusinek@gmail.com>
2025-06-30 19:47:21 -0400

    1 file modified

--- src/app/shared/services/quiz-navigation.service.ts
+++ src/app/shared/services/quiz-navigation.service.ts
@@ -105,10 +105,14 @@ export class QuizNavigationService {
   }
 
   public async advanceToNextQuestion(): Promise<void> {
+    this.quizQuestionLoaderService.clearQA();
+    this.triggerAnimation();
     await this.navigateWithOffset(1);
   }
   
   public async advanceToPreviousQuestion(): Promise<void> {
+    this.quizQuestionLoaderService.clearQA();
+    this.triggerAnimation();
     await this.navigateWithOffset(-1);
   }
 
@@ -366,5 +370,9 @@ export class QuizNavigationService {
 
   private getQuizId(): string | null {
     return this.quizId || null;
+  }
+
+  triggerAnimation(): void {
+    this.animationState$.next('animationStarted');
   }
 }
