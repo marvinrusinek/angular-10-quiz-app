80e7f806b5e5cd87e9c42ec5ccb504c9a1d9c752
marvinrusinek <marvinrusinek@gmail.com>
2025-07-05 12:32:34 -0400

    1 file modified

--- src/app/shared/services/quiz-initialization.service.ts
+++ src/app/shared/services/quiz-initialization.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import {
   BehaviorSubject,
+  combineLatest,
   EMPTY,
   firstValueFrom,
   forkJoin,
@@ -14,7 +15,6 @@ import {
 } from 'rxjs';
 import {
   catchError,
-  combineLatest,
   debounceTime,
   distinctUntilChanged,
   filter,
