271bb2a3fabe9eaa6e41a442afee437c836609db
marvinrusinek <marvinrusinek@gmail.com>
2025-07-09 13:25:59 -0400

    1 file modified

--- src/app/components/question/answer/shared-option-component/shared-option.component.ts
+++ src/app/components/question/answer/shared-option-component/shared-option.component.ts
@@ -21,7 +21,6 @@ import { UserPreferenceService } from '../../../../shared/services/user-preferen
 import { QuizQuestionComponent } from '../../../../components/question/quiz-question/quiz-question.component';
 import { HighlightOptionDirective } from '../../../../directives/highlight-option.directive';
 
-
 @Component({
   selector: 'app-shared-option',
   templateUrl: './shared-option.component.html',
