d004fa1c70532aa982df4d67b705e3adf6d9e613
marvinrusinek <marvinrusinek@gmail.com>
2025-07-12 19:01:25 -0400

    1 file modified

--- src/app/shared/models/QAPayload.model.ts
+++ src/app/shared/models/QAPayload.model.ts
@@ -1,7 +1,9 @@
+import { Option } from './Option.model';
+import { QuizQuestion } from './QuizQuestion.model';
+
 export interface QAPayload {
   quizId: string;
   index: number;            // 0-based question index
-  questionText: string;     // trimmed question text
   explanation: string;      // optional explanation text (formatted)
   selectionMessage: string; // display message related to selection
   options: Option[];        // hydrated & normalized options
