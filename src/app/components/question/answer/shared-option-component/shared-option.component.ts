53f75aa1b574e19286156f3d42555859a109289c
marvinrusinek <marvinrusinek@gmail.com>
2025-07-24 15:19:01 -0400

    1 file modified

--- src/app/components/question/answer/shared-option-component/shared-option.component.ts
+++ src/app/components/question/answer/shared-option-component/shared-option.component.ts
@@ -2011,13 +2011,6 @@ export class SharedOptionComponent implements OnInit, OnChanges, AfterViewInit, 
     });
   }
 
-  logTemplateRender(i: number): string {
-    const now = performance.now();
-    console.log(`[🖼️ Render Row ${i} at ${now.toFixed(2)} ms]`);
-    return '';
-  }
-  
-
   getFeedbackBindings(option: Option, idx: number): FeedbackProps {
     // Check if the option is selected (fallback to false if undefined or null)
     const isSelected = this.isSelectedOption(option) ?? false;
