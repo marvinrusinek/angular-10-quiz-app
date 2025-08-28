6a06eacc3ea17b6df37d5de20e1cea8ad10abf1e
marvinrusinek <marvinrusinek@gmail.com>
2025-08-28 16:01:54 -0400

    1 file modified

--- src/app/components/question/quiz-question/quiz-question.component.ts
+++ src/app/components/question/quiz-question/quiz-question.component.ts
@@ -3468,6 +3468,17 @@ export class QuizQuestionComponent extends BaseQuestionComponent
     } finally {
       queueMicrotask(() => { this._clickGate = false; });
     }
+
+    // After the option is selected, call computeSelectionMessage
+    const message = this.selectionMessageService.computeSelectionMessage({
+      index, // Question index
+      questionType: this.currentQuestionType, // Type of question (e.g., SingleAnswer, MultipleAnswer)
+      options: this.currentOptions, // List of options for the current question
+      canonicalOptions: this.canonicalOptions, // The correct options
+    });
+
+    // Set or display the message (e.g., update the message displayed to the user)
+    this.selectionMessage = message;
   }
   
 
