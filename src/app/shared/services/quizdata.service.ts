cd489b004fbe7605640919af37b894b614b52b57
marvinrusinek <marvinrusinek@gmail.com>
2025-09-25 16:41:42 -0400

    1 file modified

--- src/app/shared/services/quizdata.service.ts
+++ src/app/shared/services/quizdata.service.ts
@@ -10,6 +10,7 @@ import { QuizQuestion } from '../../shared/models/QuizQuestion.model';
 import { QuizService } from '../../shared/services/quiz.service';
 import { Utils } from '../../shared/utils/utils';
 
+
 @Injectable({ providedIn: 'root' })
 export class QuizDataService implements OnDestroy {
   private quizUrl = 'assets/data/quiz.json';
