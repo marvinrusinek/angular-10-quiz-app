31570c51b83404201f7e6e94fd91e5dccb548c70
marvinrusinek <marvinrusinek@gmail.com>
2025-09-25 16:14:45 -0400

    1 file modified

--- src/app/containers/introduction/introduction.component.ts
+++ src/app/containers/introduction/introduction.component.ts
@@ -222,7 +222,6 @@ export class IntroductionComponent implements OnInit, OnDestroy {
   
     // Navigate to the quiz with preferences passed via state
     this.router.navigate(['/question', quizId, 1], {
-      queryParams: { quizId },
       state: { shouldShuffleOptions, feedbackMode },
     })
       .then((success) => {
