260f3306c681e731a6eb08a049e5d98ecf4d4cf8
marvinrusinek <marvinrusinek@gmail.com>
2025-09-25 20:00:41 -0400

    2 files modified

--- src/app/containers/quiz/quiz.component.ts
+++ src/app/containers/quiz/quiz.component.ts
@@ -1271,9 +1271,10 @@ export class QuizComponent implements OnInit, OnDestroy, OnChanges, AfterViewIni
     }
 
     try {
-      const quiz = await this.quizDataService.getQuiz(this.quizId)
-        .pipe(take(1))
-        .toPromise() as Promise<Quiz>;
+      const quiz = (await firstValueFrom(
+        this.quizDataService.getQuiz(this.quizId)
+          .pipe(take(1), takeUntil(this.destroy$))
+      )) as Quiz;
 
       if (!quiz) {
         console.error('Quiz is null or undefined. Failed to load quiz data.');
