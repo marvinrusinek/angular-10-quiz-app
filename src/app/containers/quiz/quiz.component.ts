27ffa89f67a5a99a95afa606315ab942040842f5
marvinrusinek <marvinrusinek@gmail.com>
2025-10-20 15:06:11 -0400

    1 file modified

--- src/app/containers/quiz/quiz.component.ts
+++ src/app/containers/quiz/quiz.component.ts
@@ -3548,7 +3548,7 @@ export class QuizComponent implements OnInit, OnDestroy, OnChanges, AfterViewIni
         this.questionToDisplay = question.questionText?.trim() || 'No question available';
   
         // Assign optionIds
-        const normalizedOptions = this.quizService.assignOptionIds([...(options ?? [])]);
+        const normalizedOptions = this.quizService.assignOptionIds([...(options ?? [])], this.currentQuestionIndex);
         this.optionsToDisplay = normalizedOptions;
   
         // Ensure options are fully loaded
