170799d9e0692f7bfdd708617d66232aba57dec6
marvinrusinek <marvinrusinek@gmail.com>
2025-10-31 19:31:00 -0400

    1 file modified

--- src/app/components/question/answer/answer-component/answer.component.ts
+++ src/app/components/question/answer/answer-component/answer.component.ts
@@ -17,7 +17,7 @@ import { QuizQuestionManagerService } from '../../../../shared/services/quizques
 import { QuizStateService } from '../../../../shared/services/quizstate.service';
 import { SelectedOptionService } from '../../../../shared/services/selectedoption.service';
 import { BaseQuestionComponent } from '../../../../components/question/base/base-question.component';
-import { QuizQuestionComponent } from '../../quiz-question/quiz-question.component';
+import type { QuizQuestionComponent } from '../../../../components/question/quiz-question/quiz-question.component';
 import { SharedOptionComponent } from '../../../../components/question/answer/shared-option-component/shared-option.component';
 
 
@@ -34,7 +34,7 @@ export class AnswerComponent extends BaseQuestionComponent implements OnInit, On
   sharedOptionComponent!: SharedOptionComponent;
 
   //@Output() componentLoaded = new EventEmitter<QuizQuestionComponent>();
-  @Output() componentLoaded = new EventEmitter<any>();
+  @Output() componentLoaded = new EventEmitter<QuizQuestionComponent>();
   quizQuestionComponent?: QuizQuestionComponent;
   // quizQuestionComponent?: InstanceType<ReturnType<typeof forwardRef>>;
   //quizQuestionComponent: QuizQuestionComponent | undefined;
