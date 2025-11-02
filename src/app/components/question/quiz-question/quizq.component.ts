413de6ec4f3e7d9b1016b8b11f3fa5681e2784e5
marvinrusinek <marvinrusinek@gmail.com>
2025-11-02 13:41:10 -0500

    3 files modified, 1 added, 1 deleted

--- src/app/components/question/quiz-question/quizq.component.ts
+++ src/app/components/question/quiz-question/quizq.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { BaseQuestion } from '../basex/base-question';
+import { BaseQuestion } from '../base/base-question';
 
 @Component({
   selector: 'quizq',
